93c859f658cde296c119728f6abb0ad21b8e37c6
Wedge Co-op IT Department <dba@homer.wedgenet>
2010-03-08 12:56:43 -0600

    Added PHP setup

--- pos/installation/ubuntu/install-server.sh
+++ pos/installation/ubuntu/install-server.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 /pos/installation/ubuntu/server-mysql.sh
-# /pos/installation/ubuntu/php_server.pl
+
+/pos/installation/ubuntu/php_server.pl
 # /pos/installation/ubuntu/apache_server.pl
