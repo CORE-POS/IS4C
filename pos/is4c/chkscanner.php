b26c91b37152ffa256a58b1254138c920c5690b3
David Baucum <maxolasersquad@gmail.com>
2010-03-18 13:40:02 -0400

    Reformatted code to more closely match XHTML specifications.

--- pos/is4c/chkscanner.php
+++ pos/is4c/chkscanner.php
@@ -1,62 +1,54 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head></head>
+    <body>
+        <script type="text/javascript">
+            var xmlHttp;
+            var xmlmessage = "";
 
-<head></head>
+            function createXMLHttpRequest() {
 
-<body>
-<script type="text/javascript">
+	            if (window.ActiveXObject) {
+		            xmlHttp = new ActiveXObject("Microsft.XMLHTTP");
+	            }
+	            else if (window.XMLHttpRequest) {
+		            xmlHttp = new XMLHttpRequest();
+	            }
+            }
 
-var xmlHttp;
-var xmlmessage = "";
+            function startRequest() {
+	            createXMLHttpRequest();
+	            xmlHttp.onreadystatechange = handleStateChange;
+	            var myRandom=parseInt(Math.random()*99999999);
+	            xmlHttp.open("GET", "rs232/scanner?rand=" + myRandom, true);
+	            xmlHttp.send(null);
+            }
 
-function createXMLHttpRequest() {
+            function handleStateChange() {
+	            if (xmlHttp.readyState == 4) {
+		            if (xmlHttp.status == 200) {
+			            var xmldata = xmlHttp.responseText;
+			            if (xmldata.length >= 9) {
+				            var inputVal = window.top.input.document.form.reginput.value;
+				            window.top.input.document.form.reginput.value = "";
+				            window.top.main_frame.document.form1.input.value =inputVal+xmldata;	
+				            window.top.main_frame.document.form1.submit();
+				            var myRandom=parseInt(Math.random()*99999999);
+				            xmlHttp.open("GET", "clearscanner.php?rand=" + myRandom, true);
+				            xmlHttp.send(null);
+				            xmldata = "";
+			            }
+		            }
+	            }
+            }
 
-	if (window.ActiveXObject) {
-		xmlHttp = new ActiveXObject("Microsft.XMLHTTP");
-	}
-	else if (window.XMLHttpRequest) {
-		xmlHttp = new XMLHttpRequest();
-	}
-}
+            function listen() {
+	            startRequest();
+	            setTimeout("listen();", 70);
+            }
 
-function startRequest() {
-	createXMLHttpRequest();
-			
-	xmlHttp.onreadystatechange = handleStateChange;
-	var myRandom=parseInt(Math.random()*99999999);
-	xmlHttp.open("GET", "rs232/scanner?rand=" + myRandom, true);
-	xmlHttp.send(null);
-}
-
-function handleStateChange() {
-	if (xmlHttp.readyState == 4) {
-				
-		if (xmlHttp.status == 200) {
-
-			var xmldata = xmlHttp.responseText;
-
-			if (xmldata.length >= 9) {
-				var inputVal = window.top.input.document.form.reginput.value;
-				window.top.input.document.form.reginput.value = "";
-				window.top.main_frame.document.form1.input.value =inputVal+xmldata;	
-				window.top.main_frame.document.form1.submit();
-				var myRandom=parseInt(Math.random()*99999999);
-				xmlHttp.open("GET", "clearscanner.php?rand=" + myRandom, true);
-				xmlHttp.send(null);
-				xmldata = "";
-			}
-		}
-	}
-}
-
-function listen() {
-	startRequest();
-	setTimeout("listen();", 70);
-
-}
-
-listen();
-
-
-</script>
-</body>
+            listen();
+        </script>
+    </body>
 </html>
+
--- pos/is4c/end.php
+++ pos/is4c/end.php
@@ -1,130 +1,115 @@
-<body bgcolor='#ffffff'>
-<?
-include_once("ini/ini.php");
-include_once("session.php");
-include_once("printLib.php");
-include_once("printReceipt.php");
-include_once("connect.php");
-include_once("additem.php");
-include_once("ccLib.php");
-include_once("maindisplay.php");
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <body bgcolor='#ffffff'>
+        <?
+            include_once("ini/ini.php");
+            include_once("session.php");
+            include_once("printLib.php");
+            include_once("printReceipt.php");
+            include_once("connect.php");
+            include_once("additem.php");
+            include_once("ccLib.php");
+            include_once("maindisplay.php");
 
 
 
-if ($_SESSION["End"] == 1) {
+            if ($_SESSION["End"] == 1) {
+	            addtransDiscount();
+	            addTax();
+            }
+            $receiptType = $_SESSION["receiptType"];
+            $_SESSION["receiptType"] = "";
 
-	addtransDiscount();
-	addTax();
-}
-/* acg 
-if ($_SESSION["End"] == 1 && $_SESSION["isStaff"] == 3 && $_SESSION["volunteerDiscount"] != 0) {
-	
-	wmupdate($_SESSION["memID"], $_SESSION["volunteerDiscount"]);
-}
-*/
-$receiptType = $_SESSION["receiptType"];
-$_SESSION["receiptType"] = "";
+            if (strlen($receiptType) > 0) {
+	            printReceipt($receiptType);
 
+	            if ($_SESSION["End"] == 1 || $_SESSION["msg"] == 2) {
+		            if ($_SESSION["msg"] == 2) {
+			            $returnHome = 1;
+		            } else {
+			            $returnHome = 0;
+		            }
 
-if (strlen($receiptType) > 0) {
-	
-	printReceipt($receiptType);
+		            $_SESSION["End"] = 0;
 
-	if ($_SESSION["End"] == 1 || $_SESSION["msg"] == 2) {
+		            if (cleartemptrans() == 1) {
 
-		if ($_SESSION["msg"] == 2) {
-			$returnHome = 1;
-		} else {
-			$returnHome = 0;
-		}
+			            // force cleartemptrans to finish before returning home.
+			            // Because returnHome() depends on javascript which
+			            // can be triggered independently of php
 
-		$_SESSION["End"] = 0;
+			            if ($returnHome == 1) {
+				            $returnHome = 0;
+				            returnHome();
+			            }
+		            }
+	            }
+            }
 
-		if (cleartemptrans() == 1) {
+            function cleartemptrans() {
+	            $db = tDataConnect();
 
-			// force cleartemptrans to finish before returning home.
-			// Because returnHome() depends on javascript which
-			// can be triggered independently of php
+	            if($_SESSION["msg"] == 2) {
+		            $_SESSION["msg"] = 99;
+		            sql_query("update localtemptrans set trans_status = 'X'", $db);
+	            }
 
-			if ($returnHome == 1) {
-				$returnHome = 0;
-				returnHome();
-			}
-		}
+	            if ($_SESSION["DBMS"] == "mssql") {
+		            sql_query("exec clearTempTables", $db);
+	            } else {
+		            moveTempData();
+		            truncateTempTables();
+	            }
 
-	}
-}
+	            sql_close($db);
 
-function cleartemptrans() {
+	            testremote();
 
+	            loadglobalvalues();	
+	            $_SESSION["transno"] = $_SESSION["transno"] + 1;
+	            setglobalvalue("TransNo", $_SESSION["transno"]);
 
-	$db = tDataConnect();
+	            if ($_SESSION["TaxExempt"] != 0) {
+		            $_SESSION["TaxExempt"] = 0;
+		            setglobalvalue("TaxExempt", 0);
+	            }
 
-	if($_SESSION["msg"] == 2) {
-		$_SESSION["msg"] = 99;
-		sql_query("update localtemptrans set trans_status = 'X'", $db);
-	}
+	            memberReset();
+	            transReset();
+	            printReset();
 
-	if ($_SESSION["DBMS"] == "mssql") {
-		sql_query("exec clearTempTables", $db);
-	} else {
-		moveTempData();
-		truncateTempTables();
-	}
+	            getsubtotals();
 
-	sql_close($db);
+	            delete_file(remote_oux());
+	            delete_file(local_inx());
 
-	testremote();
-
-	loadglobalvalues();	
-	$_SESSION["transno"] = $_SESSION["transno"] + 1;
-	setglobalvalue("TransNo", $_SESSION["transno"]);
-
-	if ($_SESSION["TaxExempt"] != 0) {
-		$_SESSION["TaxExempt"] = 0;
-		setglobalvalue("TaxExempt", 0);
-	}
-
-	memberReset();
-	transReset();
-	printReset();
-
-	getsubtotals();
-
-	delete_file(remote_oux());
-	delete_file(local_inx());
-
-	return 1;
-}
+	            return 1;
+            }
 
 
-function truncateTempTables() {
-	$connection = tDataConnect();
-	$query1 = "truncate table localtemptrans";
-	$query2 = "truncate table activitytemplog";
+            function truncateTempTables() {
+	            $connection = tDataConnect();
+	            $query1 = "truncate table localtemptrans";
+	            $query2 = "truncate table activitytemplog";
 
-	sql_query($query1, $connection);
-	sql_query($query2, $connection);
+	            sql_query($query1, $connection);
+	            sql_query($query2, $connection);
 
-	sql_close($connection);
-}
+	            sql_close($connection);
+            }
 
-function moveTempData() {
-	$connection = tDataConnect();
-//	sql_query("call movetempdata()", $connection);
+            function moveTempData() {
+	            $connection = tDataConnect();
 
-	sql_query("update localtemptrans set trans_type = 'T' where trans_subtype = 'CP'", $connection);
-//	sql_query("update localtemptrans set trans_type = 'T', trans_subtype = 'IC' where upc in ('0000000008019', '0000000003031', '0000000001014')", $connection);
-	sql_query("update localtemptrans set upc = 'DISCOUNT', description = upc, department = 0 where trans_status = 'S'", $connection);
+	            sql_query("update localtemptrans set trans_type = 'T' where trans_subtype = 'CP'", $connection);
+	            sql_query("update localtemptrans set upc = 'DISCOUNT', description = upc, department = 0 where trans_status = 'S'", $connection);
 
-	sql_query("insert into localtrans select * from localtemptrans", $connection);
-	sql_query("insert into dtransactions select * from localtemptrans", $connection);
+	            sql_query("insert into localtrans select * from localtemptrans", $connection);
+	            sql_query("insert into dtransactions select * from localtemptrans", $connection);
+	            sql_query("insert into activitylog select * from activitytemplog", $connection);
+	            sql_query("insert into alog select * from activitytemplog", $connection);
 
-	sql_query("insert into activitylog select * from activitytemplog", $connection);
-	sql_query("insert into alog select * from activitytemplog", $connection);
-
-
-	sql_close($connection);
-}
-?>
-</body>
+	            sql_close($connection);
+            }
+        ?>
+    </body></html>
