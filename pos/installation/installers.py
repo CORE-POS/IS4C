f0bb1a832a235cecfe62fbe9e9d33c75d2311c7a
Lance Edgar <lance@edbob.org>
2010-04-09 01:31:29 -0500

    Updated server installer to include all table/viw/insert files it can find instead of maintaining a list.

--- pos/installation/installers.py
+++ pos/installation/installers.py
@@ -1,12 +1,18 @@
 
 
 from getpass import getpass
-import os.path
+import os
+from glob import glob
 
 
-def exec_script(connection, script_path):
+def abspath(path):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), 'mysql', path))
+
+
+def exec_script(connection, script_path, absolute=False):
     cursor = connection.cursor()
-    script_path = os.path.join(os.path.dirname(__file__), "mysql", script_path)
+    if not absolute:
+        script_path = abspath(script_path)
     sql = ""
     script_file = open(script_path)
     for line in script_file:
@@ -18,6 +24,19 @@ def exec_script(connection, script_path):
     if sql.strip():
         cursor.execute(sql)
     cursor.close()
+
+
+def exec_scripts(connection, script_pattern, first_paths=[]):
+    first_abspaths = []
+    for script_path in first_paths:
+        script_path = abspath(script_path)
+        exec_script(connection, script_path)
+        first_abspaths.append(script_path)
+
+    script_pattern = abspath(script_pattern)
+    for script_path in glob(script_pattern):
+        if script_path not in first_abspaths:
+            exec_script(connection, script_path, absolute=True)
 
 
 def get_user_input():
@@ -43,6 +62,8 @@ def get_user_input():
 
 
 __all__ = [
+    'abspath',
     'exec_script',
+    'exec_scripts',
     'get_user_input',
     ]
