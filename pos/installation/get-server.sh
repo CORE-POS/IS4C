1d57f53d16711abcd6bbf43bc382fb7317a3b864
Wedge Co-op IT <wedgenet@wedge.coop>
2010-04-27 12:05:10 -0500

    Cleaned installation script to use python and point to v2.2_dev tarball

--- pos/installation/get-server.sh
+++ pos/installation/get-server.sh
@@ -3,7 +3,7 @@
 # TODO - Oh, a lot. Use user-defined variables. Use system-defined variable. Set environments.
 
 cd /tmp
-wget http://is4c.coop/download/IS4C.tgz
+wget http://is4c.coop/download/IS4C.2.2_dev.tgz
 tar -xzf /tmp/IS4C.tgz
 cp -r /tmp/IS4C/pos /
 
