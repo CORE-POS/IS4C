1f0105dad8770776c34760312c059bdb45379234
jdpurdyvi <jdpurdyvi@yahoo.com>
2010-07-15 05:25:14 +0800

    Added FPDF and PHP-Image-Barcode packages to server installation

--- pos/installation/ubuntu/install-server.sh
+++ pos/installation/ubuntu/install-server.sh
@@ -8,7 +8,7 @@ echo "mysql-server mysql-server/root_password select $ROOT_PASSWORD" | debconf-s
 echo "mysql-server mysql-server/root_password_again select $ROOT_PASSWORD" | debconf-set-selections
 echo "python-mysqldb mysql-server/root_password select $ROOT_PASSWORD" | debconf-set-selections
 echo "python-mysqldb mysql-server/root_password_again select $ROOT_PASSWORD" | debconf-set-selections
-aptitude install -y mysql-server apache2 php5 libapache2-mod-php5 python-mysqldb php5-mysql
+aptitude install -y mysql-server apache2 php5 libapache2-mod-php5 python-mysqldb php5-mysql php-image-barcode php-fpdf
 
 python /pos/installation/install_server.py
 
