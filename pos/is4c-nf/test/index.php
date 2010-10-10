2f4f492db92e2728c1b9c303de661f7a5b933d91
unknown <andy@.(none)>
2010-10-10 15:54:44 -0500

    New test directory. With parsing taking place in ajax calls, it's convenient to double check what's actually getting generated by various inputs.

---
+++ pos/is4c-nf/test/index.php
@@ -0,0 +1,7 @@
+<html>
+<fieldset>
+<legend>Parser Test</legend>
+<form action="../ajax-callbacks/ajax-parser.php" method="get">
+Input: <input type="text" name="input" />
+<input type="submit" value="Test Parser" />
+</fieldset>
