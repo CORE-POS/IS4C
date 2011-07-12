43d941e8319c6b329af214f4b12b399dfabde384
Andy Theuninck <andy@locke.wfco-op.store>
2011-07-12 11:38:22 -0500

    Bring github in line wih current code. Added fannie/src/Credentials to store unshared files (e.g., miscellaneous passwords)

---
+++ fannie/reports/Store-Specific/WFC/ExternalEmail/index.php
@@ -0,0 +1,25 @@
+<?php
+
+include('../../../../config.php');
+include($FANNIE_ROOT.'src/SQLManager.php');
+
+$header = "External Email List";
+$page_title = "Fannie :: Email List";
+include($FANNIE_ROOT.'src/header.html');
+
+require($FANNIE_ROOT.'src/Credentials/OutsideDB.wfc.php');
+$q = "SELECT email FROM userData WHERE email LIKE '%@%.%'";
+$r = $dbc->query($q);
+
+echo '<input type="submit" value="Select All"
+	onclick="$(\'#emailListing\').focus();$(\'#emailListing\').select();"
+	/><br />';
+echo '<textarea id="emailListing" style="width:100%;height:400px;background:#cccccc;border=solid 1px black;padding:10px;">';
+while($w = $dbc->fetch_row($r)){
+	echo $w[0]."\n";
+}	
+echo '</textarea>';
+
+include($FANNIE_ROOT.'src/footer.html');
+
+?>
