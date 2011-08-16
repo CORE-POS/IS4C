fb2dbaa7f34a902598227768ad55670e39ef7bda
Andy Theuninck <andy@locke.wfco-op.store>
2011-08-16 10:17:24 -0500

    eCommerce POS tomfoolery (in no shape for real use)

---
+++ pos/is4c-web/index.php
@@ -0,0 +1,3 @@
+<?php
+header('Location: gui-modules/loginPage.php');
+?>
---
+++ pos/is4c-web/lib/paypal.php
@@ -0,0 +1,137 @@
+<?php
+/*******************************************************************************
+
+    Copyright 2011 Whole Foods Co-op
+
+    This file is part of IS4C.
+
+    IS4C is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    IS4C is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    in the file license.txt along with IS4C; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*********************************************************************************/
+ // session_start();
+ 
+$IS4C_PATH = isset($IS4C_PATH)?$IS4C_PATH:"";
+if (empty($IS4C_PATH)){ while(!file_exists($IS4C_PATH."is4c.css")) $IS4C_PATH .= "../"; }
+
+define("PAYPAL_TEST_URL","https://api-3t.sandbox.paypal.com/nvp");
+define("PAYPAL_TEST_RD","https://www.sandbox.paypal.com/webscr");
+define("PAYPAL_TEST_UID","andy_1294773506_biz_api1.wholefoods.coop");
+define("PAYPAL_TEST_PWD","1294773513");
+define("PAYPAL_TEST_KEY","AHI.ownNeZJwzILhfLs6SZLW3SbqAKuVP7Hxz8cBbpG8hElkamxQIneS");
+
+define("PAYPAL_LIVE_URL","https://api-3t.paypal.com/nvp");
+
+define("PAYPAL_URL_SUCCESS","http://key/git/IS4C/pos/is4c-web/gui-modules/confirm.php");
+define("PAYPAL_URL_FAILURE","http://key/git/IS4C/pos/is4c-web/gui-modules/cart.php");
+define("PAYPAL_NVP_VERSION",63);
+
+if (!isset($IS4C_LOCAL)) include($IS4C_PATH."lib/LocalStorage/conf.php");
+
+/* utility to transform array to url-encoded
+   argument string */
+function argstring($arr){
+	$ret = "";
+	foreach($arr as $key=>$val){
+		$ret .= $key."=".urlencode($val)."&";
+	}
+	return substr($ret,0,strlen($ret)-1);
+}
+
+/* set up arguments required on
+   all paypal NVP requests */
+function pp_init_args($method){
+	$args = array(
+		'METHOD'	=> $method,
+		'USER'		=> PAYPAL_TEST_UID,
+		'PWD'		=> PAYPAL_TEST_PWD,
+		'SIGNATURE'	=> PAYPAL_TEST_KEY,
+		'VERSION'	=> PAYPAL_NVP_VERSION,
+	);
+	return $args;
+}
+
+/* submit request to paypal nvp, return
+   results as keyed array */
+function pp_do_curl($args){
+	$curl_handle = curl_init(PAYPAL_TEST_URL);
+	curl_setopt($curl_handle,CURLOPT_POST,True);
+	curl_setopt($curl_handle,CURLOPT_POSTFIELDS,argstring($args));
+	curl_setopt($curl_handle, CURLOPT_HEADER, 0);
+	curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT,15);
+	curl_setopt($curl_handle, CURLOPT_FAILONERROR,False);
+	curl_setopt($curl_handle, CURLOPT_FOLLOWLOCATION,False);
+	curl_setopt($curl_handle, CURLOPT_FRESH_CONNECT,True);
+	curl_setopt($curl_handle, CURLOPT_TIMEOUT,30);
+	//curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, 0);
+
+	$response = curl_exec($curl_handle);
+	$result = array();
+	parse_str($response,$result);
+	return $result;
+}
+
+/* Set up express checkout request
+   NOTE: redirects to paypal on success
+
+   Returns True on success, False on failure
+*/
+function SetExpressCheckout($amt,$tax=0,$email=""){
+	$args = pp_init_args('SetExpressCheckout');
+	$args['RETURNURL'] = PAYPAL_URL_SUCCESS;
+	$args['CANCELURL'] = PAYPAL_URL_FAILURE;
+	$args['PAYMENTREQUEST_0_AMT'] = $amt;
+	$args['PAYMENTREQUEST_0_TAXAMT'] = $tax;
+	$args['PAYMENTREQUEST_0_ITEMAMT'] = $amt - $tax;
+	if (!empty($email))
+		$args['EMAIL'] = $email;
+
+	$result = pp_do_curl($args);
+	if ($result['ACK'] == 'Success' && isset($result['TOKEN'])){
+		header("Location: ".PAYPAL_TEST_RD."?cmd=_express-checkout&token=".$result['TOKEN']);
+	}
+	else var_dump($result);
+}
+
+/* Collect user information from paypal. 
+   $token is a $_GET argument provided by paypal
+   when users return to our site 
+
+   Returns paypal response as keyed array
+*/
+function GetExpressCheckoutDetails($token){
+	$args = pp_init_args('GetExpressCheckoutDetails');
+	$args['TOKEN'] = $token;
+	$result = pp_do_curl($args);
+	return $result;
+}
+
+/* Finalize payment. Token and ppID (PAYERID) are
+   provided by paypal.
+
+   Returns paypal response as keyed array
+*/
+function DoExpressCheckoutPayment($token,$ppID,$amt){
+	$args = pp_init_args('DoExpressCheckoutPayment');
+	$args['TOKEN'] = $token;
+	$args['PAYMENTREQUEST_0_PAYMENTACTION'] = 'Sale';
+	$args['PAYERID'] = $ppID;
+	$args['PAYMENTREQUEST_0_AMT'] = $amt;
+
+	$result = pp_do_curl($args);
+	return $result;
+}
+
+?>
