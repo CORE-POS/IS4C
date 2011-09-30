a4b263a821ea98d0e286c9d925f34a79b36cff14
Andy Theuninck <andy@locke.wfco-op.store>
2011-09-30 09:42:30 -0500

    Alternate pricemethod class for Ypsi

---
+++ pos/is4c-nf/lib/Scanning/PriceMethods/NoDiscOnSalesPM.php
@@ -0,0 +1,75 @@
+<?php
+/*******************************************************************************
+
+    Copyright 2010 Whole Foods Co-op
+
+    This file is part of IT CORE.
+
+    IT CORE is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    IT CORE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    in the file license.txt along with IT CORE; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*********************************************************************************/
+
+/* NoDiscOnSale PriceMethod module
+
+   A replacement for BasicPM. This disables
+   % discount for items that are on sale
+   
+*/
+$CORE_PATH = isset($CORE_PATH)?$CORE_PATH:"";
+if (empty($CORE_PATH)){ while(!file_exists($CORE_PATH."pos.css")) $CORE_PATH .= "../"; }
+
+if (!class_exists('PriceMethod')) include($CORE_PATH.'lib/Scanning/PriceMethod.php');
+if (!function_exists('addItem')) include($CORE_PATH.'lib/additem.php');
+if (!function_exists('truncate2')) include($CORE_PATH.'lib/lib.php');
+
+class NoDiscOnSalePM extends PriceMethod {
+
+	function addItem($row,$quantity,$priceObj){
+		if ($quantity == 0) return false;
+
+		$pricing = $priceObj->priceInfo($row,$quantity);
+
+		addItem($row['upc'],
+			$row['description'],
+			'I',
+			' ',
+			' ',
+			$row['department'],
+			$quantity,
+			$pricing['unitPrice'],
+			truncate2($pricing['unitPrice'] * $quantity),
+			$pricing['regPrice'],
+			$row['scale'],
+			$row['tax'],
+			$row['foodstamp'],
+			$pricing['discount'],
+			$pricing['memDiscount'],
+			($row['discounttype']>0?0:$row['discount']),
+			$row['discounttype'],
+			$quantity,
+			$row['pricemethod'],
+			$row['quantity'],
+			$row['groupprice'],
+			$row['mixmatchcode'],
+			0,
+			0,
+			(isset($row['cost'])?$row['cost']*$quantity:0.00),
+			(isset($row['numflag'])?$row['numflag']:0),
+			(isset($row['charflag'])?$row['charflag']:'')
+		);
+	}
+}
+
+?>
