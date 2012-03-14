8fa0d42bdab40bddd0c79df21f1104bda33ca463
Andy Theuninck <andy@key.wfco-op.store>
2012-03-14 09:45:40 -0500

    Table/view fixes for mysql

--- fannie/install/sql/trans/newBalanceToday_cust.php
+++ fannie/install/sql/trans/newBalanceToday_cust.php
@@ -25,12 +25,18 @@ $names = qualified_names();
 
 $CREATE['trans.newBalanceToday_cust'] = "
 	CREATE VIEW newBalanceToday_cust AS
-		SELECT   c.CardNo as memnum, c.Discount as discounttype,c.Balance as ARCurrBalance,
-		(case when a.charges is NULL then 0 ELSE a.charges END) as totcharges,
-		(CASE WHEN a.payments IS NULL THEN 0 ELSE a.payments END) as totpayments,
-		(CASE when a.card_no is NULL then c.Balance ELSE (c.Balance -a.charges - a.payments)END) as balance
-		FROM {$names['op']}.custdata as c left outer join memIouToday as a ON c.CardNo = a.card_no
-		where c.personNum = 1
+	SELECT   
+	b.CardNo as memnum, 
+	0 as discounttype,c.Balance as ARCurrBalance,
+	(case when a.charges is NULL then 0 ELSE a.charges END) as totcharges,
+	(CASE WHEN a.payments IS NULL THEN 0 ELSE a.payments END) as totpayments,
+	(CASE when a.card_no is NULL then c.Balance ELSE (c.Balance -a.charges - a.payments)END) as balance,
+	(CASE WHEN a.card_no IS NULL THEN 0 ELSE 1 END) as mark
+	FROM 
+	{$names['op']}.custdata as b left join
+	ar_sum_cache as c on b.CardNo=c.card_no and b.personNum=1
+	left outer join memIouToday as a ON c.card_no = a.card_no and b.persoNnum=1
+	where b.personNum=1
 ";
 
 if (!$con->table_exists("memIouToday"))
