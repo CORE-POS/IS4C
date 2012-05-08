a1eb14f68146590c9d0bf8d50c6e6ef55b5f4b54
Andy Theuninck <andy@key.wfco-op.store>
2012-05-08 09:43:57 -0500

    Server escaping alog.Interval

--- pos/is4c-nf/lib/connect.php
+++ pos/is4c-nf/lib/connect.php
@@ -271,7 +271,7 @@ function uploadtoServer()
 		if ($CORE_LOCAL->get("DBMS") == "mysql")
 			$local_columns = str_replace("Interval","`Interval`",$al_matches);
 		if ($CORE_LOCAL->get("mDBMS") == "mysql")
-			$local_columns = str_replace("Interval","`Interval`",$al_matches);
+			$server_columns = str_replace("Interval","`Interval`",$al_matches);
 		$al_success = $connect->transfer($CORE_LOCAL->get("tDatabase"),
 			"select $local_columns FROM alog",
 			$CORE_LOCAL->get("mDatabase"),
