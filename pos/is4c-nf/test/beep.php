eef00012bc215d9a63c2022e6938feaac03d47af
joel brock <joelbrock@gmail.com>
2012-05-21 14:16:27 -0700

    test: beep.php

--- pos/is4c-nf/test/beep.php
+++ pos/is4c-nf/test/beep.php
@@ -4,6 +4,12 @@ if (!isset($CORE_LOCAL)) include($CORE_PATH."lib/LocalStorage/conf.php");
 
 require_once ($CORE_PATH."lib/lib.php");
 
+goodBeep();
 twoPairs();
+errorBeep();
+
+rePoll();
+
+udpSend('twoPairs');
 
 ?>
