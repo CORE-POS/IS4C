3531082c2cb645333ca65fd44c5b7dad456e39e0
John Leary <john.leary@gmail.com>
2012-06-01 12:29:19 -0400

    Complete rewrite: Enable configuration changes on-the-fly; no recompiling needed.
    Signed-off-by: John Leary <john.leary@gmail.com>

--- pos/is4c-nf/scale-drivers/drivers/rs232/ssd.c
+++ pos/is4c-nf/scale-drivers/drivers/rs232/ssd.c
@@ -1,23 +1,21 @@
 /*******************************************************************************
 
-    Copyright 2001, 2004 Wedge Community Co-op
+	Copyright 2001, 2004 Wedge Community Co-op
+	This file is part of IS4C.
 
-    This file is part of IT CORE.
+	IS4C is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    IT CORE is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	IS4C is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    IT CORE is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    in the file license.txt along with IT CORE; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
+	You should have received a copy of the GNU General Public License
+	in the file license.txt along with IS4C; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ********************************************************************************
 
    This is the preliminary daemon that effectively acts as part of the
@@ -50,174 +48,326 @@
 #include <string.h>
 #include <termios.h> /* POSIX terminal control definitions */
 #include <ctype.h>
+#include <signal.h>
 
-#define SSD_SERIAL_PORT "/dev/ttyS0"
-#define SCALE_OUTPUT_FILE "/pos/is4c/rs232/scale"
-#define SCANNER_OUTPUT_FILE "/pos/is4c/rs232/scanner"
+int scannerFD = 0;
+int num, n;
+char chout[16];
+char serialBuffer[16];
+char preBuffer[16];
+char scannerInput[12];
+char scaleInput[9];
+char scaleBuffer[9] = "000000000";
+struct termios options;
+char serialInput;
 
-int main(void) {
-    /* Our process ID and Session ID */
-    pid_t pid, sid;
+char config_file	[1024]	= 	"ssd.conf";
+char serial_port	[1024]	= 	"/dev/ttyS0";
+char scanner_file	[1024]	=	"scanner";
+char scale_file		[1024]	= 	"scale";
+char log_file		[1024]	= 	"ssd.log";
+char pid_file		[1024]	= 	"ssd.pid";
 
-    /* Fork off the parent process */
-    pid = fork();
-    if (pid < 0) {
-        exit(EXIT_FAILURE);
-    }
+FILE *fp_scanner;
+FILE *fp_scale;
+FILE *fp_log;
+FILE *fp_pid;
 
-    /* If we got a good PID, then we can exit the parent process. */
-    if (pid > 0) {
-        exit(EXIT_SUCCESS);
-    }
+int in_buffer = 0;
+int i;
+int q = 0;
 
-    /* Change the file mode mask */
-    umask(0);
 
-    /* Create a new SID for the child process */
-    sid = setsid();
-    if (sid < 0) {
-        /* Log the failure */
-        exit(EXIT_FAILURE);
-    }
 
-    /* Change the current working directory */
-    if ((chdir("/")) < 0) {
-        /* Log the failure */
-        exit(EXIT_FAILURE);
-    }
+void read_config_file(char *loc)
+{
+	FILE *fp_conf;
+	char line[1024];
+	char *option;
+	char *value;
 
-    /* Close out the standard file descriptors */
-    close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
+	if (loc != NULL) strcpy(config_file, loc);
 
-    int open_port(void) {
-        int fd;                   /* File descriptor for the port */
+	fp_conf = fopen(config_file, "r");
+	if (fp_conf == NULL) return;
 
-        fd = open(SSD_SERIAL_PORT, O_RDWR | O_NOCTTY | O_NDELAY);
+	while (fgets(line, 1024, fp_conf) != NULL) {
+		option = strtok(line, " \t");
+		value = strtok(NULL, "\n");
 
-        if (fd == -1) {
-         fprintf(stderr, "open_port: Unable to open /dev/ttyS0 - %s\n",
-             strerror(errno));
-        }
+		if (strcmp(option, "ConfFile") == 0) {
+			strcpy(config_file, value);
+		}
+		else if (strcmp(option, "SerialPort") == 0) {
+			strcpy(serial_port, value);
+		}
+		else if (strcmp(option, "ScannerFile") == 0) {
+			strcpy(scanner_file, value);
+		}
+		else if (strcmp(option, "ScaleFile") == 0) {
+			strcpy(scale_file, value);
+		}
+		else if (strcmp(option, "LogFile") == 0) {
+			strcpy(log_file, value);
+		}
+		else if (strcmp(option, "PIDFile") == 0) {
+			strcpy(pid_file, value);
+		}
+	}
+	fclose(fp_conf);
+}
 
-        return (fd);
-    }
 
-    int mainfd = 0;
-    int num, n; // File descriptor
-    char chout[16];
-    char serialBuffer[100];
-    char preBuffer[16];
-    char scannerInput[100];
-    char scaleInput[9];
-    char scaleBuffer[9] = "000000000";
-    struct termios options;
-    char serialInput;
-    mainfd = open_port();
-    fcntl(mainfd, F_SETFL, FNDELAY); // Configure port reading
-    /* Get the current options for the port */
-    tcgetattr(mainfd, &options);
-    cfsetispeed(&options, B9600); // Set the baud rates to 9600
-    cfsetospeed(&options, B9600);
-    // Enable the receiver and set local mode
-    options.c_cflag |= (CLOCAL | CREAD);
-    /* 7 bits, odd parity */
-    options.c_cflag |= PARENB;
-    options.c_cflag |= PARODD;  // odd parity
-    options.c_cflag &= ~CSTOPB;
-    options.c_cflag &= ~CSIZE;
-    options.c_cflag |= CS7;
-    options.c_cflag |= CRTSCTS;
-    /* Enable data to be processed as raw input */
-    options.c_lflag &= ~(ICANON | ECHO | ISIG);
-    /* Set the new options for the port */
-    tcsetattr(mainfd, TCSANOW, &options);
-    FILE *fp_scanner;
-    FILE *fp_scale;
-    int in_buffer = 0;
-    int i;
-    n = 0;
-    num = 0;
-    write(mainfd, "S11\r", 5);
-    write(mainfd, "S14\r", 5);
+void log_message(char *message)
+{
+	fp_log = fopen(log_file, "a");
+	fprintf(fp_log, message);
+	fclose(fp_log);
+}
 
-    while (1) {
-        in_buffer = read(mainfd, &chout, 1); /* Read character from ABU */
 
-        // if data is present in the serial port buffer
-        if (in_buffer != -1) {
-            if (chout[0] == 'S') {
-                num = 0;
-            }
+void set_handler(int signal, void *handler)
+{
+	struct sigaction new_sa;
 
-            serialBuffer[num] = chout[0];
-            num++;
+	new_sa.sa_handler = handler;
+	new_sa.sa_flags = SA_RESTART;
+	if (sigaction(signal, &new_sa, 0) != 0) {
+		log_message("Couldn't set up handler.\n");
+		exit(EXIT_FAILURE);
+	}
+}
 
-            if (chout[0] == '\n' && num > 2) {
-                serialBuffer[num] = '\0';
 
-                /**************** process scanned data ****************/
-                if (serialBuffer[1] == '0') {
-		    if (serialBuffer[3] == 'A' || serialBuffer[3] == 'E' || serialBuffer[3] == 'F'){
-			/* familar UPC A/E/F */
-                        for (i = 0; i < 17; i++) {
-                            scannerInput[i] = serialBuffer[i+4];
-                        }
-		    }
-		    else if (serialBuffer[3] == 'R'){
-			/* GS1 databar */
-			scannerInput[0] = 'G';
-			scannerInput[1] = 'S';
-			scannerInput[2] = '1';
-			scannerInput[3] = '~';
-			scannerInput[4] = serialBuffer[3];
-			scannerInput[5] = serialBuffer[4];
+int connect_scanner(void)
+{
+	if (scannerFD) return scannerFD;
 
-			for (i=5; i<= num; i++)	{
-			    scannerInput[i+1] = serialBuffer[i];
-			}	
-		    }
-		    else {
-			/* unknown barcode type */
-			scannerInput[0] = '\0';
-		    }
-                    fp_scanner = fopen(SCANNER_OUTPUT_FILE, "w");
-                    fprintf(fp_scanner, "%s\n", scannerInput);
-                    fclose(fp_scanner);
-                }
+	scannerFD = open(serial_port, O_RDWR | O_NOCTTY | O_NDELAY);
+	if (scannerFD == -1) {
+		fp_log = fopen(log_file, "a");
+		fprintf(fp_log, "connect_scanner(): Unable to open %s - %s\n", serial_port, strerror(errno));
+		fclose(fp_log);
+	}
+	return scannerFD;
+}
 
-                /**************** process weight data ******************/
-                if (serialBuffer[1] == '1') {
-                    if (serialBuffer[2] == '1') {
-                        write(mainfd, "S14\r", 5);
-                    }
-                    else if (serialBuffer[2] == '4' && serialBuffer[3] == '3') {
-                        write(mainfd, "S11\r", 5);
-                        if (strcmp(scaleBuffer, serialBuffer) != 0) {
-                            fp_scale = fopen(SCALE_OUTPUT_FILE, "w");
-                            fprintf(fp_scale, "%s\n", serialBuffer);
-                            fclose(fp_scale);
-                        }
-                    }
-                    else if (serialBuffer[2] == '4') {
-                        write(mainfd, "S14\r", 5);
-                        if (strcmp(scaleBuffer, serialBuffer) != 0) {
-                            fp_scale = fopen(SCALE_OUTPUT_FILE, "w");
-                            fprintf(fp_scale, "%s\n", serialBuffer);
-                            fclose(fp_scale);
-                        }
-                    }
-                    for (i = 0; i < 10; i++) {
-                        scaleBuffer[i] = serialBuffer[i];
-                    }
-                }  /* weight data processing ends */
-            }     /* end of line data processing ends */
-        }       /* non-empty buffer data processing ends */
-        in_buffer = -1;
-        usleep(1);
-    }
 
-    close(mainfd);
-    exit(EXIT_SUCCESS);
+void start_scanner(void)
+{
+	/* Enable scale, request status fields, give a confirm beep. */
+	write(scannerFD, "S00\r", 5);		/* Hard reset */
+	write(scannerFD, "S01\r", 5);		/* Enable */
+	write(scannerFD, "S03\r", 5);		/* Scanner status */
+	write(scannerFD, "S04\r", 5);		/* Scanner switch read */
+	write(scannerFD, "S13\r", 5);		/* Scale status */
+	write(scannerFD, "S23\r", 5);		/* Display status */
+	write(scannerFD, "S336\r", 6);		/* Send status to host */
+	write(scannerFD, "S334\r", 6);		/* Good beep tone */
+
+	log_message("Scanner started.\n");
+}
+
+
+void stop_scanner(void)
+{
+	write(scannerFD, "S335\r", 6);	     /* Power down */
+	log_message("Scanner stopped.\n");
+}
+
+
+void handle_HUP(int signo /*, siginfo_t *info, void *context */)
+{
+	log_message("Received SIGHUP - rereading config, restarting scanner.\n");
+	stop_scanner();
+	read_config_file(NULL);
+	start_scanner();
+}
+
+
+void handle_TSTP(int signo /*, siginfo_t *info, void *context */)
+{
+	log_message("Received SIGTSTP - stopping scanner.\n");
+	stop_scanner();
+}
+
+
+void handle_CONT(int signo /*, siginfo_t *info, void *context */)
+{
+	log_message("Received SIGCONT - starting scanner.\n");
+	start_scanner();
+}
+
+
+void handle_XFSZ(int signo /*, siginfo_t *info, void *context */)
+{
+	fp_log = fopen(log_file, "w");
+	fprintf(fp_log, "Received XFSZ - restarting log file.\n");
+	fclose(fp_log);
+}
+
+
+int main(int argc, char *argv[])
+{
+	/* Our process ID and Session ID */
+	pid_t pid, sid;
+
+	/* Specify command-line specified config file if possible; otherwise NULL specifies compiled default */
+	read_config_file(argc > 1? argv[1] : NULL);
+
+	/* Fork off the parent process */
+	pid = fork();
+	if (pid < 0) {
+		fprintf(stderr, "Couldn't fork child process.\n");
+		exit(EXIT_FAILURE);
+	}
+	/* If we got a good PID, then
+	   we can exit the parent process. */
+	else if (pid > 0) {
+		exit(EXIT_SUCCESS);
+	}
+
+	/* Change the file mode mask */
+	umask(0);
+
+	/* Open any logs here */
+
+	/* Log our pid */
+	fp_pid = fopen(pid_file, "w");
+	fprintf(fp_pid, "%d", getpid());
+	fclose(fp_pid);
+
+	/* Create a new Session ID for the child process */
+	sid = setsid();
+	if (sid < 0) {
+		log_message("Couldn't create new Session ID.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Change the current working directory */
+/* 	if ((chdir("/")) < 0) {
+ 		log_message("Couldn't change directory.\n");
+ 		exit(EXIT_FAILURE);
+ 	}
+*/
+	/* Set up signal handlers */
+	set_handler(SIGHUP, handle_HUP);
+	set_handler(SIGTSTP, handle_TSTP);
+	set_handler(SIGCONT, handle_CONT);
+	set_handler(SIGXFSZ, handle_XFSZ);
+
+	/* Close out the standard file descriptors */
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+
+	connect_scanner();
+	start_scanner();
+
+	/* Configure port reading */
+	/*fcntl(scannerFD, F_SETFL, FNDELAY);  */
+
+	/* Get the current options for the port */
+	tcgetattr(scannerFD, &options);
+
+	/* Set the baud rates to 9600 */
+	cfsetispeed(&options, B9600);
+
+	/* Enable the receiver and set local mode */
+	options.c_cflag |= (CLOCAL | CREAD);
+
+	/* 7 bits, odd parity */
+	options.c_cflag |= PARENB;	/* enable parity */
+	options.c_cflag |= PARODD;  /* odd parity */
+	options.c_cflag &= ~CSTOPB;	/* disable double stop */
+	options.c_cflag &= ~CSIZE;
+	options.c_cflag |= CS7;
+	options.c_cflag |= CRTSCTS;
+
+	/* Enable data to be processed as raw input */
+	options.c_lflag &= ~(ICANON | ECHO | ISIG);
+
+	/* Set the new options for the port */
+/*  tcsetattr(scannerFD, TCSANOW, &options);  */
+
+	n = 0;
+	num = 0;
+
+	write(scannerFD, "S11\r", 5);
+	write(scannerFD, "S14\r", 5);
+
+	while (1) {
+		in_buffer = read(scannerFD, &chout, 1); /* Read character from ABU */
+		if (in_buffer != -1) {	/* if data is present in the serial port buffer */
+
+			if (chout[0] == 'S') {
+				num = 0;
+			}
+
+			serialBuffer[num] = chout[0];
+			num++;
+
+			if (chout[0] == '\n' && num > 2) {
+				serialBuffer[num] = '\0';
+
+				/**************** process scanned data ****************/
+				if (serialBuffer[1] == '0') {
+					for (i=0; i<17; i++) {
+						scannerInput[i] = serialBuffer[i+4];
+					}
+					fp_scanner = fopen(scanner_file, "w");
+					fprintf(fp_scanner, "%s\n", scannerInput);
+					fclose(fp_scanner);		
+					fp_log = fopen(log_file, "a");
+					fprintf(fp_log, "%d Scanned: %s", q++, serialBuffer);
+					fclose(fp_log);
+				}
+
+				/**************** process weight data ******************/
+				else if (serialBuffer[1] == '1') {
+
+					if (serialBuffer[2] == '1') {
+						write(scannerFD, "S14\r", 5);
+					}
+
+					else if (serialBuffer[2] == '4' && serialBuffer[3] == '3') {
+						write(scannerFD, "S11\r", 5);
+						if (strcmp(scaleBuffer, serialBuffer) != 0) {
+							fp_scale = fopen(scale_file, "w");
+							fprintf(fp_scale, "%s\n", serialBuffer);
+							fclose(fp_scale);		
+							fp_log = fopen(log_file, "a");
+							fprintf(fp_log, "%d Weighed: %s", q++, serialBuffer);
+							fclose(fp_log);
+						}
+					}
+
+					else if (serialBuffer[2] == '4') {
+						write(scannerFD, "S14\r", 5);		
+						if (strcmp(scaleBuffer, serialBuffer) != 0) {
+							fp_scale = fopen(scale_file, "w");
+							fprintf(fp_scale, "%s\n", serialBuffer);
+							fclose(fp_scale);
+							fp_log = fopen(log_file, "a");
+							fprintf(fp_log, "%d Weighed: %s", q++, serialBuffer);
+							fclose(fp_log);
+						}
+					}
+
+					for (i=0; i<10; i++) {
+						scaleBuffer[i] = serialBuffer[i];
+					}	
+
+				}	/* weight data processing ends */
+
+			}	/* end of line data processing ends */
+
+		}	/* non-empty buffer data processing ends */
+
+		in_buffer = -1;
+		usleep(1);
+	}
+
+	close(scannerFD);
+	exit(EXIT_SUCCESS);
 }
