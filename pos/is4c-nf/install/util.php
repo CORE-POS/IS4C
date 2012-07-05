ff04e069975f23ab35d3381510133ccbda6b5ba3
John Leary <john.leary@gmail.com>
2012-07-05 19:18:53 -0400

    feature: settings can now be split between ini.php, ini-local.php

--- pos/is4c-nf/install/util.php
+++ pos/is4c-nf/install/util.php
@@ -1,7 +1,7 @@
 <?php
 
 function confsave($key,$value){
-	if (!is_writable('../ini.php')) return;
+	if (!is_writable('../ini.php') && !is_writable('../ini-local.php')) return;
 
 	$fp = fopen('../ini.php','r');
 	$lines = array();
@@ -23,10 +23,39 @@ function confsave($key,$value){
 	}
 	fclose($fp);
 
-	$fp = fopen('../ini.php','w');
-	foreach($lines as $line)
-		fwrite($fp,$line);
+	if ($found) {
+		$fp = fopen('../ini.php','w');
+		foreach($lines as $line)
+			fwrite($fp,$line);
+		fclose($fp);
+	}
+
+	$fp = fopen('../ini-local.php','r');
+	$lines = array();
+	$found_local = False;
+	while($line = fgets($fp)){
+		if (strpos($line,'$CORE_LOCAL->set("'.$key.'"') === 0){
+			$lines[] = sprintf("\$CORE_LOCAL->set(\"%s\",%s);\n",
+					$key,$value);
+			$found_local = True;
+		}
+		elseif (strpos($line,'?>') === 0 && !$found){
+			$lines[] = sprintf("\$CORE_LOCAL->set(\"%s\",%s);\n",
+					$key,$value);
+			$lines[] = "?>\n";
+		}
+		else{
+			$lines[] = $line;
+		}
+	}
 	fclose($fp);
+
+	if ($found_local) {
+		$fp = fopen('../ini-local.php','w');
+		foreach($lines as $line)
+			fwrite($fp,$line);
+		fclose($fp);
+	}
 }
 
 function loaddata($sql, $table){
