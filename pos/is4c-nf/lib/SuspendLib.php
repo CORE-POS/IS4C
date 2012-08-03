fbf27b2fbdefc2ff8d093a9f877c0a7264050f99
Andy Theuninck <andy@key.wfco-op.store>
2012-08-03 11:31:38 -0500

    BUGFIX: Force cancel on suspended transactions

--- pos/is4c-nf/lib/SuspendLib.php
+++ pos/is4c-nf/lib/SuspendLib.php
@@ -56,6 +56,9 @@ static public function suspendorder() {
 		$result = $db_a->query($query);
 	}
 
+	/* ensure the cancel happens */
+	$cancelR = $db_a->query("UPDATE localtemptrans SET trans_status='X'");
+
 	$CORE_LOCAL->set("plainmsg",_("transaction suspended"));
 	$CORE_LOCAL->set("msg",2);
 	$recall_line = $CORE_LOCAL->get("standalone")." ".$CORE_LOCAL->get("laneno")." ".$cashier_no." ".$trans_no;
