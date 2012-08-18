6df1bc04be5784043916d22330eb383709832323
Eric Lee <ericlee@sympatico.ca>
2012-08-17 20:17:11 -0400

    ssd.c from lane as of 17Aug12

--- pos/is4c-nf/scale-drivers/drivers/rs232/ssd.c
+++ pos/is4c-nf/scale-drivers/drivers/rs232/ssd.c
@@ -74,10 +74,10 @@
 
 #define SSD_SERIAL_PORT "/dev/ttyS0"
 //
-#define SCALE_OUTPUT_FILE "/var/www/IS4C/pos/is4c-nf/scale-drivers/drivers/rs232/scale"
+#define SCALE_OUTPUT_FILE "scale-drivers/drivers/rs232/scale"
 //Restore this if the install page removes the path.
 //define SCALE_OUTPUT_FILE "/var/www/IS4C/pos/is4c-nf/scale-drivers/drivers/rs232/scale"
-#define SCANNER_OUTPUT_FILE "/var/www/IS4C/pos/is4c-nf/scale-drivers/drivers/rs232/scanner"
+#define SCANNER_OUTPUT_FILE "scale-drivers/drivers/rs232/scanner"
 //
 //Restore this if the install page removes the path.
 //define SCANNER_OUTPUT_FILE "/var/www/IS4C/pos/is4c-nf/scale-drivers/drivers/rs232/scanner"
