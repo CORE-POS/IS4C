bc83511c7efecd7d8bbf786f72836bdee2fb0df3
Andy Theuninck <andy@key.wfco-op.store>
2013-01-10 14:24:53 -0600

    Object-based wrapper for reporting
    I'm trying to add some features to a reporting base class
    such that all child reports can inherit the features. Besides
    some standardization the main features added so far are
    jQuery based sorting & styling and result caching.
    
    The department and product movement reports have the
    best commenting.
    
    I realize "classlib2.0" is kind of ridiculous. I'm not entirely
    happy with the module system I was tinkering with in "class-lib"
    and it will go away eventually. In the long run I'd like the
    backend class and plugin system to mimic the lane both for the
    sake of consistency and because I think the lane model is
    better.

---
+++ fannie/classlib2.0/FannieReportPage.php
@@ -0,0 +1,402 @@
+<?php
+/*******************************************************************************
+
+    Copyright 2012 Whole Foods Co-op
+
+    This file is part of Fannie.
+
+    IT CORE is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    IT CORE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    in the file license.txt along with IT CORE; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*********************************************************************************/
+
+include_once(dirname(__FILE__).'/FanniePage.php');
+
+/**
+  @class FanniePage
+  Class for drawing screens
+*/
+class FannieReportPage extends FanniePage {
+
+	public $required = True;
+
+	public $description = "
+	Base class for creating reports.
+	";
+
+	/**
+	  Function for drawing page content.
+	  form_content and report_content are provided
+	  by default.
+	*/
+	protected $content_function = "form_content";
+
+	/**
+	  Define report headers. Headers are necessary if sorting is desired
+	*/
+	protected $report_headers = array();
+
+	/**
+	  Define report format. Valid values are: html, xls, csv
+	*/
+	protected $report_format = 'html';
+
+	/**
+	  Enable caching of SQL data. Valid values are: none, day, month
+	*/
+	protected $report_cache = 'none';
+
+	/**
+	  Which column to sort by default
+	*/
+	protected $sort_column = 0;
+
+	/**
+	  Sort direction. 0 is ascending, 1 is descending
+	*/
+	protected $sort_direction = 0;
+
+	/**
+	  Handle pre-display tasks such as input processing
+	  @return
+	   - True if the page should be displayed
+	   - False to stop here
+
+	  Typically in a report this checks for posted data
+	  and decides between showing a data entry form
+	  or the report results.  
+	*/
+	function preprocess(){
+		return True;
+	}
+	
+	/**
+	  Define the data input form
+	  @return An HTML string
+	*/
+	function form_content(){
+	
+	}
+
+	/**
+	  Define the report display
+	  @return An HTML string
+
+	  Generally this function is not overriden.
+
+	  This will first check the cache to see
+	  if data for this report has been saved. If not,
+	  it will look up the data by calling the
+	  fetch_report_data function. That function should
+	  be overriden.
+
+	  Once the data is retrieved, this will call
+	  the calculate_footers function on the data. 
+	  Footers are not required, but it's useful for some
+	  final calculations. 
+
+	  Finally, the render_data function is called. Overriding
+	  that is not recommended.	
+	*/
+	function report_content(){
+		$data = array();
+		$cached = $this->check_data_cache();
+		if ($cached !== False){
+			$data = unserialize(gzuncompress($cached));
+			if ($data === False)
+				$data = $this->fetch_report_data();
+		}
+		else {
+			$data = $this->fetch_report_data();
+			$this->freshen_cache($data);
+		}
+		$footers = $this->calculate_footers($data);
+		$output = $this->render_data($data,$this->report_headers,
+				$footers,$this->report_format);
+		echo $output;
+	}
+
+	/**
+	  Calculate a footer row
+	  @param $data an two-dimensional array of data
+	  @return array of footer values
+
+	  Principally, footers are separate from data
+	  so they can be marked in such in HTML rendering
+	  and stay at the bottom when data sorting changes.
+
+	  This function may also be used to set values
+	  for headers or default sorting. On more elaborate reports,
+	  the number of columns may vary depending on what options
+	  are selected. This function is always called so those values
+	  will be set reliably even if caching is enabled.
+	*/
+	function calculate_footers($data){
+		return array();
+	}
+
+	/**
+	  Look for cached SQL data
+	
+	  Data is stored in the archive database, reportDataCache table.
+
+	  The key column is an MD5 hash of the current URL (minus the excel
+	  parameter, if present). This means your forms should use type GET
+	  if caching is enabled.
+
+	  The data is stored as a serialized, gzcompressed string.
+	*/
+	function check_data_cache(){
+		global $dbc,$FANNIE_ARCHIVE_DB;
+		if ($this->report_cache != 'day' && $this->report_cache != 'month')
+			return False;
+		$table = $FANNIE_ARCHIVE_DB.$dbc->sep()."reportDataCache";
+		$hash = $_SERVER['REQUEST_URI'];
+		$hash = str_replace("&excel=xls","",$hash);
+		$hash = str_replace("&excel=csv","",$hash);
+		$query = sprintf("SELECT report_data FROM %s WHERE
+			hash_key=%s AND expires >= %s",
+			$table, $dbc->escape(md5($hash)), $dbc->now());
+		$result = $dbc->query($query);
+		if ($dbc->num_rows($result) > 0)
+			return array_pop($dbc->fetch_row($result));
+		else
+			return False;
+	}
+
+	/**
+	  Store SQL data in the cache
+	  @param $data the data
+	  @return True or False based on success
+
+	  See check_data_cache for details
+	*/
+	function freshen_cache($data){
+		global $dbc,$FANNIE_ARCHIVE_DB;
+		if ($this->report_cache != 'day' && $this->report_cache != 'month')
+			return False;
+		$table = $FANNIE_ARCHIVE_DB.$dbc->sep()."reportDataCache";
+		$hash = $_SERVER['REQUEST_URI'];
+		$hash = str_replace("&excel=xls","",$hash);
+		$hash = str_replace("&excel=csv","",$hash);
+		$expires = '';
+		if ($this->report_cache == 'day')
+			$expires = date('Y-m-d',mktime(0,0,0,date('n'),date('j')+1,date('Y')));
+		elseif ($this->report_cache == 'month')
+			$expires = date('Y-m-d',mktime(0,0,0,date('n')+1,date('j'),date('Y')));
+
+		$delQ = sprintf("DELETE FROM $table WHERE hash_key=%s",
+			$dbc->escape(md5($hash)));
+		$dbc->query($delQ);
+		$upQ = sprintf("INSERT INTO $table (hash_key, report_data, expires)
+			VALUES (%s,%s,%s)",$dbc->escape(md5($hash)),
+			$dbc->escape(gzcompress(serialize($data))), $dbc->escape($expires));
+		$dbc->query($upQ);
+		return True;
+	}
+
+	/**
+	  Get the report data
+	  @return a two dimensional array
+
+	  Actual SQL queries go here!
+	*/
+	function fetch_report_data(){
+
+	}
+
+	/**
+	  Format data for display
+	  @param $data a two dimensional array of data
+	  @param $headers a header row (optional)
+	  @param $format output format (html | xls | csv)
+	  @return formatted string
+	*/
+	function render_data($data,$headers=array(),$footers=array(),$format='html'){
+		global $FANNIE_URL,$FANNIE_ROOT;
+		$ret = "";
+		switch(strtolower($format)){
+		case 'html':
+			$this->add_css_file($FANNIE_URL.'src/jquery/themes/blue/style.css');
+			$ret .= sprintf('<html><head></head><body>
+				<a href="%s&excel=xls">Download XLS</a>
+				&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="%s&excel=csv">Download CSV</a>
+				<table id="mySortableTable" cellspacing="0" 
+				cellpadding="4" border="1" class="tablesorter">',
+				$_SERVER['REQUEST_URI'],$_SERVER['REQUEST_URI']);
+			break;
+		case 'csv':
+		case 'xls':
+			break;
+		}
+
+		if (!empty($headers)){
+			switch(strtolower($format)){
+			case 'html':
+				$ret .= '<thead>';
+				$ret .= $this->html_line($headers, True);
+				$ret .= '</thead>';
+				break;
+			case 'csv':
+				$ret .= $this->csv_line($headers);
+				break;
+			case 'xls':
+				break;
+			}
+		}
+
+		for ($i=0;$i<count($data);$i++){
+			switch(strtolower($format)){
+			case 'html':
+				if ($i==0) $ret .= '<tbody>';
+				$ret .= $this->html_line($data[$i]);
+				if ($i==count($data)-1) $ret .= '</tbody>';
+				break;
+			case 'csv':
+				$ret .= $this->csv_line($data[$i]);
+				break;
+			case 'xls':
+				break;
+			}
+		}
+
+		if (!empty($footers)){
+			switch(strtolower($format)){
+			case 'html':
+				$ret .= '<tfoot>';
+				$ret .= $this->html_line($footers, True);
+				$ret .= '</tfoot>';
+				break;
+			case 'csv':
+				$ret .= $this->csv_line($data[$i]);
+				break;
+			case 'xls':
+				break;
+			}
+		}
+
+		switch(strtolower($format)){
+		case 'html':
+			$ret .= '</table></body></html>';
+			$this->add_script($FANNIE_URL.'src/jquery/js/jquery.js');
+			$this->add_script($FANNIE_URL.'src/jquery/jquery.tablesorter.js');
+			$sort = sprintf('[[%d,%d]]',$this->sort_column,$this->sort_direction);
+			$this->add_onload_command("\$('#mySortableTable').tablesorter({sortList: $sort});");
+			break;
+		case 'csv':
+			header('Content-Type: application/ms-excel');
+			header('Content-Disposition: attachment; filename="'.$this->header.'.csv"');
+			break;
+		case 'xls':
+			$xlsdata = $data;
+			if (!empty($headers)) array_unshift($xlsdata,$headers);
+			if (!empty($footers)) array_push($xlsdata,$footers);
+			if (!function_exists('ArrayToXls'))
+				include_once($FANNIE_ROOT.'src/ReportConvert/ArrayToXls.php');
+			$ret = ArrayToXls($xlsdata);
+			header('Content-Type: application/ms-excel');
+			header('Content-Disposition: attachment; filename="'.$this->header.'.xls"');
+			break;
+		}
+
+		return $ret;
+	}
+
+	/**
+	  Turn array into HTML table row
+	  @param $row an array of data
+	  @param $header True means <th> tags, False means <td> tags
+	  @return HTML string
+
+	  Javascript sorting utility requires header rows to be <th> tags
+	*/
+	function html_line($row, $header=False){
+		$ret = "<tr>";
+		$tag = $header ? 'th' : 'td';
+		for($i=0;$i<count($row);$i){
+			$span = 1;
+			while(isset($row[$i+$span]) && $row[$i+$span] === null && ($i+$span)<count($row)){
+				$span++;
+			}
+			if ($row[$i] === "") $row[$i] = '&nbsp;';
+			$ret .= '<'.$tag.' colspan="'.$span.'">'.$row[$i].'</'.$tag.'>';
+			$i += $span;
+		}
+		return $ret.'</tr>';;
+	}
+
+	/**
+	  Turn array into CSV line
+	  @param $row an array of data
+	  @return CSV string
+	*/
+	function csv_line($row){
+		$ret = "";
+		foreach($row as $item){
+			$item = str_replace('"','',$item);
+			$ret .= '"'.$item.'",';
+		}
+		$ret = substr($ret,0,strlen($ret)-1)."\r\n";
+		return $ret;
+	}
+
+	/**
+	  Check for input and display the page
+	*/
+	function draw_page(){
+		if ($this->preprocess()){
+			
+			if ($this->window_dressing)
+				echo $this->get_header();
+
+			$fn = $this->content_function;
+			echo $this->$fn();
+
+			if ($this->window_dressing)
+				echo $this->get_footer();
+
+			foreach($this->scripts as $s_url => $s_type){
+				printf('<script type="%s" src="%s"></script>',
+					$s_type, $s_url);
+				echo "\n";
+			}
+			
+			$js_content = $this->javascript_content();
+			if (!empty($js_content) || !empty($this->onload_commands)){
+				echo '<script type="text/javascript">';
+				echo $js_content;
+				echo "\n\$(document).ready(function(){\n";
+				foreach($this->onload_commands as $oc)
+					echo $oc."\n";
+				echo "});\n";
+				echo '</script>';
+			}
+
+			$page_css = $this->css_content();
+			if (!empty($page_css)){
+				echo '<style type="text/css">';
+				echo $page_css;
+				echo '</style>';
+			}
+			foreach($this->css_files as $css_url){
+				printf('<link rel="stylesheet" type="text/css" href="%s">',
+					$css_url);
+				echo "\n";
+			}
+		}
+	}
+}
+
+?>
