693e574430faf6260854c81833c152e31bc4c7d6
Andy Theuninck <andy@gohanman.com>
2013-03-12 14:16:45 -0500

    Batch related pages that use API classes (except main batch editor)

---
+++ fannie/batches/BatchIndexPage.php
@@ -0,0 +1,57 @@
+<?php
+/*******************************************************************************
+
+    Copyright 2011 Whole Foods Co-op
+
+    This file is part of Fannie.
+
+    IT CORE is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    IT CORE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    in the file license.txt along with IT CORE; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*********************************************************************************/
+include('../config.php');
+include($FANNIE_ROOT.'classlib2.0/FanniePage.php');
+
+class BatchIndexPage extends FanniePage {
+	protected $title = 'Fannie - Batch Module';
+	protected $header = 'Sales Batches';
+
+	function body_content(){
+		ob_start();
+		?>
+		<ul>
+			<li><a href="newbatch/">Sales Batches</a> is a tool to create
+				batches manually one item at a time.</li>
+			<li><a href="xlsbatch/">Upload Batch</a> is a tool to create
+				a batch from a spreadsheet.</li>
+			<li><a href="BatchTypeEditor.php">Manage Batch Types</a> adds, removes, or
+				adjusts batch types</li>
+			<li><a href="CAP/">Co+op Deals</a> imports the Co+op Deals pricing
+				spreadsheet, determines where sale items exist in POS,
+				and creates appropriate sales batches.</li>
+			<li><a href="UNFI/">Vendor Pricing</a> imports cost information
+				from vendor spreadsheets, calculates SRPs based on desired
+				margins, and creates price change batches to apply new
+				SRPs.</li>
+		</ul>
+		<?php
+		return ob_get_clean();
+	}
+}
+
+if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)){
+	$obj = new BatchIndexPage();
+	$obj->draw_page();
+}
+?>
--- fannie/src/defaultmenu.php
+++ fannie/src/defaultmenu.php
@@ -76,7 +76,8 @@ array('label'=>'Item Maintenance','url'=>'item/ItemEditorPage.php','subheading'=
 array('label'=>'Sales Batches','url'=>'batches/','subheading'=>'Create automated sales & price changes',
 'submenu'=>array(
 	array('label'=>'Sales Batches','url'=>'batches/newbatch/'),
-	array('label'=>'Manage Batch Types','url'=>'batches/types.php'),
+	array('label'=>'Upload Batch','url'=>'batches/xlsbatch/'),
+	array('label'=>'Manage Batch Types','url'=>'batches/BatchTypeEditor.php'),
 	array('label'=>'Co+op Deals Sales','url'=>'batches/CAP/'),
 	array('label'=>'Vendor Pricing','url'=>'batches/UNFI/')
 	)
