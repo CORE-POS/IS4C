f0073cf885a560c98fa5d8d487826d8664d732b1
Andy Theuninck <andy@gohanman.com>
2013-03-14 11:52:29 -0500

    API class updates for shelftags, misc fixes in new OO pages

--- fannie/item/departments/DepartmentIndexPage.php
+++ fannie/item/departments/DepartmentIndexPage.php
@@ -1,7 +1,7 @@
 <?php
 /*******************************************************************************
 
-    Copyright 2009 Whole Foods Co-op
+    Copyright 2009,2013 Whole Foods Co-op
 
     This file is part of Fannie.
 
@@ -20,5 +20,29 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 *********************************************************************************/
-header('Location: ShelfTagIndex.php');
+
+include('../../config.php');
+include($FANNIE_ROOT.'classlib2.0/FanniePage.php');
+
+class DepartmentIndexPage extends FanniePage {
+	protected $title = "Fannie : Manage Departments";
+	protected $header = "Manage Departments";
+	
+	function body_content(){
+		ob_start();
+		?>
+		<ul>
+		<li> <a href="SuperDeptEditor.php">Super Departments</a></li>
+		<li> <a href="DepartmentEditor.php">Departments</a></li>
+		<li> <a href="SubDeptEditor.php">Sub Departments</a></li>
+		</ul>
+		<?php
+		return ob_get_clean();
+	}
+}
+
+if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)){
+	$obj = new DepartmentIndexPage();
+	$obj->draw_page();
+}
 ?>
