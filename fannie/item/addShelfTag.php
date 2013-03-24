9bf0d987bc7b02b51f6b96d21bbf554c8cd34dbb
Eric Lee <ericlee@sympatico.ca>
2013-03-23 23:55:52 -0400

    Shelftag #1

--- fannie/item/addShelfTag.php
+++ fannie/item/addShelfTag.php
@@ -21,24 +21,43 @@
 
 *********************************************************************************/
 
+/* --COMMENTS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+	* 21Mar2013 EL Hacked FANNIE_POUNDS_AS_POUNDS until established.
+	*              Use input description width 30, not 27, OK per AT.
+	* 16Mar2013 Eric Lee Need to get the vendor name either from the form
+	*            or from, ideally, vendors, or prodExtra.
+	*            Currently the vendor name input is just text, not controlled.
+	*           It would be better if it used size and unitofmeasure from the form.
+	*            In update, would need a post-update shelftag create as in insertItem.php
+
+*/
+
 require('pricePerOunce.php');
 
 $upc=str_pad($_GET['upc'],13,0,STR_PAD_LEFT);
 
 require('../config.php');
 require_once($FANNIE_ROOT.'src/mysql_connect.php');
+$FANNIE_POUNDS_AS_POUNDS = 1;
+if ( isset($FANNIE_COUNTRY) && $FANNIE_COUNTRY != 'US' ) {
+	require('pricePerSI.php');
+}
 
+// EL 16Mar13 Get vendorItem and vendor data for the item being edited or that was just created.
+// This favours UNFI which traditionally has vendorID 1.
+//was: $unfiQ = "SELECT DISTINCT * FROM vendorItems WHERE upc = '$upc' ORDER BY vendorID";
+$vendiQ = "SELECT DISTINCT vi.*, v.vendorID vid, v.vendorName
+	FROM vendorItems AS vi
+		LEFT JOIN vendors AS v ON v.vendorID=vi.vendorID
+	WHERE vi.upc = '$upc'
+	ORDER BY vi.vendorID";
+$vendiR = $dbc->query($vendiQ);
+$vendiN = $dbc->num_rows($vendiR);
 
-$unfiQ = "SELECT DISTINCT * FROM vendorItems where upc = '$upc' ORDER BY vendorID";
-//echo $unfiQ;
-
-$unfiR = $dbc->query($unfiQ);
-$unfiN = $dbc->num_rows($unfiR);
-
-$prodQ = "SELECT p.*,s.superID FROM products AS p
+$prodQ = "SELECT p.*, s.superID FROM products AS p
 	LEFT JOIN MasterSuperDepts AS s ON p.department=s.dept_ID
 	where upc='$upc'";
-//echo $prodQ;
 $prodR = $dbc->query($prodQ);
 $prodW = $dbc->fetch_array($prodR);
 $price = $prodW['normal_price'];
@@ -51,34 +70,53 @@ $vendor = '';
 $ppo = '';
 $superID = $prodW['superID'];
 
-echo "New Shelf Tag:<br> " . $upc;
-$prodExtraN = 0;
-if($unfiN == 1){
-   $unfiW = $dbc->fetch_array($unfiR);
-   $size = $unfiW['size'];
-   $brand = $unfiW['brand'];
-   $units = $unfiW['units'];
-   $sku = $unfiW['sku'];
-   $vendor = 'UNFI';
-   $ppo = pricePerOunce($price,$size);
+if($vendiN > 0){
+ // Use only the first hit.
+ $vendiW = $dbc->fetch_array($vendiR);
+ // Composed: "200 g"
+ $size = $vendiW['size'];
+ $brand = $vendiW['brand'];
+ $units = $vendiW['units'];
+ $sku = $vendiW['sku'];
+ if ( $vendiW['vendorName'] != "" ) {
+	 $vendor = $vendiW['vendorName'];
+ } else {
+	if ($dbc->table_exists('prodExtra')) {
+		$prodExtraQ = "select distributor from prodExtra where upc='$upc'";
+		$prodExtraR = $dbc->query($prodExtraQ);
+		$prodExtraN = $dbc->num_rows($prodExtraR);
+		if ($prodExtraN > 0){
+			$prodExtraW = $dbc->fetch_array($prodExtraR);
+			$vendor = $prodExtraW['distributor'];
+		}
+	}
+ }
+ if ( isset($FANNIE_COUNTRY) && $FANNIE_COUNTRY != 'US' ) {
+	 $ppo = pricePerSI($price,$size, $prodW['size'], $prodW['unitofmeasure'], 'SI');
+ } else {
+	 $ppo = pricePerOunce($price,$size);
+ }
 }
-else if ($dbc->table_exists('prodExtra')) {
-	$prodExtraQ = "select manufacturer,distributor from prodExtra where upc='$upc'";
-	$prodExtraR = $dbc->query($prodExtraQ);
-	$prodExtraN = $dbc->num_rows($prodExtraR);
-	if ($prodExtraN == 1){
-		$prodExtraW = $dbc->fetch_array($prodExtraR);
-		$brand = $prodExtraW[0];
-		$vendor = $prodExtraW[1];
+else {
+	if ($dbc->table_exists('prodExtra')) {
+		$prodExtraQ = "select manufacturer,distributor from prodExtra where upc='$upc'";
+		$prodExtraR = $dbc->query($prodExtraQ);
+		$prodExtraN = $dbc->num_rows($prodExtraR);
+		if ($prodExtraN == 1){
+			$prodExtraW = $dbc->fetch_array($prodExtraR);
+			$brand = $prodExtraW['manufacturer'];
+			$vendor = $prodExtraW['distributor'];
+		}
 	}
 }
 
+echo "<body bgcolor='ffffcc'>";
+echo "New Shelf Tag:<!-- br / --> " . $upc;
 ?>
-<body bgcolor='ffffcc'>
 <form method='post' action='addShelfTag1.php'>
 <input type='hidden' name=upc value='<?php echo $upc; ?>'>
 <font color='blue'>Description</font>
-<input type='text' name='description' size=27 maxlength=27
+<input type='text' name='description' size=30 maxlength=30
 <?php
 echo "value='".strtoupper($desc)."'";
 ?>
@@ -110,13 +148,13 @@ SKU: <input type='text' name='sku' size=8
 echo "value='".$sku."'";
 ?>
 >
-Price: <font color='green' size=+1><b><?php echo $price; ?><input type='hidden' name='price' size=8 value=<?php echo $price; ?> ></b></font>
+Price: <font color='green' size=+1><b><?php echo printf("%.2f",$price); ?><input type='hidden' name='price' size=8 value=<?php echo $price; ?> ></b></font>
 <?php 
 
 echo "<input type='submit' value='New' name='submit'>";
 
 ?>
-Barcode page: <select name=subID>
+<br />Barcode page: <select name=subID>
 <?php
 $subsQ = "SELECT superID,super_name FROM superDeptNames";
 $subsR = $dbc->query($subsQ);
