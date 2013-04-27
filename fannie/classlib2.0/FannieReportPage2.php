0b9288101927f578e8289f471ba0a707ea0f5b55
Eric Lee <ericlee@sympatico.ca>
2013-04-27 02:06:45 -0400

    StoreSummary v1

--- fannie/classlib2.0/FannieReportPage2.php
+++ fannie/classlib2.0/FannieReportPage2.php
@@ -21,73 +21,95 @@
 
 *********************************************************************************/
 
-include_once(dirname(__FILE__).'/FanniePage.php');
+/* --COMMENTS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	* 26Apr13 Eric Lee
+	* If this is extended for a multi_report_format page in not-sortable mode:
+	* * Format the report in a single table
+	* * Support an optional summary report at the end with
+	*   * Different headers
+	* Right-Align text in columns of numbers.
+	* Use a .css
+	* To do:
+	* * Add summary to xls and csv outputs
+	* * Remove the parts that do not override the base page.
+*/
+
+//include_once(dirname(__FILE__).'/FanniePage.php');
+include_once(dirname(__FILE__).'/FannieReportPage.php');
 
 /**
   @class FannieReportPage2
   Class for drawing screens
 */
-class FannieReportPage2 extends FanniePage {
+class FannieReportPage2 extends FannieReportPage {
 
 	public $required = True;
 
 	public $description = "
 	Base class for creating reports.
+	Formats non-sortable multi-section report as single table.
 	";
 
 	/**
 	  Function for drawing page content.
 	  form_content and report_content are provided
 	  by default.
-	*/
 	protected $content_function = "form_content";
+	*/
 
 	/**
 	  Define report headers. Headers are necessary if sorting is desired
-	*/
 	protected $report_headers = array();
+	*/
 
 	/**
 	  End-of report summary(ies), with integrated headers. (optional)
 		Two-dimensional, as for body data.
 		Same number of columns as body data.
-		EL+
 	*/
+	// 3New
 	protected $summary_data = array();
 	protected $summary_counter = 0;
 	protected $number_of_summaries = 0;
 
 	/**
 	  Define report format. Valid values are: html, xls, csv
-	*/
 	protected $report_format = 'html';
+	*/
 
 	/**
 	  Enable caching of SQL data. Valid values are: none, day, month
-	*/
 	protected $report_cache = 'none';
+	*/
 
 	/**
 	  Allow for reports that contain multiple separate tables of data
-	*/
 	protected $multi_report_mode = False;
 	protected $multi_counter = 1;
+	*/
+	// 1New
 	protected $number_of_reports = 0;
 
 	/**
 	  Option to enable/disable javascript sorting
-	*/
 	protected $sortable = True;
+	*/
 
 	/**
 	  Which column to sort by default
-	*/
 	protected $sort_column = 0;
+	*/
 
 	/**
 	  Sort direction. 0 is ascending, 1 is descending
-	*/
 	protected $sort_direction = 0;
+	*/
+
+	/**
+	  Alignment of table cell text. Valid values are: '' 'left' 'right'
+	*/
+	// 1New
+	protected $cellTextAlign = '';
 
 	/**
 	  Handle pre-display tasks such as input processing
@@ -98,38 +120,44 @@ class FannieReportPage2 extends FanniePage {
 	  Typically in a report this checks for posted data
 	  and decides between showing a data entry form
 	  or the report results.  
-	*/
 	function preprocess(){
 		return True;
 	}
+	No change.
+	*/
 	
 	/**
 	  Define the data input form
 	  @return An HTML string
-	*/
 	function form_content(){
 	
 	}
+	No change.
+	*/
 
 	/**
-	  Define the report display
-	  @return An HTML string
+	  Define the report display,
+		 all the reports and summaries on the page
+	  @return An HTML string (echoes, doesn't return)
 
 	  Generally this function is not overriden.
 
 	  This will first check the cache to see
-	  if data for this report has been saved. If not,
-	  it will look up the data by calling the
-	  fetch_report_data function. That function should
-	  be overriden.
+	  if data for this report has been saved.
+		If not, it will look up the data by calling the
+	  fetch_report_data function.
+		That function should be overriden.
+		fetch_report_data can optionally return summary
+		report data in separate array.
 
 	  Once the data is retrieved, this will call
 	  the calculate_footers function on the data. 
 	  Footers are not required, but it's useful for some
 	  final calculations. 
 
-	  Finally, the render_data function is called. Overriding
-	  that is not recommended.	
+	  Finally, the render_data function is called.
+		Overriding that is not recommended.	
+	Changed.
 	*/
 	function report_content(){
 		$data = array();
@@ -146,28 +174,39 @@ class FannieReportPage2 extends FanniePage {
 		$output = '';
 		if ($this->multi_report_mode && $this->report_format != 'xls'){
 			$this->number_of_reports = count($data);
+			$this->number_of_summaries = count($this->summary_data);
 			foreach($data as $report_data){
 				$footers = $this->calculate_footers($report_data);
-				$output .= $this->render_data($report_data,$this->report_headers,
-						$footers,$this->report_format);
+				$output .= $this->render_data($report_data,
+						$this->report_headers,
+						$footers,
+						$this->report_format);
 				if ($this->sortable)
 					$output .= '<br />';
 			}
 			// A summary or grand total of the report
-			// First row (of each) contains headers.
-			if ( True && !empty($this->summary_data) ) {
-				$this->number_of_summaries = count($this->summary_data);
+			// First row (of each) tested to see if seems like headers.
+			if ( !empty($this->summary_data) ) {
 				foreach($this->summary_data as $report_data){
 					$this->summary_counter++;
-					// Could move $report_data[0] to headers if all non-numbers.
-					// Maybe not always wanted.
 					$headers = array();
+					// Move $report_data[0] to headers if all non-numbers.
+					for($i=0;$i<count($report_data[0]);$i++){
+						if ( $report_data[0][$i] != '' && preg_match("/^[0-9., $%]+$/",$report_data[0][$i]) ) {
+							break;
+						}
+					}
+					if ( $i >= count($report_data[0]) )
+						$headers = array_shift($report_data);
 					$footers = array();
-					$output .= $this->render_data($report_data,$headers,
-							$footers,$this->report_format);
-					//->EL Better with margin-bottom
-					if ($this->sortable)
+					$output .= $this->render_data($report_data,
+							$headers,
+							$footers,
+							$this->report_format);
+					/* Better with CSS
+					if (False && $this->sortable)
 						$output .= '<br />';
+					*/
 				}
 			}
 		}
@@ -192,6 +231,8 @@ class FannieReportPage2 extends FanniePage {
 					$footers,$this->report_format);
 		}
 		echo $output;
+
+	// report_content()
 	}
 
 	/**
@@ -208,10 +249,11 @@ class FannieReportPage2 extends FanniePage {
 	  the number of columns may vary depending on what options
 	  are selected. This function is always called so those values
 	  will be set reliably even if caching is enabled.
-	*/
 	function calculate_footers($data){
 		return array();
 	}
+	No change.
+	*/
 
 	/**
 	  Look for cached SQL data
@@ -223,7 +265,6 @@ class FannieReportPage2 extends FanniePage {
 	  if caching is enabled.
 
 	  The data is stored as a serialized, gzcompressed string.
-	*/
 	function check_data_cache(){
 		global $dbc,$FANNIE_ARCHIVE_DB;
 		if ($this->report_cache != 'day' && $this->report_cache != 'month')
@@ -241,6 +282,8 @@ class FannieReportPage2 extends FanniePage {
 		else
 			return False;
 	}
+	No change.
+	*/
 
 	/**
 	  Store SQL data in the cache
@@ -248,7 +291,6 @@ class FannieReportPage2 extends FanniePage {
 	  @return True or False based on success
 
 	  See check_data_cache for details
-	*/
 	function freshen_cache($data){
 		global $dbc,$FANNIE_ARCHIVE_DB;
 		if ($this->report_cache != 'day' && $this->report_cache != 'month')
@@ -271,15 +313,19 @@ class FannieReportPage2 extends FanniePage {
 		$dbc->exec_statement($upQ, array($hash, gzcompress(serialize($data)), $expires));
 		return True;
 	}
+	No change.
+	*/
 
 	/**
 	  Extra, non-tabular information prepended to
 	  reports
 	  @return array of strings
-	*/
+
 	function report_description_content(){
 		return array();
 	}
+	No change.
+	*/
 
 	/**
 	  Get the report data
@@ -291,18 +337,21 @@ class FannieReportPage2 extends FanniePage {
 	  return an array of two dimensional arrays
 	  where each two dimensional arrays contains
 	  a report's data.
-	*/
+
 	function fetch_report_data(){
 
 	}
+	No change.
+	*/
 
 	/**
-	  Format data for display
+	  Format data for one report for display
 	  @param $data a two dimensional array of data
 	  @param $headers a header row (optional)
 	  @param $footers a column-totals row (optional)
 	  @param $format output format (html | xls | csv)
 	  @return formatted string
+	Changed.
 	*/
 	function render_data($data,$headers=array(),$footers=array(),$format='html'){
 		global $FANNIE_URL,$FANNIE_ROOT;
@@ -314,19 +363,22 @@ class FannieReportPage2 extends FanniePage {
 				$this->add_css_file($FANNIE_URL.'src/css/reports.css');
 				if ( !$this->window_dressing )
 					$ret .= '<html><head></head><body>';
-				$ret .= sprintf('<a href="%s%sexcel=xls">Download Excel</a>
-					&nbsp;&nbsp;&nbsp;&nbsp;
-					<a href="%s%sexcel=csv">Download CSV</a>',
+				$ret .= sprintf(
+				'<a href="%s%sexcel=xls">Download Excel</a>',
 					$_SERVER['REQUEST_URI'],
-					(strstr($_SERVER['REQUEST_URI'],'?') ===False ? '?' : '&'),
+					(strstr($_SERVER['REQUEST_URI'],'?') === False ? '?' : '&'));
+				$ret .= '	&nbsp;&nbsp;&nbsp;&nbsp ';
+				$ret .= sprintf('<a href="%s%sexcel=csv">Download CSV</a>',
 					$_SERVER['REQUEST_URI'],
-					(strstr($_SERVER['REQUEST_URI'],'?') ===False ? '?' : '&')
-				);
+					(strstr($_SERVER['REQUEST_URI'],'?') === False ? '?' : '&'));
+				$ret .= '	&nbsp;&nbsp;&nbsp;&nbsp ';
+				$ret .= '<a href="javascript:history:back();">Back</a>';
 				foreach($this->report_description_content() as $line)
 					$ret .= (substr($line,0,1) == '<')?$line:'<br />'.$line;
 			}
 			$class = 'mySortableTable';
-			if ($this->sortable) $class .= ' tablesorter';
+			if ($this->sortable)
+				$class .= ' tablesorter fancytable';
 			if ($this->sortable || $this->multi_counter == 1)
 				$ret .= '<table class="'.$class.'" cellspacing="0" 
 					cellpadding="4" border="1">';
@@ -356,11 +408,21 @@ class FannieReportPage2 extends FanniePage {
 		for ($i=0;$i<count($data);$i++){
 			switch(strtolower($format)){
 			case 'html':
-				// ->ELif not sortable then only before the first section
-				if ($i==0) $ret .= '<tbody>';
+				if ($i==0) {
+					if ($this->sortable || $this->multi_counter == 1) {
+						$ret .= '<tbody>';
+					}
+				}
 				$ret .= $this->html_line($data[$i]);
-				// ->ELif not sortable then only at the end of the last section
-				if ($i==count($data)-1) $ret .= '</tbody>';
+				if ($i==count($data)-1) {
+					if ( $this->sortable ||
+								!$this->multi_report_mode ||
+								($this->multi_counter >= $this->number_of_reports &&
+									$this->summary_counter == $this->number_of_summaries)
+						) {
+						$ret .= '</tbody>';
+					}
+				}
 				break;
 			case 'csv':
 				$ret .= $this->csv_line($data[$i]);
@@ -376,13 +438,14 @@ class FannieReportPage2 extends FanniePage {
 			case 'html':
 				if ($this->sortable) {
 					$ret .= '<tfoot>';
-					$ret .= $this->html_line($footers, True);
+					$ret .= $this->html_line($footers, False, True);
 					$ret .= '</tfoot>';
 				}
 				else {
-					$ret .= $this->html_line($footers, True);
-					if ( $this->multi_counter < ($this->number_of_reports + count($this->summary_data)) )
-						$ret .= "<tr><td colspan=0> &nbsp; </td></tr>";
+					$ret .= $this->html_line($footers, False, True);
+					if ( $this->multi_counter < ($this->number_of_reports + count($this->summary_data)) ) {
+						$ret .= "<tr><td colspan='99'> &nbsp; </td></tr>";
+					}
 				}
 				break;
 			case 'csv':
@@ -398,10 +461,9 @@ class FannieReportPage2 extends FanniePage {
 			if ( $this->sortable ||
 					($this->multi_counter == $this->number_of_reports && empty($this->summary_data)) ||
 					($this->multi_counter >= $this->number_of_reports && $this->summary_counter == $this->number_of_summaries)
-					) 
-				$ret .= "</table> <!-- mc: {$this->multi_counter} nor: {$this->number_of_reports} sc: {$this->summary_counter} nos: {$this->number_of_summaries} -->";
-				// <!-- mc: 5 nor: 4 sc: 1 nos: 1 -->  appeared before summary
-				// <!-- mc: 4 nor: 4 sc: 0 nos: 0 -->  correct, after last main when summaries defeated.
+					) {
+				$ret .= '</table>';
+				}
 
 			$this->add_script($FANNIE_URL.'src/jquery/js/jquery.js');
 			$this->add_script($FANNIE_URL.'src/jquery/jquery.tablesorter.js');
@@ -436,7 +498,7 @@ class FannieReportPage2 extends FanniePage {
 	/**
 	   Convert keyed array to numerical
 	   indexes and maintain order
-	*/
+
 	function dekey_array($arr){
 		$ret = array();
 		foreach($arr as $outer_key => $row){
@@ -447,6 +509,8 @@ class FannieReportPage2 extends FanniePage {
 		}
 		return $ret;
 	}
+	No change.
+	*/
 
 	/**
 	  Turn array into HTML table row
@@ -455,8 +519,9 @@ class FannieReportPage2 extends FanniePage {
 	  @return HTML string
 
 	  Javascript sorting utility requires header rows to be <th> tags
+	Changed.
 	*/
-	function html_line($row, $header=False){
+	function html_line($row, $header=False, $footer=False){
 		global $FANNIE_URL;
 		$ret = "<tr>";
 		$tag = $header ? 'th' : 'td';
@@ -465,7 +530,10 @@ class FannieReportPage2 extends FanniePage {
 			while(isset($row[$i+$span]) && $row[$i+$span] === null && ($i+$span)<count($row)){
 				$span++;
 			}
-			$textAlign = '';
+			if ($header)
+				$textAlign = '';
+			else
+				$textAlign = $this->cellTextAlign;
 			if ($row[$i] === "" || $row[$i] === null) $row[$i] = '&nbsp;';
 			elseif (is_numeric($row[$i]) && strlen($row[$i]) == 13){
 				// auto-link UPCs to edit tool
@@ -475,18 +543,22 @@ class FannieReportPage2 extends FanniePage {
 			}
 			if ( $textAlign == '' && preg_match("/^[0-9., $%]+$/",$row[$i]) )
 				$textAlign = 'right';
-			$class = ($textAlign == 'right') ? " class='number'" : '';
+			$class = '';
+			$class .= ($textAlign == 'right') ? " number" : '';
+			$class .= ($footer) ? " footer" : '';
+			$class = ($class) ? " class='$class'" : '';
 			$ret .= '<'.$tag.$class.' colspan="'.$span.'">'.$row[$i].'</'.$tag.'>';
 			$i += $span;
 		}
 		return $ret.'</tr>';
+	// html_line()
 	}
 
 	/**
 	  Turn array into CSV line
 	  @param $row an array of data
 	  @return CSV string
-	*/
+
 	function csv_line($row){
 		$ret = "";
 		foreach($row as $item){
@@ -496,10 +568,12 @@ class FannieReportPage2 extends FanniePage {
 		$ret = substr($ret,0,strlen($ret)-1)."\r\n";
 		return $ret;
 	}
+	No change.
+	*/
 
 	/**
 	  Check for input and display the page
-	*/
+
 	function draw_page(){
 		if ($this->preprocess()){
 			
@@ -542,6 +616,9 @@ class FannieReportPage2 extends FanniePage {
 			}
 		}
 	}
+	No change.
+	*/
+
 }
 
 ?>
