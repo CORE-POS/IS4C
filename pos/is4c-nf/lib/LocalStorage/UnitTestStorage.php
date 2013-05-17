ddd828d629e5284398695873d23232be431218dd
Andy Theuninck <andy@gohanman.com>
2013-05-17 10:05:29 -0500

    Stand-in module to eliminate PHP session usage during unit tests

---
+++ pos/is4c-nf/lib/LocalStorage/UnitTestStorage.php
@@ -0,0 +1,50 @@
+<?php
+/*******************************************************************************
+
+    Copyright 2013 Whole Foods Co-op
+
+    This file is part of IT CORE.
+
+    IT CORE is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    IT CORE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    in the file license.txt along with IT CORE; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*********************************************************************************/
+
+if (!class_exists("LocalStorage")) include_once(realpath(dirname(__FILE__).'/LocalStorage.php'));
+
+/**
+  @class UnitTestStorage
+
+  Doesn't do any persistant storage. Just facilitates unit testing
+*/
+class UnitTestStorage extends LocalStorage {
+
+	private $mutable = array();
+
+	function get($key){
+		if ($this->is_immutable($key)) return $this->immutables[$key];
+		if (!isset($this->mutable["$key"])) return "";
+		return $this->mutable["$key"];
+	}
+
+	function set($key,$val,$immutable=False){
+		if ($immutable)
+			$this->immutable_set($key,$val);
+		else
+			$this->mutable["$key"] = $val;
+		$this->debug($key,$val);
+	}
+}
+
+?>
