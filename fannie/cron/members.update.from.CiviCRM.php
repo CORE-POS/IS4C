72807351da4cb7ad9d3bd6d46d695acc88750532
Eric Lee <ericlee@sympatico.ca>
2013-05-17 02:40:22 -0400

    cron member-civicrm

--- fannie/cron/members.update.from.CiviCRM.php
+++ fannie/cron/members.update.from.CiviCRM.php
@@ -1,5 +1,37 @@
 <?php
-/* updateMembers.php
+/*******************************************************************************
+
+    Copyright 2012 West End Food Co-op, Toronto, ON, Canada
+
+    This file is part of Fannie.
+
+    IT CORE is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    IT CORE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    in the file license.txt along with IT CORE; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*********************************************************************************/
+
+/* HELP
+
+   members.update.from.CiviCRM.php
+
+	 Updates Fannie membership data from CiviCRM 3.4.4.
+	 Matches records on custdata.CardNo = civicrm_membership.id
+	 Add CiviCRM records that don't exist in Fannie.
+
+*/
+
+/* members.update.from.CiviCRM.php
    update IS4C members tables from CiviCRM membership tables
 	 
  --FUNCTIONALITY { - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
