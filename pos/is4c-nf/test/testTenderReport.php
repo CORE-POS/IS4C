a01c9b5401fb28e52dc89ab4fed7e61be59275a8
Andy Theuninck <andy@gohanman.com>
2013-06-27 16:40:32 -0500

    TR testing rig

---
+++ pos/is4c-nf/test/testTenderReport.php
@@ -0,0 +1,28 @@
+<?php
+include('../lib/AutoLoader.php');
+AutoLoader::LoadMap();
+CoreState::initiate_session();
+$mods = AutoLoader::ListModules('TenderReport');
+?>
+<form action="testTenderReport.php">
+<select name='t_mod'>
+<?php foreach ($mods as $m){ ?>
+<option><?php echo $m; ?></option>
+<?php } ?>
+</select>
+
+Emp# <input type="text" size="3" name="emp_no" />
+
+<input type="submit" value="Get Output" />
+</form>
+<hr />
+<?php
+if (isset($_REQUEST['t_mod']) && isset($_REQUEST['emp_no'])){
+	$CORE_LOCAL->set('CashierNo',$_REQUEST['emp_no']);
+	$tmod = $_REQUEST['t_mod'];
+	echo "Output for $tmod:<br />";
+	echo '<pre>';
+	var_dump($tmod::get());
+	echo '</pre>';
+}
+?>
