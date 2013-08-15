3e7f444384873f390034a93d2e4c7a4a3c6ba87c
Andy Theuninck <andy@gohanman.com>
2013-08-15 11:05:46 -0500

    Draft data model for ordering from vendors

--- fannie/install/sql/op/vendorItems.php
+++ fannie/install/sql/op/vendorItems.php
@@ -29,11 +29,22 @@ units, each with a size of 12 oz.
 
 Cost represents the unit cost. Cost times units 
 should then equal the case cost.
+
+upc corresponds to products.upc. Multiple vendorItems
+records may map to one products record if an item
+is available from more than one vendor or under 
+several SKUs from the one vendor. sku should 
+uniquely identify an item for the purpose of ordering
+it from the vendor. If the vendor does not have SKUs
+you have to assign some. The field is wide enough
+to hold a UPC; putting your UPC or the vendor's UPC
+in the SKU field may be a simple solution to assigning
+SKUs.
 */
 $CREATE['op.vendorItems'] = "
 	CREATE TABLE vendorItems (
 		upc varchar(13),
-		sku varchar(10),
+		sku varchar(13),
 		brand varchar(50),
 		description varchar(50),
 		size varchar(25),
@@ -41,9 +52,10 @@ $CREATE['op.vendorItems'] = "
 		cost decimal(10,2),
 		vendorDept int,
 		vendorID int,
-		PRIMARY KEY (vendorID,upc),
+		PRIMARY KEY (vendorID,sku),
 		INDEX(vendorID),
-		INDEX(upc)
+		INDEX(upc),
+		INDEX(sku)
 	)
 ";
 
