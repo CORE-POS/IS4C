146655668293d9d4d6ba37dccdc5235e89baf5a9
Joel Brock <joelbrock@gmail.com>
2013-08-29 12:19:41 -0700

    rcpt printing: add newline after item listing and before totals

--- pos/is4c-nf/lib/ReceiptLib.php
+++ pos/is4c-nf/lib/ReceiptLib.php
@@ -817,6 +817,8 @@ static public function receiptDetail($reprint=False,$trans_num='') { // put into
 					$detail .= "\n";
 				}
 			}
+			// add newline after item listing and before totals 
+			if ($row[1] == 1) $detail .= "\n";
 			/***** jqh 12/14/05 fix tax exempt on receipt *****/
 			if ($row[1]==2 and $CORE_LOCAL->get("TaxExempt")==1){
 				$detail .= "                                         TAX    0.00\n";
