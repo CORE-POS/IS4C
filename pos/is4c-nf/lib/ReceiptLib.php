80612eb55a68ed425817c3be3a8d365d70ebbe9c
Joel Brock <joelbrock@gmail.com>
2013-09-06 12:21:05 -0700

    rcpt: newline after date line

--- pos/is4c-nf/lib/ReceiptLib.php
+++ pos/is4c-nf/lib/ReceiptLib.php
@@ -199,7 +199,7 @@ static public function printReceiptHeader($dateTimeStamp, $ref) {
 	$time = self::build_time($dateTimeStamp);
 	$time = str_replace(" ","     ",$time);
 	$spaces = 55 - strlen($time) - strlen($ref);
-	$receipt .= $time.str_repeat(' ',$spaces).$ref."\n";
+	$receipt .= $time.str_repeat(' ',$spaces).$ref."\n\n";
 			
 	return $receipt;
 }
