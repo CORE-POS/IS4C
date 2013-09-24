ac356d09193c0d73e60cb93379614dd94182c8e3
Andy Theuninck <andy@gohanman.com>
2013-09-24 11:08:52 -0500

    Always prompt for password when canceling a transaction

--- pos/is4c-nf/parser-class-lib/parse/Steering.php
+++ pos/is4c-nf/parser-class-lib/parse/Steering.php
@@ -171,6 +171,10 @@ class Steering extends Parser {
 			return True;
 		*/
 		case "CN":
+			/* always prompt for a login. this older behavior can
+			   be brought back as an optional feature, but I'm not
+			   going to add yet-another-setting unless someone actually
+			   needs this. Andy 24Sep13
 			if ($CORE_LOCAL->get("runningTotal") == 0) {
 				$this->ret['receipt'] = 'cancelled';
 				$this->ret['output'] = DisplayLib::printheaderb();
@@ -179,6 +183,8 @@ class Steering extends Parser {
 			else {
 				$this->ret['main_frame'] = $my_url."gui-modules/mgrlogin.php";
 			}
+			*/
+			$this->ret['main_frame'] = $my_url."gui-modules/mgrlogin.php";
 			return True;
 		case "CC":
 			if ($CORE_LOCAL->get("ttlflag") != 1){
