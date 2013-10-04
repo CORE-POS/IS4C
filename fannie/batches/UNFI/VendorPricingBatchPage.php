412c229d444c7be1f93d61b536ce372dad7a2e7d
Andy Theuninck <andy@gohanman.com>
2013-10-04 12:31:31 -0500

    Batch update bugfixes (missing global, wrong var for $price) Added 'selection' color to vendor pricing in addition to red/green

--- fannie/batches/UNFI/VendorPricingBatchPage.php
+++ fannie/batches/UNFI/VendorPricingBatchPage.php
@@ -60,6 +60,9 @@ class VendorPricingBatchPage extends FanniePage {
 		tr.white td.sub {
 			background:#ffffff;
 		}
+		tr.selection td.sub {
+			background:#add8e6;
+		}
 		td.srp {
 			text-decoration: underline;
 		}';
@@ -88,7 +91,7 @@ function toggleB(upc){
 			url: 'batchAjax.php',
 			data: dstr + '&action=batchAdd&price='+price,
 			success: function(data){
-
+				$('#row'+upc).attr('class','selection');
 			}
 		});
 	}
@@ -98,7 +101,12 @@ function toggleB(upc){
 			url: 'batchAjax.php',
 			data: dstr + '&action=batchDel',
 			success: function(data){
-
+				if ($('tr#row'+upc+' input.varp:checked').length > 0)
+					$('#row'+upc).attr('class','white');
+				else if ($('tr#row'+upc+' td.price').html() < $('tr#row'+upc+' td.srp').html())
+					$('#row'+upc).attr('class','red');
+				else
+					$('#row'+upc).attr('class','green');
 			}
 		});
 	}
@@ -265,7 +273,9 @@ function saveprice(upc){
 			<th>Batch</th></tr>";
 		while($row = $dbc->fetch_row($result)){
 			$bg = "white";
-			if ($row['variable_pricing'] != 1)
+			if (isset($batchUPCs[$row['upc']]))
+				$bg = 'selection';
+			elseif ($row['variable_pricing'] != 1)
 				$bg = ($row['normal_price']<$row['srp'])?'red':'green';
 			$ret .= sprintf("<tr id=row%s class=%s>
 				<td class=\"sub\">%s</td>
