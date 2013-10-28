9bf14652d9c06d6b7dfcface787c07bb2d749f10
Andy Theuninck <andy@gohanman.com>
2013-10-28 12:43:10 -0500

    BUGFIX: normal vs special pricing interacting with volume sales

--- pos/is4c-nf/lib/Scanning/DiscountTypes/EveryoneSale.php
+++ pos/is4c-nf/lib/Scanning/DiscountTypes/EveryoneSale.php
@@ -83,11 +83,7 @@ class EveryoneSale extends DiscountType {
 				$ret['unitPrice'] = MiscLib::truncate2($total / $quantity);
 			}
 		}
-		else if ($row['special_price'] == $row['specialgroupprice'] && $row['special_price'] <> 0){
-			// volumed priced
-			$ret['unitPrice'] = $row['normal_price'];
-			$ret['discount'] = 0;
-		}
+
 		$this->savedRow = $row;
 		$this->savedInfo = $ret;
 		return $ret;
