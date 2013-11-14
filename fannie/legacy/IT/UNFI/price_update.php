17ee9c55282a7688eed47af3c387f4d9f8463b1e
Andy Theuninck <andy@gohanman.com>
2013-11-14 10:48:48 -0600

    Legacy cleanup: prepared statements on old vendor pricing tools

--- fannie/legacy/IT/UNFI/price_update.php
+++ fannie/legacy/IT/UNFI/price_update.php
@@ -1,6 +1,5 @@
 <?php
 include('../../../config.php');
-include($FANNIE_ROOT.'src/functions.php');
 require($FANNIE_ROOT.'item/pricePerOunce.php');
 
 if (!class_exists("SQLManager")) require_once($FANNIE_ROOT."src/SQLManager.php");
@@ -12,28 +11,34 @@ if ($buyID == 99){
 	$buyID=0;
 }
 else if ($buyID != 0){
-	$getBuyerQ = "SELECT subdept_name from subdepts where subdept_no = $buyID";
+	$getBuyerQ = $sql->prepare("SELECT subdept_name from subdepts where subdept_no = ?");
 
-	$getBuyerR = $sql->query($getBuyerQ);
+	$getBuyerR = $sql->execute($getBuyerQ, array($buyID));
 	$getBuyerW = $sql->fetch_array($getBuyerR);
 	$buyer = $getBuyerW['subdept_name'];
 }
 $date = date('mjY');
 $batchName = "priceUpdate".$buyer.$date;
 
-$insBatchQ = "INSERT INTO batchTest(startDate,endDate,batchName,batchType,discounttype) 
-              VALUES(now(),now(),'$batchName',7,0)";
+$insBatchQ = $sql->prepare("INSERT INTO batchTest(startDate,endDate,batchName,batchType,discounttype) 
+              VALUES(".$sql->now().','.$sql->now().",?,7,0)");
 //echo $insBatchQ;
-$insBatchR = $sql->query($insBatchQ);
+$insBatchR = $sql->execute($insBatchQ, array($batchName));
+$maxID = $sql->insert_id();
 
 echo "<b>".$buyer."</b><br>";
 echo "<html><head><title>Check tag info</title></head><body bgcolor='ffffcc'>";
 echo "<table border=1 cellspacing=0 cellpadding=0><th>UPC<th><font color=blue>Description</font><th>SKU<th>Brand<th>Pack<th>Size<th>Price";
 echo "<form action=newBarBatch.php method=Post>";
+$getUNFIPriceQ = $sql->prepare("SELECT * FROM unfi_all where upc = ?");
+$insBItemQ = $sql->prepare("INSERT INTO batchListTest(upc,batchID,salePrice)
+            VALUES(?,?,?)");
+$getTagInfoQ = $sql->prepare("SELECT * FROM unfi_order WHERE upcc LIKE ?");
+$barP = $sql->prepare("INSERT INTO shelftags (id,upc,description,normal_price,brand,sku,units,size,vendor,
+    pricePerUnit) VALUES (?,?,?,?,?,?,?,?,'UNFI',?)");
 foreach ($_POST["pricechange"] as $value) {
-      $getUNFIPriceQ = "SELECT * FROM unfi_all where upc = '".$value."'";
       //echo $getUNFIPriceQ . "<br>";
-      $getUNFIPriceR = $sql->query($getUNFIPriceQ);
+      $getUNFIPriceR = $sql->execute($getUNFIPriceQ, array($value));
       $getUNFIPriceW = $sql->fetch_array($getUNFIPriceR);
       $upc = $getUNFIPriceW['upc'];
       $upcl = ltrim($getUNFIPriceW['upc'],0);
@@ -41,19 +46,11 @@ foreach ($_POST["pricechange"] as $value) {
       //$upc = str_pad($upc,11,0,STR_PAD_LEFT);
       $unfiPrice  = $getUNFIPriceW['wfc_srp'];
       
-      $maxBatchQ = "SELECT max(batchID) as maxID FROM batchTest";
-      $maxBatchR = $sql->query($maxBatchQ);
-      $maxBatchW = $sql->fetch_array($maxBatchR);
-      $maxID = $maxBatchW['maxID'];
-
-      $insBItemQ = "INSERT INTO batchListTest(upc,batchID,salePrice)
-                    VALUES('$upc',$maxID,$unfiPrice)";      
-      $insBItemR = $sql->query($insBItemQ);
+      $insBItemR = $sql->execute($insBItemQ, array($upc, $maxID, $unfiPrice));
       //echo $insBItemQ;
 
-      $getTagInfoQ = "SELECT * FROM unfi_order WHERE upcc LIKE '%".$upcl."%'";
       //echo $getTagInfoQ;
-      $getTagInfoR = $sql->query($getTagInfoQ);
+      $getTagInfoR = $sql->execute($getTagInfoQ, array('%'.$upcl.'%'));
       $getTagInfoW = $sql->fetch_array($getTagInfoR);
       $desc = $getTagInfoW['item_desc'];
       $sku = $getTagInfoW['unfi_sku'];
@@ -65,9 +62,8 @@ foreach ($_POST["pricechange"] as $value) {
       if(empty($pak)){
          $pak = 0;
       }
-      $insQ = "INSERT INTO shelftags VALUES(".$buyID.",'".$upc."','".$desc."',$unfiPrice,'".$brand."','".$sku."','".$size."',$pak,'UNFI','$ppo')";
       //echo $insQ . "<br>";
-      $insR = $sql->query($insQ);
+      $insR = $sql->execute($barP, array($buyID, $upc, $desc, $unfiPrice, $brand, $sku, $size, $pak, $ppo));
       
       echo "<tr><td>$upc</td><td><font color=blue><b>$desc</b></font></td><td>$sku</td>";
       echo "<td>$brand</td><td>$pak</td><td>$size</td>";
