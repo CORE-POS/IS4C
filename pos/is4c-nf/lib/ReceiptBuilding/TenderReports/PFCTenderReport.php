0bc983c10434d1110c5e02f139eaf361f49eeecc
Joel Brock <joelbrock@gmail.com>
2013-12-05 14:32:32 -0800

    PFC Tender report customizations - building out the trTotal() function, adding itemizations

--- pos/is4c-nf/lib/ReceiptBuilding/TenderReports/PFCTenderReport.php
+++ pos/is4c-nf/lib/ReceiptBuilding/TenderReports/PFCTenderReport.php
@@ -141,7 +141,7 @@ static public function get(){
 
 		if ($itemize == 1) $receipt .=	ReceiptLib::centerString("------------------------------------------------------");
 
-		$query = "select tdate,register_no,emp_no,trans_no,total 
+		$query = "select tdate,register_no,emp_no,trans_no,card_no,total 
 			from dlog where register_no=".$CORE_LOCAL->get('laneno').
 			" and trans_subtype = '".$tender_code."' and (total <> 0 OR total <> -0) 
 			 AND tdate >= '$shiftCutoff'$excl order by tdate";
@@ -159,6 +159,7 @@ static public function get(){
 				.substr($row["register_no"].$blank, 0, 9)
 				.substr($row["trans_no"].$blank, 0, 8)
 				.substr($blank.$row['emp_no'], -10)
+				.substr($blank.$row["card_no"],-6)
 				.substr($blank.number_format($row["total"], 2), -14)."\n";
 			}
 			$sum += $row["total"];
@@ -171,104 +172,106 @@ static public function get(){
 //		$receipt .= chr(27).chr(105);
 	}
 
-	$titleStr = "M E M B E R   E Q U I T Y";
-	$receipt .= ReceiptLib::centerString($titleStr)."\n";
-
-	$receipt .=	ReceiptLib::centerString("------------------------------------------------------");
-
-	$query = "select tdate,register_no,emp_no,trans_no,total
-	       	from dlog where register_no=".$CORE_LOCAL->get('laneno').
-			" and department = 36 AND tdate >= '$shiftCutoff'$excl order by tdate";
-	$result = $db_a->query($query);
-	$num_rows = $db_a->num_rows($result);
+	// $titleStr = "M E M B E R   E Q U I T Y";
+	// $receipt .= ReceiptLib::centerString($titleStr)."\n";
+	// 
+	// $receipt .=	ReceiptLib::centerString("------------------------------------------------------");
+	// 
+	// $query = "select tdate,register_no,emp_no,trans_no,total
+	//        	from dlog where register_no=".$CORE_LOCAL->get('laneno').
+	// 		" and department = 36 AND tdate >= '$shiftCutoff'$excl order by tdate";
+	// $result = $db_a->query($query);
+	// $num_rows = $db_a->num_rows($result);
+	// 
+	// $itemize = 1;
+	// if ($itemize == 1) $receipt .= $fieldNames;
+	// $sum = 0;
+	// 
+	// for ($i = 0; $i < $num_rows; $i++) {
+	// 	$row = $db_a->fetch_array($result);
+	// 	$timeStamp = self::timeStamp($row["tdate"]);
+	// 	if ($itemize == 1) {
+	// 		$receipt .= "  ".substr($timeStamp.$blank, 0, 13)
+	// 		.substr($row["register_no"].$blank, 0, 9)
+	// 		.substr($row["trans_no"].$blank, 0, 8)
+	// 		.substr($blank.$row['emp_no'], -10)
+	// 		.substr($blank.number_format($row["total"], 2), -14)."\n";
+	// 	}
+	// 	$sum += $row["total"];
+	// }
+	// 
+	// $receipt.= ReceiptLib::centerString("------------------------------------------------------");
+	// 
+	// $receipt .= substr($blank.$blank.$blank."Count: ".$num_rows."  Total: ".number_format($sum,2), -56)."\n";
+	// 
+	// $receipt .= str_repeat("\n", 3);
+	$receipt .= trTotal(36,'MEMBER EQUITY', True);
 	
-	$itemize = 1;
-	if ($itemize == 1) $receipt .= $fieldNames;
-	$sum = 0;
-
-	for ($i = 0; $i < $num_rows; $i++) {
-		$row = $db_a->fetch_array($result);
-		$timeStamp = self::timeStamp($row["tdate"]);
-		if ($itemize == 1) {
-			$receipt .= "  ".substr($timeStamp.$blank, 0, 13)
-			.substr($row["register_no"].$blank, 0, 9)
-			.substr($row["trans_no"].$blank, 0, 8)
-			.substr($blank.$row['emp_no'], -10)
-			.substr($blank.number_format($row["total"], 2), -14)."\n";
-		}
-		$sum += $row["total"];
-	}
+	// $titleStr = "R C V D  /  A C C O U N T";
+	// $receipt .= ReceiptLib::centerString($titleStr)."\n";
+	// 
+	// $receipt .=	ReceiptLib::centerString("------------------------------------------------------");
+	// 
+	// $query = "select tdate,register_no,emp_no,trans_no,total
+	//        	from dlog where register_no=".$CORE_LOCAL->get('laneno').
+	// 		" and  department = 45 AND tdate >= '$shiftCutoff'$excl order by tdate";
+	// $result = $db_a->query($query);
+	// $num_rows = $db_a->num_rows($result);
+	// $itemize = 1;	
+	// if ($itemize == 1) $receipt .= $fieldNames;
+	// $sum = 0;
+	// 
+	// for ($i = 0; $i < $num_rows; $i++) {
+	// 	$row = $db_a->fetch_array($result);
+	// 	$timeStamp = self::timeStamp($row["tdate"]);
+	// 	if ($itemize == 1) {
+	// 		$receipt .= "  ".substr($timeStamp.$blank, 0, 13)
+	// 		.substr($row["register_no"].$blank, 0, 9)
+	// 		.substr($row["trans_no"].$blank, 0, 8)
+	// 		.substr($blank.$row['emp_no'], -10)
+	// 		.substr($blank.number_format($row["total"], 2), -14)."\n";
+	// 	}
+	// 	$sum += $row["total"];
+	// }
+	// 
+	// $receipt.= ReceiptLib::centerString("------------------------------------------------------");
+	// 
+	// $receipt .= substr($blank.$blank.$blank."Count: ".$num_rows."  Total: ".number_format($sum,2), -56)."\n";
+	// 
+	// $receipt .= str_repeat("\n", 3);
+	$receipt .= trTotal(45,'RCVD / ACCOUNT', True);
 	
-	$receipt.= ReceiptLib::centerString("------------------------------------------------------");
-
-	$receipt .= substr($blank.$blank.$blank."Count: ".$num_rows."  Total: ".number_format($sum,2), -56)."\n";
-
-	$receipt .= str_repeat("\n", 3);
-
-	$titleStr = "R C V D  /  A C C O U N T";
-	$receipt .= ReceiptLib::centerString($titleStr)."\n";
-
-	$receipt .=	ReceiptLib::centerString("------------------------------------------------------");
-
-	$query = "select tdate,register_no,emp_no,trans_no,total
-	       	from dlog where register_no=".$CORE_LOCAL->get('laneno').
-			" and  department = 45 AND tdate >= '$shiftCutoff'$excl order by tdate";
-	$result = $db_a->query($query);
-	$num_rows = $db_a->num_rows($result);
-	$itemize = 1;	
-	if ($itemize == 1) $receipt .= $fieldNames;
-	$sum = 0;
-
-	for ($i = 0; $i < $num_rows; $i++) {
-		$row = $db_a->fetch_array($result);
-		$timeStamp = self::timeStamp($row["tdate"]);
-		if ($itemize == 1) {
-			$receipt .= "  ".substr($timeStamp.$blank, 0, 13)
-			.substr($row["register_no"].$blank, 0, 9)
-			.substr($row["trans_no"].$blank, 0, 8)
-			.substr($blank.$row['emp_no'], -10)
-			.substr($blank.number_format($row["total"], 2), -14)."\n";
-		}
-		$sum += $row["total"];
-	}
-	
-	$receipt.= ReceiptLib::centerString("------------------------------------------------------");
-
-	$receipt .= substr($blank.$blank.$blank."Count: ".$num_rows."  Total: ".number_format($sum,2), -56)."\n";
-
-	$receipt .= str_repeat("\n", 3);
-
-	$titleStr = "G I F T   C A R D   S O L D";
-	$receipt .= ReceiptLib::centerString($titleStr)."\n";
-
-	$receipt .=	ReceiptLib::centerString("------------------------------------------------------");
-
-	$query = "select tdate,register_no,emp_no,trans_no,total
-	       	from dlog where register_no=".$CORE_LOCAL->get('laneno').
-			" and department = 41 AND tdate >= '$shiftCutoff'$excl order by tdate";
-	$result = $db_a->query($query);
-	$num_rows = $db_a->num_rows($result);
-	$itemize = 1;
-	if ($itemize == 1) $receipt .= $fieldNames;
-	$sum = 0;
-
-	for ($i = 0; $i < $num_rows; $i++) {
-		$row = $db_a->fetch_array($result);
-		$timeStamp = self::timeStamp($row["tdate"]);
-		if ($itemize == 1) {
-			$receipt .= "  ".substr($timeStamp.$blank, 0, 13)
-			.substr($row["register_no"].$blank, 0, 9)
-			.substr($row["trans_no"].$blank, 0, 8)
-			.substr($blank.$row['emp_no'], -10)
-			.substr($blank.number_format($row["total"], 2), -14)."\n";
-		}
-		$sum += $row["total"];
-	}
-	
-	$receipt.= ReceiptLib::centerString("------------------------------------------------------");
-
-	$receipt .= substr($blank.$blank.$blank."Count: ".$num_rows."  Total: ".number_format($sum,2), -56)."\n";
-
+	// $titleStr = "G I F T   C A R D   S O L D";
+	// $receipt .= ReceiptLib::centerString($titleStr)."\n";
+	// 
+	// $receipt .=	ReceiptLib::centerString("------------------------------------------------------");
+	// 
+	// $query = "select tdate,register_no,emp_no,trans_no,total
+	//        	from dlog where register_no=".$CORE_LOCAL->get('laneno').
+	// 		" and department = 41 AND tdate >= '$shiftCutoff'$excl order by tdate";
+	// $result = $db_a->query($query);
+	// $num_rows = $db_a->num_rows($result);
+	// $itemize = 1;
+	// if ($itemize == 1) $receipt .= $fieldNames;
+	// $sum = 0;
+	// 
+	// for ($i = 0; $i < $num_rows; $i++) {
+	// 	$row = $db_a->fetch_array($result);
+	// 	$timeStamp = self::timeStamp($row["tdate"]);
+	// 	if ($itemize == 1) {
+	// 		$receipt .= "  ".substr($timeStamp.$blank, 0, 13)
+	// 		.substr($row["register_no"].$blank, 0, 9)
+	// 		.substr($row["trans_no"].$blank, 0, 8)
+	// 		.substr($blank.$row['emp_no'], -10)
+	// 		.substr($blank.number_format($row["total"], 2), -14)."\n";
+	// 	}
+	// 	$sum += $row["total"];
+	// }
+	// 
+	// $receipt.= ReceiptLib::centerString("------------------------------------------------------");
+	// 
+	// $receipt .= substr($blank.$blank.$blank."Count: ".$num_rows."  Total: ".number_format($sum,2), -56)."\n";
+	$receipt .= trTotal(41,'GIFT CARDS SOLD', True);
 	
 	$receipt .= str_repeat("\n", 5);
 
@@ -277,7 +280,37 @@ static public function get(){
 
 }
 
-function trTotal($k, $label) {
+// function trTotal($k, $label) {
+// 	global $CORE_LOCAL;
+// 	$db_a = Database::mDataConnect();
+// 
+// 	$blank = "             ";
+// 	$shiftCutoff = date('Y-m-d 00:00:00');
+// 	$lookup = $db_a->query("SELECT MAX(datetime) FROM dtransactions 
+// 		WHERE DATE(datetime) = CURDATE() AND upc='ENDOFSHIFT' AND 
+// 		register_no=".$CORE_LOCAL->get('laneno'));
+// 	if ($db_a->num_rows($lookup) > 0){
+// 		$row = $db_a->fetch_row($lookup);
+// 		if ($row[0] != '') $shiftCutoff = $row[0];
+// 	}
+// 
+// 	if (is_array($k)) $k = "'" . implode("','", $k) . "'";
+//     elseif (!is_numeric($k)) $k = "'".$k."'";
+// 	$q = (!is_numeric($k)) ? 'trans_subtype' : 'department';
+// 	
+//     $tenderQ = "SELECT -SUM(total) AS net, COUNT(total) FROM dlog 
+//     	WHERE register_no=".$CORE_LOCAL->get('laneno').
+// 		" AND $q IN($k) AND tdate >= '$shiftCutoff' AND emp_no <> 9999";
+// 	
+// 	$tenderR = $db_a->query($tenderQ);
+// 	$tender = $db_a->fetch_row($tenderR);
+// 
+// 	return "  ".substr($label.$blank.$blank,0,20).substr($blank.number_format(($tender[0]),2),-8).substr($blank.$tender[1],-8)."\n";
+// 	
+// }
+
+
+function trTotal($k, $label,$i=False) {
 	global $CORE_LOCAL;
 	$db_a = Database::mDataConnect();
 
@@ -295,15 +328,45 @@ function trTotal($k, $label) {
     elseif (!is_numeric($k)) $k = "'".$k."'";
 	$q = (!is_numeric($k)) ? 'trans_subtype' : 'department';
 	
-    $tenderQ = "SELECT -SUM(total) AS net, COUNT(total) FROM dlog 
-    	WHERE register_no=".$CORE_LOCAL->get('laneno').
-		" AND $q IN($k) AND tdate >= '$shiftCutoff' AND emp_no <> 9999";
-	
+	if($i===False) {
+	    $tenderQ = "SELECT -SUM(total) AS net, COUNT(total) FROM dlog 
+	    	WHERE register_no=".$CORE_LOCAL->get('laneno').
+			" AND $q IN($k) AND tdate >= '$shiftCutoff' AND emp_no <> 9999";
+	} else {
+		$tenderQ = "SELECT tdate,register_no,emp_no,trans_no,card_no,total FROM dlog 
+			WHERE register_no=".$CORE_LOCAL->get('laneno').
+			" and $q IN($k) AND tdate >= '$shiftCutoff' AND emp_no <> 9999 order by tdate";
+		
+	}
 	$tenderR = $db_a->query($tenderQ);
-	$tender = $db_a->fetch_row($tenderR);
+	$tender = $db_a->fetch_array($tenderR);
+	$num_rows = $db_a->num_rows($tenderR);
 
-	return "  ".substr($label.$blank.$blank,0,20).substr($blank.number_format(($tender[0]),2),-8).substr($blank.$tender[1],-8)."\n";
+	if($i===False) {
+		$ret = "  ".substr($label.$blank.$blank,0,20).substr($blank.number_format(($tender[0]),2),-8).substr($blank.$tender[1],-8)."\n";
+	} else {
+		$sum = 0;
+		$titleStr = chunk_split($label,1);
+		$ret = ReceiptLib::centerString($titleStr)."\n";
+		$ret .=	ReceiptLib::centerString("------------------------------------------------------");
+		for ($i = 0; $i < $num_rows; $i++) {
+			$row = $db_a->fetch_array($result);
+			$timeStamp = self::timeStamp($row["tdate"]);
+			$ret .= "  ".substr($timeStamp.$blank, 0, 13)
+				.substr($row["register_no"].$blank, 0, 9)
+				.substr($row["trans_no"].$blank, 0, 8)
+				.substr($blank.$row['emp_no'], -8)
+				.substr($blank.$row["card_no"],-6)
+				.substr($blank.number_format($row["total"], 2), -14)."\n";
+			$sum += $row["total"];
+		}
+		$ret .= ReceiptLib::centerString("------------------------------------------------------");
+		$ret .= substr($blank.$blank.$blank."Count: ".$num_rows."  Total: ".number_format($sum,2), -56)."\n";
+
+		$ret .= str_repeat("\n", 3);
+	}	
 	
+	return $ret;
 }
 
 ?>
