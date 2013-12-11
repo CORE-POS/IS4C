b6a30fcd89faf7dafa8ace56c335fd27ea4b58e2
Andy Theuninck <andy@gohanman.com>
2013-12-11 12:37:56 -0600

    New setting for FS total page. Lets you change which option is selected by default.

--- pos/is4c-nf/gui-modules/fsTotalConfirm.php
+++ pos/is4c-nf/gui-modules/fsTotalConfirm.php
@@ -23,20 +23,23 @@
 
 include_once(dirname(__FILE__).'/../lib/AutoLoader.php');
 
-class fsTotalConfirm extends NoInputPage {
+class fsTotalConfirm extends NoInputPage 
+{
 
-	var $tendertype;
+	private $tendertype;
 
-	function preprocess(){
+	function preprocess()
+    {
 		global $CORE_LOCAL;
 		$this->tendertype = "";
-		if (isset($_REQUEST["selectlist"])){
+		if (isset($_REQUEST["selectlist"])) {
 			$choice = $_REQUEST["selectlist"];
-			if ($choice == "EF"){
+			if ($choice == "EF") {
 				$chk = PrehLib::fsEligible();
-				if ($chk !== True){
+				if ($chk !== true) {
 					$this->change_page($chk);
-					return False;
+
+					return false;
 				}
 				// 13Feb13 Andy
 				// Disable option to enter tender here by returning immediately	
@@ -44,61 +47,64 @@ class fsTotalConfirm extends NoInputPage {
 				// functionality removed entirely
 				$this->tendertype = 'EF';
 				$this->change_page($this->page_url."gui-modules/pos2.php");
-				return False;
-			}
-			elseif ($choice == "EC"){
+
+				return false;
+			} else if ($choice == "EC") {
 				$chk = PrehLib::ttl();
-				if ($chk !== True){
+				if ($chk !== true) {
 					$this->change_page($chk);
-					return False;
+
+					return false;
 				}
 				// 13Feb13 Andy
 				// Disabled option; see above
 				$this->tendertype = 'EC';
 				$this->change_page($this->page_url."gui-modules/pos2.php");
-				return False;
-			}
-			else if ($choice == ''){
+
+				return false;
+			} else if ($choice == '') {
 				$this->change_page($this->page_url."gui-modules/pos2.php");
-				return False;
+
+				return false;
 			}
-		}
-		elseif (isset($_REQUEST['tendertype'])){
+		} else if (isset($_REQUEST['tendertype'])) {
 			$this->tendertype = $_REQUEST['tendertype'];
-			$valid_input = False;
+			$valid_input = false;
 			$in = $_REQUEST['tenderamt'];
-			if (empty($in)){
-				if ($this->tendertype == 'EF')
+			if (empty($in)) {
+				if ($this->tendertype == 'EF') {
 					$CORE_LOCAL->set("strRemembered",100*$CORE_LOCAL->get("fsEligible")."EF");		
-				else
+				} else {
 					$CORE_LOCAL->set("strRemembered",100*$CORE_LOCAL->get("runningTotal")."EC");		
+                }
 				$CORE_LOCAL->set("msgrepeat",1);
-				$valid_input = True;
-			}
-			elseif (is_numeric($in)){
-				if ($this->tendertype == 'EF' && $in > (100*$CORE_LOCAL->get("fsEligible")))
-					$valid_input = False;
-				else {
+				$valid_input = true;
+			} else if (is_numeric($in)) {
+				if ($this->tendertype == 'EF' && $in > (100*$CORE_LOCAL->get("fsEligible"))) {
+					$valid_input = false;
+				} else {
 					$CORE_LOCAL->set("strRemembered",$in.$this->tendertype);
 					$CORE_LOCAL->set("msgrepeat",1);
-					$valid_input = True;
+					$valid_input = true;
 				}
-			}
-			elseif (strtoupper($in) == "CL"){
+			} else if (strtoupper($in) == "CL") {
 				$CORE_LOCAL->set("strRemembered","");
 				$CORE_LOCAL->set("msgrepeat",0);
-				$valid_input = True;
+				$valid_input = true;
 			}
 
-			if ($valid_input){
+			if ($valid_input) {
 				$this->change_page($this->page_url."gui-modules/pos2.php");
-				return False;
+
+				return false;
 			}
-		}	
-		return True;
+		}
+
+		return true;
 	}
 	
-	function head_content(){
+	function head_content()
+    {
 		?>
 		<script type="text/javascript" >
 		var prevKey = -1;
@@ -123,21 +129,28 @@ class fsTotalConfirm extends NoInputPage {
 		<?php
 	} // END head() FUNCTION
 
-	function body_content() {
+	function body_content() 
+    {
 		global $CORE_LOCAL;
+        $default = '';
+        if ($CORE_LOCAL->get('fntlDefault') === '' || $CORE_LOCAL->get('fntlDefault') == 1) {
+            $default = 'EC';
+        } else if ($CORE_LOCAL->get('fntlDefault') == 0) {
+            $default = 'EF';
+        }
 		?>
 		<div class="baseHeight">
 		<div class="centeredDisplay colored">
-		<?php if (empty($this->tendertype)){ ?>
+		<?php if (empty($this->tendertype)) { ?>
 		<span class="larger">Customer is using the</span>
 		<?php } ?>
 		<form id="selectform" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 
-		<?php if (empty($this->tendertype)){ ?>
+		<?php if (empty($this->tendertype)) { ?>
 			<select size="2" name="selectlist" 
 				id="selectlist" onblur="$('#selectlist').focus();">
-			<option value='EF' selected>Food Portion
-			<option value='EC'>Cash Portion
+			<option value='EC' <?php echo ($default == 'EC') ? 'selected' : ''; ?>>Cash Portion
+			<option value='EF' <?php echo ($default == 'EF') ? 'selected' : ''; ?>>Food Portion
 			</select>
 		<?php } else { ?>
 			<input type="text" id="tenderamt" 
@@ -157,15 +170,17 @@ class fsTotalConfirm extends NoInputPage {
 		</div>
 		</div>
 		<?php
-		if (empty($this->tendertype)){
+		if (empty($this->tendertype)) {
 			$this->add_onload_command("\$('#selectlist').focus();\n");
 			$this->add_onload_command("\$('#selectlist').keypress(processkeypress);\n");
-		}
-		else
+		} else {
 			$this->add_onload_command("\$('#tenderamt').focus();\n");
+        }
 	} // END body_content() FUNCTION
 }
 
-if (basename(__FILE__) == basename($_SERVER['PHP_SELF']))
+if (basename(__FILE__) == basename($_SERVER['PHP_SELF'])) {
 	new fsTotalConfirm();
+}
+
 ?>
