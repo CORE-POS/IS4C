6fa900b2eec56e65a68e217a34bbceed0aa63bd2
Joel Brock <joelbrock@gmail.com>
2013-12-30 19:10:12 -0500

    QUICK MENUS!!!

---
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/2.php
@@ -0,0 +1,14 @@
+<?php
+
+$my_menu = array(
+	"Hot Soup - 16oz. $3" => "",
+	"Hot Soup - 8oz. $1.50" => "",
+	"Hot Coffee/Tea $0.91" => "",
+	"Iced Coffee $1.36" => "",
+	"Hot Cider $1.36" => "",
+	"Grab'n'Go Sandwiches" => "",
+	"Grab'n'Go Salads" => "",
+	"Cup of Ice $0.45" => ""
+);
+
+?>
---
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/4.php
@@ -0,0 +1,14 @@
+<?php
+
+$my_menu = array(
+	"Lollipops" => "",
+	"Dans Chocolates" => "",
+	"Endangered Species" => "",
+	"Ginger Chews" => "",
+	"Goat Milk Caramels" => "",
+	"Honey Mints" => "",
+	"Honey Sticks" => "",
+	"Lake Champ. Coins" => ""
+);
+
+?>
