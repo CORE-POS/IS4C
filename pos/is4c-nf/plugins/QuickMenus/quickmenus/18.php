0200ea7ce8bc77b6da871d5978496df3003f6975
Joel Brock <joelbrock@gmail.com>
2014-01-01 17:04:43 -0500

    the last of the department-specific quickmenus

---
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/18.php
@@ -0,0 +1,8 @@
+<?php
+
+$my_menu = array(
+	"Enviro-Tote Co-op Canvas Bag" => "18001",
+	"Enviro-Tote Organic Co-op Wine" => "18002"
+);
+
+?>
---
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/20.php
@@ -0,0 +1,24 @@
+<?php
+
+$my_menu = array(
+	"Vegetarian Sandwich" => "20000",
+	"Regular Sandwich" => "20001",
+	"Pressed Salads" => "20002",
+	"Vegetarian Sushi" => "20003",
+	"Quinoa Salad" => "20004",
+	"Sesame Slippery Noodles" => "20005",
+	"Hummus" => "20006",                   
+	"Fruit Salad" => "20007",
+	"Kale Chips" => "20008",
+	"Rice Salad" => "20009",
+	"Tofu Salad" => "20010",
+	"Vegetable Salad" => "20011",
+	"Chicken Wings" => "20012",
+	"Chutney" => "20013",
+	"Pesto" => "20014",
+	"Potato Salad" => "20015",
+	"Eggs" => "20016",
+	"Fish Salad" => "20017"
+);
+
+?>
