aff8bf2aa636200414ae4443e8b7f428bbc0011c
Andy Theuninck <andy@gohanman.com>
2014-03-11 09:41:27 -0500

    Add translookup to integrated CC menu options

--- pos/is4c-nf/plugins/QuickMenus/quickmenus/1.php
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/1.php
@@ -4,7 +4,6 @@ $my_menu = array(
 	"Charge Available Card" => "CCFROMCACHE",
 	"Reset Terminal (Quick)" => "TERMRESET",
 	"Tender External CC" => "MANUALCC",
-    "Lookup Transaction" => "PCLOOKUP",
 	"Reboot Terminal (Slow)" => "TERMREBOOT",
 );
 
