6e20306f6657da5cc3bb1ea77dd56718cbb64c71
Andy Theuninck <andy@gohanman.com>
2014-04-03 13:00:45 -0500

    New report: suspended membership by date + reason

---
+++ fannie/reports/SuspensionsReport/index.php
@@ -0,0 +1,24 @@
+<?php
+/*******************************************************************************
+
+    Copyright 2009 Whole Foods Co-op
+
+    This file is part of Fannie.
+
+    Fannie is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Fannie is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    in the file license.txt along with IT CORE; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*********************************************************************************/
+header('Location: SuspensionsReport.php');
+
