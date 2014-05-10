ca80ab07a59ea828253e49c1879f22272b18e8ca
Joel Brock <joelbrock@gmail.com>
2014-05-10 10:23:24 -0400

    Departments quickmenu

--- pos/is4c-nf/plugins/QuickMenus/quickmenus/2.php
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/2.php
@@ -1,15 +1,15 @@
 <?php
 
 $my_menu = array(
-	"Brownies" => "2018",
-	"Danishes" => "2022",
-	"Chocolate Chip Cookies" => "2024",
-	"Organic Multi-Grain Bread" => "2038",
-	"Organic White Bread" => "2039",
-	"Organic Spelt Bread" => "2037",
-	"Organic Wheat Bread" => "2041",
-	"Organic Wheat English Muffins" => "2036",
-	"Baguette" => "2000"
+	"YFC Honey" => "DP170",
+	"Hot Coffee" => "DP160",
+	"Seeds" => "DP180",
+	"-------" => "",
+	"Stamps" => "DP2040",
+	"Non-YFC Sales" => "DP2070",
+	"Equity - Associate" => "3010DP",
+	"Equity - Scholarship" => "DP3020",
+	"" => ""
 	
 );
 
