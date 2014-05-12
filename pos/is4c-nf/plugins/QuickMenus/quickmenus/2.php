e6ff6d423173294884c927109e8fc9be0bd81b88
Joel Brock <joelbrock@gmail.com>
2014-05-12 13:42:40 -0400

    Add SPO depts. to QM2

--- pos/is4c-nf/plugins/QuickMenus/quickmenus/2.php
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/2.php
@@ -5,12 +5,16 @@ $my_menu = array(
 	"Hot Coffee" => "DP160",
 	"Seeds" => "DP180",
 	"-------" => "",
+	"SPO Regular" => "DP1100",
+	"SPO Taxable" => "DP1110",
+	"SPO Supplements" => "DP1120",
+	"SPO HBA" => "DP1130",
+	"SPO Beer" => "DP1140",
+	"-------" => "",
 	"Stamps" => "DP2040",
 	"Non-YFC Sales" => "DP2070",
 	"Equity - Associate" => "3010DP",
-	"Equity - Scholarship" => "DP3020",
-	"" => ""
-	
+	"Equity - Scholarship" => "DP3020"	
 );
 
 ?>
