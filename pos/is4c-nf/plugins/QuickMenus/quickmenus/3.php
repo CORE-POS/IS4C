0e3fcc24436a319a825e03aca0f4de09c1fac52d
Joel Brock <joelbrock@gmail.com>
2014-05-12 05:49:04 -0400

    Bakery quickmenu

--- pos/is4c-nf/plugins/QuickMenus/quickmenus/3.php
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/3.php
@@ -1,15 +1,10 @@
 <?php
 
 $my_menu = array(
-	"Almonds, Raw, Org." => "3183",
-	"OATS OG1 ROLLED REG GF" => "3181",
-	"QUINOA OG1 WHITE" => "3115",
-	"RICE OG1 WHITE BASMATI" => "3180",
-	"RICE BASMATI BROWN" => "3149",
-	"RICE BASMATI WHITE" => "3128",
-	"LENTILS OG2 RED SPLIT" => "3161",
-	"BEANS OG1 KIDNEY DARK RED" => "3136",
-	"POPCORN OG2 YELLOW" => "3138"
+	"Bakery Other" => "99000",
+	"Bakery Bread" => "99001",
+	"Bakery Granola" => "99002",
+	"Bakery Sweets" => "99003"
 );
 
 ?>
