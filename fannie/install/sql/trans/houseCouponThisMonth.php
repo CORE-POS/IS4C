3baf21955075898c10edd17d54f51117b010d185
Andy Theuninck <gohanman@alkan.dreamhost.com>
2014-08-06 17:14:10 -0700

    Fix fannie sample data. Custdata and departments corrected. Employees, memtype, super departments, tenders verified working. Products does still need to be fixed.

--- fannie/install/sql/trans/houseCouponThisMonth.php
+++ fannie/install/sql/trans/houseCouponThisMonth.php
@@ -16,28 +16,13 @@ List of custom coupons redeemed, per member
 */
 $CREATE['trans.houseCouponThisMonth'] = "
     CREATE VIEW houseCouponThisMonth AS
-    SELECT
-        s.card_no,
-        s.upc,
-        SUM(s.quantity) AS quantity
-    FROM (
-        SELECT card_no,upc,quantity
-        FROM dlog
-        WHERE 
-            trans_type='T'
-            AND trans_subtype='IC'
-            AND upc LIKE '004%'
-
-        UNION ALL
-
-        SELECT card_no,upc,quantity
+        SELECT card_no,upc,SUM(quantity) AS quantity
         FROM dlog_90_view
         WHERE 
             trans_type='T'
             AND trans_subtype='IC'
             AND upc LIKE '004%'
             AND " . $con->monthdiff($con->now(),'tdate') . "=0
-    ) AS s
-    GROUP BY s.card_no, s.upc
+	GROUP BY card_no, upc
 ";
 
