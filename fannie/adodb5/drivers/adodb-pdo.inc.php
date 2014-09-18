2a8ed7434c23f5ce73ce57a3b7e45d8fada3b3e5
Andy Theuninck <andy@gohanman.com>
2014-09-18 18:07:37 -0500

    Catching up fannie ADOdb code to upstream version. The mysql parts and main adodb file still need work to merge CORE-specific changes

--- fannie/adodb5/drivers/adodb-pdo.inc.php
+++ fannie/adodb5/drivers/adodb-pdo.inc.php
@@ -1,6 +1,6 @@
 <?php
 /* 
-V5.09 25 June 2009   (c) 2000-2009 John Lim (jlim#natsoft.com). All rights reserved.
+V5.20dev  ??-???-2014  (c) 2000-2014 John Lim (jlim#natsoft.com). All rights reserved.
   Released under both BSD license and Lesser GPL library license. 
   Whenever there is any discrepancy between the two licenses, 
   the BSD license will take precedence. 
@@ -436,12 +436,12 @@ class ADODB_pdo_base extends ADODB_pdo {
 		return $ret;
 	}
 	
-	function MetaTables($ttype=false,$showSchema=false,$mask=false)
+	function MetaTables()
 	{
 		return false;
 	}
 	
-	function MetaColumns($table,$normalize=true)
+	function MetaColumns()
 	{
 		return false;
 	}
@@ -622,5 +622,3 @@ class ADORecordSet_pdo extends ADORecordSet {
 	}
 
 }
-
-?>
