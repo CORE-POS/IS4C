c09e6eba4caa0c3840c253739283efb1c37cac19
Andy Theuninck <andy@gohanman.com>
2014-11-14 15:07:07 -0600

    Example unit test for TransArchiveTask monthly table creation

--- fannie/unit-tests/TasksTest.php
+++ fannie/unit-tests/TasksTest.php
@@ -14,5 +14,44 @@ class TasksTest extends PHPUnit_Framework_TestCase
         }
     }
 
+    public function testTransactionArchiving()
+    {
+        $task = new TransArchiveTask();
+
+        /**
+          Point references at the unit test databases
+        */
+        $GLOBALS['FANNIE_OP_DB'] = 'unit_test_op';
+        $GLOBALS['FANNIE_TRANS_DB'] = 'unit_test_trans';
+        $GLOBALS['FANNIE_ARCHIVE_DB'] = 'unit_test_archive';
+        $GLOBALS['FANNIE_ARCHIVE_METHOD'] = 'tables';
+
+        /**
+          Put a record in dtransactions that should trigger
+          a new monthly table & view
+        */
+        $dtrans = new DTransactionsModel(FannieDB::get('unit_test_trans'));
+        $dtrans->datetime('2999-01-01 00:00:00');
+        $dtrans->save();
+
+        $task->run();
+
+        /**
+          Verify the task created new monthly table & view
+        */
+        $archive_db = FannieDB::get('unit_test_archive');
+        $archive_table_exists = $archive_db->tableExists('transArchive299901');
+        $archive_dlog_exists = $archive_db->tableExists('dlog299901');
+        $this->assertEquals('true', $archive_table_exists, 'Monthly archive table not created');
+        $this->assertEquals('true', $archive_dlog_exists, 'Monthly dlog view not created');
+
+        /**
+          Verify dtransactions was cleared
+        */
+        $trans_db = FannieDB::get('unit_test_trans');
+        $records = $db->query('SELECT * FROM dtransactions');
+        $this->assertEquals(0, $trans_db->num_rows($records), 'dtransactions not cleared');
+    }
+
 }
 
