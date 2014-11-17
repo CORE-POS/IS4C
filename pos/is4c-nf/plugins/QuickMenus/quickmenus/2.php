79708c4b0035c4089e5b504747fb2a8aa76da673
Joel Brock <joelbrock@gmail.com>
2014-11-17 10:29:20 -0800

    custom deli quickmenu

--- pos/is4c-nf/plugins/QuickMenus/quickmenus/2.php
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/2.php
@@ -1,11 +1,11 @@
 <?php
 
 $my_menu = array(
-	"2020" => "Certified Local Soup - $8.99",
-	"2022" => "Certified Local Jam - $4.99",
-	"2037" => "Kale Salad Sm. - $4.99",
-	"2038" => "Kale Salad Lg. - $6.99",
-	"2019" => "Parsley Pesto - $6.99"
+	"Certified Local Soup - $8.99" => "2020",
+	"Certified Local Jam - $4.99" => "2022",
+	"Kale Salad Sm. - $4.99" => "2037",
+	"Kale Salad Lg. - $6.99" => "2038",
+	"Parsley Pesto - $6.99" => "2019"
 );
 
 ?>
