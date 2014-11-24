1a7c7f94b9f33b376de1a6676afb9d0ad98a926f
Andy Theuninck <andy@gohanman.com>
2014-11-24 16:52:18 -0600

    Avoid existence verification on DROP VIEW queries

--- pos/is4c-nf/unit-tests/InstallTest.php
+++ pos/is4c-nf/unit-tests/InstallTest.php
@@ -36,6 +36,10 @@ class InstallTest extends PHPUnit_Framework_TestCase
             $this->assertArrayHasKey('error', $error, 'Status entry missing key: error');
             $this->assertEquals(0, $error['error'], 'Error creating ' . $error['struct']
                 . ', ' . print_r($error, true));
+            if (isset($error['query']) && stristr($error['query'], 'DROP VIEW')) {
+                // don't check for existence on DROP VIEW queries
+                continue;
+            }
             $exists = $db->table_exists($error['struct']);
             $this->assertEquals(true, $exists, 'Failed to create ' . $error['struct']
                 . ', ' . print_r($error, true));
