9867e71151503f0b4f77d964db0aec229de2d376
Andy Theuninck <andy@gohanman.com>
2015-03-07 10:24:54 -0800

    Allow comments on all member correction pages. Prime candidates for refactoring, too. Lots of duplicated code here.

--- fannie/mem/correction_pages/PatronageTransferTool.php
+++ fannie/mem/correction_pages/PatronageTransferTool.php
@@ -144,6 +144,12 @@ class PatronageTransferTool extends FanniePage {
         $ret .= sprintf("\$%.2f will be moved from %d to %d (%s)",
             $this->amt,$this->cn1,$this->cn2,$this->name2);
         $ret .= "</div><p>";
+        $ret .= sprintf('<div class="form-group">
+            <label>Comment</label>
+            <input type="text" class="form-control" 
+                name="correction-comment" value="PAT XFER %d TO %d" />
+            </div>',
+            $this->cn1, $this->cn2);
         $ret .= "<input type=\"hidden\" name=\"date\" value=\"{$this->date}\" />";
         $ret .= "<input type=\"hidden\" name=\"trans_num\" value=\"{$this->tn}\" />";
         $ret .= "<input type=\"hidden\" name=\"memTo\" value=\"{$this->cn2}\" />";
@@ -166,12 +172,21 @@ class PatronageTransferTool extends FanniePage {
         $dtrans['trans_no'] = $this->getTransNo($this->CORRECTION_CASHIER,$this->CORRECTION_LANE);
         $dtrans['trans_id'] = 1;
         $this->doInsert($dtrans,-1*$this->amt,$this->CORRECTION_DEPT,$this->cn1);
+        $comment = FormLib::get('correction-comment');
+        if (!empty($comment)) {
+            $dtrans['trans_id'] = 2;
+            $this->doComment($dtrans, $comment, $this->cn1);
+        }
 
         $ret .= sprintf("Receipt #1: %s",$this->CORRECTION_CASHIER.'-'.$this->CORRECTION_LANE.'-'.$dtrans['trans_no']);
 
         $dtrans['trans_no'] = $this->getTransNo($this->CORRECTION_CASHIER,$this->CORRECTION_LANE);
         $dtrans['trans_id'] = 1;
         $this->doInsert($dtrans,$this->amt,$this->CORRECTION_DEPT,$this->cn2);
+        if (!empty($comment)) {
+            $dtrans['trans_id'] = 2;
+            $this->doComment($dtrans, $comment, $this->cn2);
+        }
 
         $ret .= "<br /><br />";
         $ret .= sprintf("Receipt #2: %s",$this->CORRECTION_CASHIER.'-'.$this->CORRECTION_LANE.'-'.$dtrans['trans_no']);
@@ -290,6 +305,70 @@ class PatronageTransferTool extends FanniePage {
                 $defaults['description'] = $nameW['dept_name'];
             }
         }
+
+        $q = $dbc->prepare_statement("SELECT memType,Staff FROM {$OP}custdata WHERE CardNo=?");
+        $r = $dbc->exec_statement($q,array($cardno));
+        $w = $dbc->fetch_row($r);
+        $defaults['memType'] = $w[0];
+        $defaults['staff'] = $w[1];
+
+        $columns = 'datetime,';
+        $values = $dbc->now().',';
+        $args = array();
+        foreach($defaults as $k=>$v){
+            $columns .= $k.',';
+            $values .= '?,';
+            $args[] = $v;
+        }
+        $columns = substr($columns,0,strlen($columns)-1);
+        $values = substr($values,0,strlen($values)-1);
+        $prep = $dbc->prepare_statement("INSERT INTO dtransactions ($columns) VALUES ($values)");
+        $dbc->exec_statement($prep, $args);
+    }
+
+    private function doComment($dtrans, $comment, $cardno)
+    {
+        global $FANNIE_OP_DB, $FANNIE_TRANS_DB;
+        $dbc = FannieDB::get($FANNIE_TRANS_DB);
+        $OP = $FANNIE_OP_DB.$dbc->sep();
+
+        $defaults = array(
+            'register_no'=>$this->CORRECTION_LANE,
+            'emp_no'=>$this->CORRECTION_CASHIER,
+            'trans_no'=>$dtrans['trans_no'],
+            'upc'=>'0',
+            'description'=>$comment,
+            'trans_type'=>'C',
+            'trans_subtype'=>'CM',
+            'trans_status'=>'',
+            'department'=>'',
+            'quantity'=>0,
+            'scale'=>0,
+            'cost'=>0,
+            'unitPrice'=>'',
+            'total'=>'',
+            'regPrice'=>'',
+            'tax'=>0,
+            'foodstamp'=>0,
+            'discount'=>0,
+            'memDiscount'=>0,
+            'discountable'=>0,
+            'discounttype'=>0,
+            'voided'=>0,
+            'percentDiscount'=>0,
+            'ItemQtty'=>0,
+            'volDiscType'=>0,
+            'volume'=>0,
+            'volSpecial'=>0,
+            'mixMatch'=>'',
+            'matched'=>0,
+            'memType'=>'',
+            'staff'=>'',
+            'numflag'=>0,
+            'charflag'=>'',
+            'card_no'=>$cardno,
+            'trans_id'=>$dtrans['trans_id']
+        );
 
         $q = $dbc->prepare_statement("SELECT memType,Staff FROM {$OP}custdata WHERE CardNo=?");
         $r = $dbc->exec_statement($q,array($cardno));
