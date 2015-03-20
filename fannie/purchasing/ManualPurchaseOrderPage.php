77dc75115c48584dc3fca02c3622f105dc8c8f89
Andy Theuninck <andy@gohanman.com>
2015-03-20 16:17:18 -0500

    Merge same-SKU purchase order lines together

--- fannie/purchasing/ManualPurchaseOrderPage.php
+++ fannie/purchasing/ManualPurchaseOrderPage.php
@@ -99,6 +99,7 @@ class ManualPurchaseOrderPage extends FannieRESTfulPage
         if (FormLib::get('order-id') !== '' && is_numeric(FormLib::get('order-id'))) {
             $orderID = FormLib::get('order-id');
             $po->orderID($orderID);
+            $po->save();
         } else {
             $orderID = $po->save();
         }
@@ -123,6 +124,15 @@ class ManualPurchaseOrderPage extends FannieRESTfulPage
             $units = $caseSize[$i];
             $qty = $cases[$i];
             $unitCost = $total[$i] / $qty / $units;
+            /**
+              Multiple same-SKU records
+              Sum the quantities and costs to merge
+              into a single record
+            */
+            if ($pitem->load()) {
+                $qty += $pitem->receivedQty();
+                $total[$i] += $pitem->receivedTotalCost();
+            }
 
             $pitem->quantity($qty);
             $pitem->caseSize($units);
