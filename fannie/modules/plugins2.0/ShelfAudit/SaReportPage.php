af9a28dd0c217ebda09d1fd4b49a82996375539c
Andy Theuninck <andy@gohanman.com>
2015-04-01 10:31:28 -0500

    BUGFIX: bad include path in ShelfAudit

--- fannie/modules/plugins2.0/ShelfAudit/SaReportPage.php
+++ fannie/modules/plugins2.0/ShelfAudit/SaReportPage.php
@@ -24,7 +24,7 @@
 
 include(dirname(__FILE__).'/../../../config.php');
 if (!class_exists('FannieAPI')) {
-    include_once(dirname(__FILE__) . '/classlib2.0/FannieAPI.php');
+    include_once(dirname(__FILE__) . '/../../../classlib2.0/FannieAPI.php');
 }
 
 /**
