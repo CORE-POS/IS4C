7d9c1d82850e3a3ba999ba6c8e63faef9564d77d
Andy Theuninck <andy@gohanman.com>
2015-06-05 13:03:20 -0500

    WFC: sloppy adjustments to target promos for June

--- fannie/modules/plugins2.0/TargetedPromos/DevelopersList.php
+++ fannie/modules/plugins2.0/TargetedPromos/DevelopersList.php
@@ -4,14 +4,14 @@ if (!class_exists('FannieAPI')) {
     include(dirname(__FILE__) . '/../../../classlib2.0/FannieAPI.php');
 }
 
-class DefectorList extends FanniePage
+class DevelopersList extends FanniePage
 {
     public function body_content()
     {
         $dbc = $this->connection;
         $settings = $this->config->PLUGIN_SETTINGS;
         $dbc->selectDB($settings['TargetedPromosDB']);
-        $model = new DefectorTargetsModel($dbc);
+        $model = new DeveloperTargetsModel($dbc);
         $model->addedDate('2015-06-01', '>');
         echo '<textarea>';
         foreach ($model->find() as $obj) {
