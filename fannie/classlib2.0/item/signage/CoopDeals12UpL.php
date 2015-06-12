2f7e2a34479fc47d4a67ce7e8d71ec2759bf84bf
Andy Theuninck <andy@gohanman.com>
2015-06-12 11:19:37 -0500

    Coop Deals 12 up signs to go with 16s, light variants

--- fannie/classlib2.0/item/signage/CoopDeals12UpL.php
+++ fannie/classlib2.0/item/signage/CoopDeals12UpL.php
@@ -23,20 +23,20 @@
 
 namespace COREPOS\Fannie\API\item\signage {
 
-class CoopDeals16UpP extends \COREPOS\Fannie\API\item\FannieSignage 
+class CoopDeals12UpL extends \COREPOS\Fannie\API\item\FannieSignage 
 {
     protected $BIG_FONT = 40;
     protected $MED_FONT = 14;
     protected $SMALL_FONT = 10;
     protected $SMALLER_FONT = 8;
-    protected $SMALLEST_FONT = 5;
+    protected $SMALLEST_FONT = 6;
 
     protected $footer_image = 'cd_line_16.png';
 
     public function drawPDF()
     {
-        $pdf = new \FPDF('P', 'mm', 'Letter');
-        $pdf->SetMargins(0, 3.175, 0);
+        $pdf = new \FPDF('L', 'mm', 'Letter');
+        $pdf->SetMargins(3.175, 3.175, 3.175);
         $pdf->SetAutoPageBreak(false);
         $pdf->AddFont('Gill', '', 'GillSansMTPro-Medium.php');
         $pdf->AddFont('Gill', 'B', 'GillSansMTPro-Heavy.php');
@@ -46,29 +46,14 @@ class CoopDeals16UpP extends \COREPOS\Fannie\API\item\FannieSignage
         $data = $this->loadItems();
         $count = 0;
         $sign = 0;
-        $width = 53.975;
-        $height = 66.68;
-        $top = 20;
-        $left = 5.175;
-        $effective_width = $width - (2*$left);
-        $pdf->SetDrawColor(0, 0, 0);
-        $pdf->SetLineWidth(0.2);
+        $width = 68.67;
+        $height = 71;
+        $top = 21;
+        $left = 8.5;
+        $effective_width = $width - $left;
         foreach ($data as $item) {
-            if ($count % 16 == 0) {
+            if ($count % 12 == 0) {
                 $pdf->AddPage();
-                // draw tick marks for cutting
-                $pdf->Line(2, $height+1.5, 6, $height+1.5);
-                $pdf->Line(2, (2*$height)+1.5, 6, (2*$height)+1.5);
-                $pdf->Line(2, (3*$height)+1.5, 6, (3*$height)+1.5);
-                $pdf->Line($width, 2, $width, 6);
-                $pdf->Line(2*$width, 2, 2*$width, 6);
-                $pdf->Line(3*$width, 2, 3*$width, 6);
-                $pdf->Line($width, (4*$height)-4, $width, 4*$height);
-                $pdf->Line(2*$width, (4*$height)-4, 2*$width, 4*$height);
-                $pdf->Line(3*$width, (4*$height)-4, 3*$width, 4*$height);
-                $pdf->Line(4*$width-6, $height+1.5, 4*$width-2, $height+1.5);
-                $pdf->Line(4*$width-6, (2*$height)+1.5, 4*$width-2, (2*$height)+1.5);
-                $pdf->Line(4*$width-6, (3*$height)+1.5, 4*$width-2, (3*$height)+1.5);
                 $sign = 0;
             }
 
@@ -85,37 +70,27 @@ class CoopDeals16UpP extends \COREPOS\Fannie\API\item\FannieSignage
                 $price = $this->formatPrice($item['normal_price']);
             }
 
-            $pdf->Image(dirname(__FILE__) . '/cd_head_16.png', ($left-2) + ($width*$column), ($top-17) + ($row*$height), $width-6);
+            $pdf->Image(dirname(__FILE__) . '/cd_head_16.png', ($left-1) + ($width*$column), ($top-19) + ($row*$height), $width-6);
 
-            $pdf->SetXY($left + ($width*$column), $top + ($row*$height) - 2);
+            $pdf->SetXY($left + ($width*$column), $top + ($row*$height));
             $pdf->SetFont('Gill', 'B', $this->SMALL_FONT);
-            $font_shrink = 0;
-            while (true) {
-                $pdf->SetX($left + ($width*$column));
-                $y = $pdf->GetY();
-                $pdf->MultiCell($effective_width, 6, strtoupper($item['brand']), 0, 'C');
-                if ($pdf->GetY() - $y > 6) {
-                    $pdf->SetFillColor(0xff, 0xff, 0xff);
-                    $pdf->Rect($left + ($width*$column), $y, $left + ($width*$column) + $effective_width, $pdf->GetY(), 'F');
-                    $font_shrink++;
-                    if ($font_shrink >= $this->SMALL_FONT) {
-                        break;
-                    }
-                    $pdf->SetFontSize($this->MED_FONT - $font_shrink);
-                    $pdf->SetXY($left + ($width*$column), $y);
-                } else {
-                    break;
-                }
-            }
+            $pdf->MultiCell($effective_width, 7, strtoupper($item['brand']), 0, 'C');
 
-            $pdf->SetX($left + ($width*$column));
+            /**
+              This block attempts to write the description then
+              checks how many lines it took. If the description was
+              longer than two lines, it whites the whole thing out,
+              drops one font size, and tries again. Calculating
+              effective text size with smart line breaks seems
+              really tough.
+            */
             $pdf->SetFont('Gill', '', $this->MED_FONT);
             $font_shrink = 0;
             while (true) {
                 $pdf->SetX($left + ($width*$column));
                 $y = $pdf->GetY();
-                $pdf->MultiCell($effective_width, 6, $item['description'], 0, 'C');
-                if ($pdf->GetY() - $y > 12) {
+                $pdf->MultiCell($effective_width, 7, $item['description'], 0, 'C');
+                if ($pdf->GetY() - $y > 14) {
                     $pdf->SetFillColor(0xff, 0xff, 0xff);
                     $pdf->Rect($left + ($width*$column), $y, $left + ($width*$column) + $effective_width, $pdf->GetY(), 'F');
                     $font_shrink++;
@@ -125,7 +100,7 @@ class CoopDeals16UpP extends \COREPOS\Fannie\API\item\FannieSignage
                     $pdf->SetFontSize($this->MED_FONT - $font_shrink);
                     $pdf->SetXY($left + ($width*$column), $y);
                 } else {
-                    if ($pdf->GetY() - $y < 12) {
+                    if ($pdf->GetY() - $y < 14) {
                         $words = explode(' ', $item['description']);
                         $multi = '';
                         for ($i=0;$i<count($words)/2;$i++) {
@@ -139,7 +114,7 @@ class CoopDeals16UpP extends \COREPOS\Fannie\API\item\FannieSignage
                         $pdf->SetFillColor(0xff, 0xff, 0xff);
                         $pdf->Rect($left + ($width*$column), $y, $left + ($width*$column) + $effective_width, $pdf->GetY(), 'F');
                         $pdf->SetXY($left + ($width*$column), $y);
-                        $pdf->MultiCell($effective_width, 6, $item['description'], 0, 'C');
+                        $pdf->MultiCell($effective_width, 7, $item['description'], 0, 'C');
                     }
                     break;
                 }
@@ -159,39 +134,40 @@ class CoopDeals16UpP extends \COREPOS\Fannie\API\item\FannieSignage
                 $item['size'] = 'PLU# ' . ltrim($item['upc'], '0'); // show PLU #s on by-weight
             }
             $pdf->Cell($effective_width, 6, $item['size'], 0, 1, 'C');
-            $pdf->SetXY($left + ($width*$column), $top + ($height*$row) + ($height-$top-18));
+
+            $pdf->SetXY($left + ($width*$column), $top + ($height*$row) + ($height - 41));
             $pdf->SetFont('Gill', '', $this->BIG_FONT);
-            $pdf->MultiCell($effective_width, 10, $price, 0, 'C');
+            $pdf->Cell($effective_width, 12, $price, 0, 1, 'C');
 
             if ($item['startDate'] != '' && $item['endDate'] != '') {
                 // intl would be nice
                 $datestr = date('M d', strtotime($item['startDate']))
                     . chr(0x96) // en dash in cp1252
                     . date('M d', strtotime($item['endDate']));
-                $pdf->SetXY($left + ($width*$column), $top + ($height*$row) + ($height - $top - 7));
+                $pdf->SetXY($left + ($width*$column), $top + ($height*$row) + ($height - 36));
                 $pdf->SetFont('GillBook', '', $this->SMALLEST_FONT);
-                $pdf->Cell($effective_width+4, 6, strtoupper($datestr), 0, 1, 'R');
+                $pdf->Cell($effective_width, 20, strtoupper($datestr), 0, 1, 'R');
             }
 
-            if ($item['upc'] != '') {
-                $pdf->SetXY($left-2 + ($width*$column), $top + ($height*$row) + ($height - $top - 7));
+            if ($item['originShortName'] != '') {
+                $pdf->SetXY($left + ($width*$column), $top + ($height*$row) + ($height - 36));
                 $pdf->SetFont('GillBook', '', $this->SMALLEST_FONT);
                 $pdf->Cell($effective_width, 6, $item['upc'], 0, 1, 'L');
             }
 
-            $pdf->Image(dirname(__FILE__) . '/' . $this->footer_image, ($left-2)+($width*$column), $top + ($height*$row) + ($height-$top-2), $width-6);
+            $pdf->Image(dirname(__FILE__) . '/' . $this->footer_image, ($left-1)+($width*$column), $top + ($height*$row) + ($height-$top-3), $width-6);
 
             $count++;
             $sign++;
         }
 
-        $pdf->Output('Signage16UpP.pdf', 'I');
+        $pdf->Output('Signage12UpL.pdf', 'I');
     }
 }
 
 }
 
 namespace {
-    class CoopDeals16UpP extends \COREPOS\Fannie\API\item\signage\CoopDeals16UpP {}
+    class CoopDeals12UpL extends \COREPOS\Fannie\API\item\signage\CoopDeals12UpL {}
 }
 
