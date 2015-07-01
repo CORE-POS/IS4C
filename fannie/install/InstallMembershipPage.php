8660700fd7c6f9d15c1ca2d6527aae235cc9fd9f
Andy Theuninck <andy@gohanman.com>
2015-07-01 10:55:38 -0500

    BUGFIX: Membership tab of the install/config page was dropping views and failing to re-create them

--- fannie/install/InstallMembershipPage.php
+++ fannie/install/InstallMembershipPage.php
@@ -199,38 +199,29 @@ else {
 
     // rebuild views that depend on ar & equity
     // department definitions
-    function recreate_views($con){
-        global $FANNIE_TRANS_DB,$FANNIE_OP_DB,$FANNIE_SERVER_DBMS;
+    function recreate_views($con)
+    {
+        $con->query("DROP VIEW ar_history_today",$FANNIE_TRANS_DB);
+        $model = new ArHistoryTodayModel($con);
+        $model->createIfNeeded($this->config->get('TRANS_DB'));
 
         $con->query("DROP VIEW ar_history_today_sum",$FANNIE_TRANS_DB);
-        create_if_needed($con,$FANNIE_SERVER_DBMS,$FANNIE_TRANS_DB,
-                'ar_history_today_sum','trans');
+        $model = new ArHistoryTodaySumModel($con);
+        $model->createIfNeeded($this->config->get('TRANS_DB'));
 
         $con->query("DROP VIEW ar_live_balance",$FANNIE_TRANS_DB);
-        create_if_needed($con,$FANNIE_SERVER_DBMS,$FANNIE_TRANS_DB,
-                'ar_live_balance','trans');
+        $model = new ArLiveBalanceModel($con);
+        $model->addExtraDB($this->config->get('OP_DB'));
+        $model->createIfNeeded($this->config->get('TRANS_DB'));
 
         $con->query("DROP VIEW stockSumToday",$FANNIE_TRANS_DB);
-        create_if_needed($con,$FANNIE_SERVER_DBMS,$FANNIE_TRANS_DB,
-                'stockSumToday','trans');
+        $model = new StockSumTodayModel($con);
+        $model->createIfNeeded($this->config->get('TRANS_DB'));
 
         $con->query("DROP VIEW equity_live_balance",$FANNIE_TRANS_DB);
-        create_if_needed($con,$FANNIE_SERVER_DBMS,$FANNIE_TRANS_DB,
-                'equity_live_balance','trans');
-
-        if ($con->tableExists('newBalanceStockToday_test')) {
-            $con->query("DROP VIEW newBalanceStockToday_test",$FANNIE_TRANS_DB);
-            create_if_needed($con,$FANNIE_SERVER_DBMS,$FANNIE_TRANS_DB,
-                    'newBalanceStockToday_test','trans');
-        }
-
-        $con->query("DROP VIEW dheader",$FANNIE_TRANS_DB);
-        create_if_needed($con,$FANNIE_SERVER_DBMS,$FANNIE_TRANS_DB,
-                'dheader','trans');
-
-        $con->query("DROP VIEW ar_history_today",$FANNIE_TRANS_DB);
-        create_if_needed($con,$FANNIE_SERVER_DBMS,$FANNIE_TRANS_DB,
-                'ar_history_today','trans');
+        $model = new EquityLiveBalanceModel($con);
+        $model->addExtraDB($this->config->get('OP_DB'));
+        $model->createIfNeeded($this->config->get('TRANS_DB'));
     }
 
 // InstallMembershipPage
