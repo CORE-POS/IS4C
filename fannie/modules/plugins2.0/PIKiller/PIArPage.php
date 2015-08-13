8511e75d2936742dbaf76347be8c0eb743c12463
Andy Theuninck <andy@gohanman.com>
2015-08-13 15:25:50 -0500

    Only lood first 100 AR receipts in PI by default

--- fannie/modules/plugins2.0/PIKiller/PIArPage.php
+++ fannie/modules/plugins2.0/PIKiller/PIArPage.php
@@ -29,26 +29,34 @@ if (!class_exists('PIKillerPage')) {
     include('lib/PIKillerPage.php');
 }
 
-class PIArPage extends PIKillerPage {
-
-    protected function get_id_handler(){
+class PIArPage extends PIKillerPage 
+{
+    protected function get_id_handler()
+    {
         global $FANNIE_TRANS_DB;
         $this->card_no = $this->id;
 
         $this->title = 'AR History : Member '.$this->card_no;
 
-        $this->__models['ar'] = $this->get_model(FannieDB::get($FANNIE_TRANS_DB), 'ArHistoryModel',
-                        array('card_no'=>$this->id),'tdate');
-        $this->__models['ar'] = array_reverse($this->__models['ar']);
-    
-        return True;
+        $this->all = FormLib::get('all', 0);
+        $this->connection->selectDB($this->config->get('TRANS_DB'));
+        $model = new ArHistoryModel($this->connection);
+        $model->card_no($this->id);
+        if (!$this->all) {
+            $model->setFindLimit(100);
+        }
+        $this->__models['ar'] = $model->find('tdate', true);
+        
+        return true;
     }
 
-    protected function get_id_view(){
+    protected function get_id_view()
+    {
         global $FANNIE_URL;
         echo '<table border="1" style="background-color: #ffff99;">';
         echo '<tr align="left"></tr>';
-        foreach($this->__models['ar'] as $transaction){
+        $rowcount = 0;
+        foreach ($this->__models['ar'] as $transaction) {
             $stamp = strtotime($transaction->tdate());
             if ($transaction->Payments() != 0){
                 printf('<tr>
@@ -74,11 +82,17 @@ class PIArPage extends PIKillerPage {
                     $transaction->card_no()
                 );
             }
+            $rowcount++;
+            if ($rowcount > 100 && !$this->all) {
+                break;
+            }
         }
         echo '</table>';
+        if (!$this->all) {
+            echo ' <a href="?id=' . $this->id . '&all=1">Show Entire AR History</a>';
+        }
     }
 }
 
 FannieDispatch::conditionalExec();
 
-?>
