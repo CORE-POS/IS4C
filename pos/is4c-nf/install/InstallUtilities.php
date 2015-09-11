49bcfe168f5e7fff67ff04600d747783b5fff497
Andy Theuninck <andy@gohanman.com>
2015-09-11 11:24:26 -0500

    New lane configuration UI for NewMagellan driver options

--- pos/is4c-nf/install/InstallUtilities.php
+++ pos/is4c-nf/install/InstallUtilities.php
@@ -235,7 +235,7 @@ class InstallUtilities extends LibraryClass
       @param $value string value
       @return boolean success
     */
-    static private function jsonConfSave($key, $value)
+    static public function jsonConfSave($key, $value)
     {
         $ini_json = dirname(__FILE__) . '/../ini.json';
         if (!is_writable($ini_json)) {
