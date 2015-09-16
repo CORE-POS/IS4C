98abc21adb5d409e6f72a0a6d369517000d27aef
Andy Theuninck <andy@gohanman.com>
2015-09-16 17:02:03 -0500

    BUGFIX: Markup/UI problems in household separate discount
    Conflicts:
    	fannie/mem/modules/HouseholdSeparateDiscounts.php

--- fannie/mem/modules/HouseholdSeparateDiscounts.php
+++ fannie/mem/modules/HouseholdSeparateDiscounts.php
@@ -54,15 +54,17 @@ class HouseholdSeparateDiscounts extends \COREPOS\Fannie\API\member\MemberModule
                     </div>
                     <label>
                         <span class="label primaryBackground">Checks</span>
-                        <input type="checkbox" name="HouseholdSeparateDiscounts_writeChecks[] value="%d" />
+                        <input type="checkbox" name="HouseholdSeparateDiscounts_writeChecks[]" %s value="%d" />
                     </label>
                     <label>
                         <span class="label primaryBackground">FFA</span>
-                        <input type="checkbox" name="HouseholdSeparateDiscounts_SSI[] value="%d" />
+                        <input type="checkbox" name="HouseholdSeparateDiscounts_SSI[]" %s value="%d" />
                     </label>
                 </div>',
                 $infoW['firstName'],$infoW['lastName'],$infoW['discount'],
-                $count+2, $count+2); // $count+2 == personNum
+                ($infoW['checksAllowed'] ? 'checked' : ''), $count+2, 
+                ($infoW['lowIncomeBenefits'] ? 'checked' : ''), $count+2, 
+                $infoW['customerID']); // $count+2 == personNum
             $count++;
         }
 
