d2ae81578517787f3018db02bc3b1fd80b8817f9
Andy Theuninck <andy@gohanman.com>
2015-10-21 13:43:02 -0500

    Use one test suite to control execution order

--- tests/TestListener.php
+++ tests/TestListener.php
@@ -5,11 +5,15 @@ class TestListener extends PHPUnit_Framework_BaseTestListener
     public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
     {
         if ($suite->getName() == "pos") {
-            var_dump("re-bootstrap");
             include(dirname(__FILE__) . '/../pos/is4c-nf/unit-tests/bootstrap.php');
         } else {
-            var_dump($suite->getName());
+            var_dump('SUITE: ' . $suite->getName());
         }
+    }
+
+    public function startTest(PHPUnit_Framework_Test $test)
+    {
+        var_dump('TEST: ' . $test->getName());
     }
 }
 
