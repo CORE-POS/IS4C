007442bd8eff044024ddfccb42ce39ad9da49277
Andy Theuninck <andy@gohanman.com>
2016-01-07 15:11:38 -0600

    Forgot argument in testing ItemModule::getFormJavascript

--- tests/fannie/ItemsTest.php
+++ tests/fannie/ItemsTest.php
@@ -18,7 +18,7 @@ class ItemsTest extends PHPUnit_Framework_TestCase
             $this->assertNotEquals(0, strlen($obj->showEditForm('0000000004011')));
             $this->assertInternalType('int', $obj->width());
             $this->assertInternalType('array', $obj->summaryRows('0000000004011'));
-            $this->assertInternalType('string', $obj->getFormJavascript());
+            $this->assertInternalType('string', $obj->getFormJavascript('0000000004011'));
         }
     }
 
