0bcb0e260b21016aa2c2b0fb3ebc77fce4fb6a97
Andy Theuninck <andy@gohanman.com>
2016-02-10 17:47:37 -0600

    Forgot to actually pass the $row into the UPC result handler

--- pos/is4c-nf/parser-class-lib/parse/UPC.php
+++ pos/is4c-nf/parser-class-lib/parse/UPC.php
@@ -142,11 +142,11 @@ class UPC extends Parser
         if (!$row) {
             return $this->nonProductUPCs($upc, $ret);
         } else {
-            return $this->handleItem($upc, $scaleStickerItem, $scalepriceUPC, $scalepriceEAN);
+            return $this->handleItem($upc, $row, $scaleStickerItem, $scalepriceUPC, $scalepriceEAN);
         }
     }
 
-    private function handleItem($upc, $scaleStickerItem, $scalepriceUPC, $scalepriceEAN)
+    private function handleItem($upc, $row, $scaleStickerItem, $scalepriceUPC, $scalepriceEAN)
     {
         $ret = $this->default_json();
         $my_url = MiscLib::base_url();
