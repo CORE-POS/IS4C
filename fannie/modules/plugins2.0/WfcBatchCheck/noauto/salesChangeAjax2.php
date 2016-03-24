a1c64fa009fa6cd3aee89eadc24656b9b6be1ec4
Andy Theuninck <andy@gohanman.com>
2016-03-24 13:47:42 -0500

    New plugin: WFC Batch Sign Checker

---
+++ fannie/modules/plugins2.0/WfcBatchCheck/noauto/salesChangeAjax2.php
@@ -0,0 +1,41 @@
+<?php 
+include('/var/www/html/git/fannie/config.php');
+include('/var/www/html/git/fannie/classlib2.0/FannieAPI.php');
+?>
+<style>
+p {
+    font-size: 30;
+    text-align: center;
+    border: 1px solid black;
+    background-color: #acfaf7;
+}
+</style>
+
+<?php
+session_start();
+$upc = $_GET['upc'];
+$queue = $_GET['queue'];
+
+echo '<p>UPC ' . $upc . ', Sent to Queue ' . $queue . '</p';
+
+$database_name = "woodshed_no_replicate";
+
+$dbc = FannieDB::get($database_name);
+
+$query = "UPDATE SaleChangeQueues
+        SET queue={$queue}
+		WHERE upc={$upc}
+        AND store_id={$_SESSION['store_id']}
+        ;";
+$result = $dbc->query($query);
+
+if ($queue == 99) {
+    $query = "INSERT INTO SaleChangeQueues (queue, upc, store_id)
+        VALUES (
+        99,
+        '{$upc}',
+        '{$_SESSION['store_id']}'
+        )
+        ;";
+    $result = $dbc->query($query);
+}
