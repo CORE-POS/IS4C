76bff35e75c40de685f85bbfdc080b85156c0de7
andy <andy@denfeld.wholefoods.coop>
2016-03-29 12:48:54 -0500

    Bad config paths in WfcBatchCheck plugin

--- fannie/modules/plugins2.0/WfcBatchCheck/noauto/SalesChangeQueuesGood.php
+++ fannie/modules/plugins2.0/WfcBatchCheck/noauto/SalesChangeQueuesGood.php
@@ -1,5 +1,5 @@
 <?php 
-include(dirname(__FILE__) '/../../../../config.php');
+include(dirname(__FILE__) . '/../../../../config.php');
 if (!class_exists('FannieAPI.php')) {
     include($FANNIE_ROOT . 'classlib2.0/FannieAPI.php');
 }
