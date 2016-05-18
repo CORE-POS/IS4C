97cbee3b40a7b10103203d056897725bb5156071
Andy Theuninck <andy@gohanman.com>
2016-05-18 14:39:27 -0500

    Add current day AR activity for #727

--- fannie/reports/AR/ArReport.php
+++ fannie/reports/AR/ArReport.php
@@ -46,14 +46,21 @@ class ArReport extends FannieReportPage
     {
         $dbc = $this->connection;
         $dbc->selectDB($this->config->get('TRANS_DB'));
-        $q = $dbc->prepare("select charges,trans_num,payments,
+        $prep = $dbc->prepare("
+            SELECT charges,trans_num,payments,
                 year(tdate),month(tdate),day(tdate)
-                from ar_history AS s 
-                WHERE s.card_no=? ORDER BY tdate DESC");
-        $r = $dbc->execute($q,array($this->form->memNum));
+            FROM ar_history_today
+            WHERE card_no=?
+            UNION ALL
+            SELECT charges,trans_num,payments,
+                year(tdate),month(tdate),day(tdate)
+            FROM ar_history
+            WHERE card_no=? 
+            ORDER BY tdate DESC");
+        $res = $dbc->execute($prep,array($this->form->memNum, $this->form->memNum));
 
         $data = array();
-        while ($row = $dbc->fetchRow($r)) {
+        while ($row = $dbc->fetchRow($res)) {
             $data[] = $this->rowToRecord($row);
         }
 
