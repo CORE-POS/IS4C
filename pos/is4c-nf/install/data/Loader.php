05fef6eca5d3322ecfdd972e9501b293627a62f9
Andy Theuninck <andy@gohanman.com>
2016-05-21 16:05:35 -0700

    Loader should get MiscLib from global namespace

--- pos/is4c-nf/install/data/Loader.php
+++ pos/is4c-nf/install/data/Loader.php
@@ -131,7 +131,7 @@ class Loader
               Handle symlinks on windows by checking if the first line
               of the file contains the name of another CSV file.
             */
-            if (MiscLib::win32()) {
+            if (\MiscLib::win32()) {
                 $fptr = fopen($path, 'r');
                 $first_line = trim(fgets($fptr));
                 if (substr($first_line, -4) == '.csv') {
