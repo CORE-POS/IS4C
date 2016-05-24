be73e50b0bb67f142d62a99af8a07667244ba477
Andy Theuninck <andy@gohanman.com>
2016-05-23 18:26:58 -0700

    Further cleanup on productList.js by adding functions to handle duplicate blocks

--- fannie/item/productList.js
+++ fannie/item/productList.js
@@ -1,70 +1,61 @@
 var productList = (function($) {
     var mod = {};
 
+    var drawCheckbox = function(cell, field) {
+        var fs = elem.find('.' + cell + ':first').html();
+        var content = "<input type=checkbox class=" + field + " "+((fs==='X')?'checked':'')+" />";
+        elem.find('.' + cell + ':first').html(content);
+    };
+
+    var drawTextBox = function(cell, field, size) {
+        var brand = elem.find('.' + cell + ':first').html();
+        var content = "<input type=text class=\"" + field + " form-control input-sm\" size="+size+" value=\""+brand+"\" />";   
+        elem.find('.' + cell + ':first').html(content);
+    };
+
+    var drawKeyValSelect = function(cell, field, obj) {
+        var dept = elem.find('.' + cell + ':first').text();
+        var content = '<select class=\"" + field + " form-control input-sm\"><optgroup style="font-size: 90%;">';
+        for (var i in obj) {
+            if (obj.hasOwnProperty(i)) {
+                content += "<option value=\""+i+"\" "+((dept==obj[i])?'selected':'')+">";
+                content += obj[i]+"</option>";
+            }
+        }
+        content += '</optgroup></select>';
+        elem.find('.' + cell + ':first').html(content);
+    };
+
+    var drawTupleSelect = function(cell, field, obj) {
+        var tax = elem.find('.' + cell + ':first').html();
+        var content = '<select class=\"" + field + " form-control input-sm\">';
+        for (var ch in obj) {
+            if (obj.hasOwnProperty(ch)) {
+                var t_sel = (tax == ch) ? 'selected' : '';
+                content += "<option value=\""+ch+":"+obj[ch][0]+"\" "+t_sel+">";
+                content += obj[ch][1]+"</option>";
+            }
+        }
+        elem.find('.' + cell + ':first').html(content);
+    };
+
     mod.edit = function(elem) {
-        var brand = elem.find('.td_brand:first').html();
-        var content = "<input type=text class=\"in_brand form-control input-sm\" size=8 value=\""+brand+"\" />";   
-        elem.find('.td_brand:first').html(content);
+        drawTextBox('td_brand', 'in_brand', 8);
+        drawTextBox('td_desc', 'in_desc', 10);
 
-        var desc = elem.find('.td_desc:first').html();
-        content = "<input type=text class=\"in_desc form-control input-sm\" size=10 value=\""+desc+"\" />";   
-        elem.find('.td_desc:first').html(content);
+        drawKeyValSelect('td_dept', 'in_dept', deptObj);
+        drawKeyValSelect('td_supplier', 'in_supplier', vendorObj);
 
-        var dept = elem.find('.td_dept:first').text();
-        content = '<select class=\"in_dept form-control input-sm\"><optgroup style="font-size: 90%;">';
-        for (var dept_no in deptObj) {
-            content += "<option value=\""+dept_no+"\" "+((dept==deptObj[dept_no])?'selected':'')+">";
-            content += deptObj[dept_no]+"</option>";
-        }
-        content += '</optgroup></select>';
-        elem.find('.td_dept:first').html(content);
+        drawTextBox('td_cost', 'in_cost', 4);
+        drawTextBox('td_price', 'in_price', 4);
 
-        var supplier = elem.find('.td_supplier:first').text();
-        content = '<select class=\"in_supplier form-control input-sm\"><optgroup style="font-size: 90%;">';
-        for(var i in vendorObj){
-            content += "<option "+((supplier==vendorObj[i])?'selected':'')+">";
-            content += vendorObj[i]+"</option>";
-        }
-        content += '</optgroup></select>';
-        elem.find('.td_supplier:first').html(content);
+        drawTupleSelect('td_tax', 'in_tax', taxObj);
 
-        var cost = elem.find('.td_cost:first').html();
-        content = "<input type=text class=\"in_cost form-control input-sm\" size=4 value=\""+cost+"\" />";    
-        elem.find('.td_cost:first').html(content);
+        drawCheckbox('td_fs', 'in_fs');
+        drawCheckbox('td_disc', 'in_disc');
+        drawCheckbox('td_wgt', 'in_wgt');
 
-        var price = elem.find('.td_price:first').html();
-        content = "<input type=text class=\"in_price form-control input-sm\" size=4 value=\""+price+"\" />";  
-        elem.find('.td_price:first').html(content);
-
-        var tax = elem.find('.td_tax:first').html();
-        content = '<select class=\"in_tax form-control input-sm\">';
-        for (var ch in taxObj) {
-            var t_sel = (tax == ch) ? 'selected' : '';
-            content += "<option value=\""+ch+":"+taxObj[ch][0]+"\" "+t_sel+">";
-            content += taxObj[ch][1]+"</option>";
-        }
-        elem.find('.td_tax:first').html(content);
-
-        var fs = elem.find('.td_fs:first').html();
-        content = "<input type=checkbox class=in_fs "+((fs==='X')?'checked':'')+" />";
-        elem.find('.td_fs:first').html(content);
-
-        var disc = elem.find('.td_disc:first').html();
-        content = "<input type=checkbox class=in_disc "+((disc==='X')?'checked':'')+" />";
-        elem.find('.td_disc:first').html(content);
-
-        var wgt = elem.find('.td_wgt:first').html();
-        content = "<input type=checkbox class=in_wgt "+((wgt==='X')?'checked':'')+" />";
-        elem.find('.td_wgt:first').html(content);
-
-        var local = elem.find('.td_local:first').html();
-        content = '<select class=\"in_local form-control input-sm\">';
-        for (var ch in localObj){
-            var l_sel = (local == ch) ? 'selected' : '';
-            content += "<option value=\""+ch+":"+localObj[ch][0]+"\" "+l_sel+">";
-            content += localObj[ch][1]+"</option>";
-        }
-        elem.find('.td_local:first').html(content);
+        drawTupleSelect('td_local', 'in_local', taxObj);
 
         elem.find('.td_cmd:first .edit-link').hide();
         elem.find('.td_cmd:first .save-link').show();
@@ -84,40 +75,40 @@ var productList = (function($) {
         });
     };
 
+    var formToCell = function(cell, field) {
+        var brand = elem.find('.' + field + ':first').val();
+        elem.find('.' + cell + ':first').html(brand);
+        return brand;
+    };
+
+    var checkBoxToCell = function(cell, field) {
+        var fs = elem.find('.'+field+':first').is(':checked') ? 1 : 0;
+        elem.find('.'+cell+':first').html((fs===1)?'X':'-');
+        return fs;
+    };
+
     mod.save = function(elem) {
         var upc = elem.find('.hidden_upc:first').val();
         var store_id = elem.find('.hidden_store_id:first').val();
 
-        var brand = elem.find('.in_brand:first').val();
-        elem.find('.td_brand:first').html(brand);
-
-        var desc = elem.find('.in_desc:first').val();
-        elem.find('.td_desc:first').html(desc);
+        var brand = formToCell('td_brand', 'in_brand');
+        var desc = formToCell('td_desc', 'in_desc');
 
         var dept = elem.find('.in_dept:first').val();
         elem.find('.td_dept:first').html(deptObj[dept]);
 
-        var supplier = elem.find('.in_supplier:first').val();
-        elem.find('.td_supplier:first').html(supplier);
+        var supplier = formToCell('td_supplier', 'in_supplier');
 
         mathField(elem.find('.in_cost:first').get(0));
-        var cost = elem.find('.in_cost:first').val();
-        elem.find('.td_cost:first').html(cost);
-
-        var price = elem.find('.in_price:first').val();
-        elem.find('.td_price:first').html(price);
+        var cost = formToCell('td_cost', 'in_cost');
+        var price = formToCell('td_price', 'in_price');
 
         var tax = elem.find('.in_tax:first').val().split(':');
         elem.find('.td_tax:first').html(tax[0]);
         
-        var fs = elem.find('.in_fs:first').is(':checked') ? 1 : 0;
-        elem.find('.td_fs:first').html((fs===1)?'X':'-');
-
-        var disc = elem.find('.in_disc:first').is(':checked') ? 1 : 0;
-        elem.find('.td_disc:first').html((disc===1)?'X':'-');
-
-        var wgt = elem.find('.in_wgt:first').is(':checked') ? 1 : 0;
-        elem.find('.td_wgt:first').html((wgt===1)?'X':'-');
+        var fs = checkBoxToCell('td_fs', 'in_fs');
+        var disc = checkBoxToCell('td_disc', 'in_disc');
+        var wgt = checkBoxToCell('td_wgt', 'in_wgt');
 
         var local = elem.find('.in_local:first').val().split(':');
         elem.find('.td_local:first').html(local[0]);
@@ -133,7 +124,6 @@ var productList = (function($) {
         $.ajax({
             url: 'ProductListPage.php',
             data: dstr,
-            cache: false,
             type: 'post'
         });
     };
@@ -143,22 +133,20 @@ var productList = (function($) {
             url: 'ProductListPage.php',
             data: 'ajax=deleteCheck&upc='+upc+'&desc='+desc,
             dataType: 'json',
-            cache: false,
             type: 'post'
         }).done(function(data) {
             if (data.alertBox && data.upc && data.enc_desc){
-                if (confirm(data.alertBox)){
+                if (window.confirm(data.alertBox)){
                     $.ajax({
                         url: 'ProductListPage.php',
                         data: 'ajax=doDelete&upc='+upc+'&desc='+data.enc_desc,
-                        cache: false,
                         type: 'post'
-                    }).done(function(data){
+                    }).done(function(){
                         $('#' + upc).remove();
                     });
                 }
             } else {
-                alert('Data error: cannot delete');
+                window.alert('Data error: cannot delete');
             }
         });
     };
@@ -166,7 +154,6 @@ var productList = (function($) {
     mod.enableEditing = function() {
         $('tr').each(function(){
             if ($(this).find('.hidden_upc').length !== 0) {
-                var upc = $(this).find('.hidden_upc').val();
                 $(this).find('.clickable').click(function() {
                     if ($(this).find(':input').length === 0) {
                         mod.edit($(this).closest('tr'));
