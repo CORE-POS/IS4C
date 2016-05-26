65b7e192a98cf957e64d7a6ddd525a85797c2499
Andy Theuninck <andy@gohanman.com>
2016-05-26 13:55:54 -0500

    Log forced-batch changes earlier

--- fannie/classlib2.0/data/models/op/BatchesModel.php
+++ fannie/classlib2.0/data/models/op/BatchesModel.php
@@ -402,6 +402,9 @@ those same items revert to normal pricing.
             $upcs[$w['upc']] = $w;
         }
 
+        $update = new ProdUpdateModel($this->connection);
+        $update->logManyUpdates(array_keys($upcs), $updateType);
+
         $updateQ = '
             UPDATE products AS p SET
                 p.normal_price = ?,
@@ -465,9 +468,6 @@ those same items revert to normal pricing.
                 $client->send();
             }
         }
-
-        $update = new ProdUpdateModel($this->connection);
-        $update->logManyUpdates(array_keys($upcs), $updateType);
     }
 
     /**
