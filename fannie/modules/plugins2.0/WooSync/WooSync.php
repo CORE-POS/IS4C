beba4507b9d6ea4daa35352b4ffb443fc11e62a3
Andy Theuninck <andy@gohanman.com>
2016-06-18 07:57:27 -0700

    Basic syncing for woo #759

---
+++ fannie/modules/plugins2.0/WooSync/WooSync.php
@@ -0,0 +1,42 @@
+<?php
+/*******************************************************************************
+
+    Copyright 2014 Whole Foods Co-op
+
+    This file is part of IT CORE.
+
+    IT CORE is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    IT CORE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    in the file license.txt along with IT CORE; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*********************************************************************************/
+
+include(dirname(__FILE__).'/../../../config.php');
+if (!class_exists('FannieAPI')) {
+    include_once($FANNIE_ROOT.'classlib2.0/FannieAPI.php');
+}
+
+/**
+*/
+class WooSync extends \COREPOS\Fannie\API\FanniePlugin 
+{
+    public $plugin_settings = array(
+    'WooUrl' => array('default'=>'', 'label'=>'Woo API URL',
+            'description'=>'URL for the WooCommerce REST API'), 
+    'WooKey' => array('default'=>'', 'label'=>'Woo Consumer Key'),
+    'WooSecret' => array('default'=>'', 'label'=>'Woo Consumer Secret'),
+    );
+
+    public $plugin_description = 'Plugin for syncing data to a WooCommerce site';
+}
+
