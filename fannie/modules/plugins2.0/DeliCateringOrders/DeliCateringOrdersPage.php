dc32ec23383a897f6f18352ace15984c76602984
Andy Theuninck <andy@gohanman.com>
2016-07-15 09:21:29 -0500

    Tabs to spaces

--- fannie/modules/plugins2.0/DeliCateringOrders/DeliCateringOrdersPage.php
+++ fannie/modules/plugins2.0/DeliCateringOrders/DeliCateringOrdersPage.php
@@ -1518,32 +1518,32 @@ function autoFill()
     var x = document.getElementById("orderform");
     var card_no = x.elements[4].value;
     $.ajax({
-		type: 'get',
+        type: 'get',
         url: 'DeliCateringAjax.php',
-		dataType: 'json',
+        dataType: 'json',
         data: 'card_no='+card_no,
-		error: function(xhr, status, error)
-		{ 
-			alert('error:' + status + ':' + error + ':' + xhr.responseText) 
-		},
+        error: function(xhr, status, error)
+        { 
+            alert('error:' + status + ':' + error + ':' + xhr.responseText) 
+        },
         success: function(response)
         {
         }
     })
-	.done(function(data){
-		if (data.name) {
-			$('#name').val(data.name);
-		}
-		if (data.phone) {
-			$('#phone').val(data.phone);
-		}
-		if (data.altPhone) {
-			$('#altPhone').val(data.altPhone);
-		}
-		if (data.email) {
-			$('#email').val(data.email);
-		}
-	})
+    .done(function(data){
+        if (data.name) {
+            $('#name').val(data.name);
+        }
+        if (data.phone) {
+            $('#phone').val(data.phone);
+        }
+        if (data.altPhone) {
+            $('#altPhone').val(data.altPhone);
+        }
+        if (data.email) {
+            $('#email').val(data.email);
+        }
+    })
 }
         <?php
         return ob_get_clean();
