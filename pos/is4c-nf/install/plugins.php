08f55add7b957513d3dd4cdea712bd7c239339d8
Andy Theuninck <andy@gohanman.com>
2016-07-26 15:21:50 -0500

    BUGFIX: save issues on plugins tab

--- pos/is4c-nf/install/plugins.php
+++ pos/is4c-nf/install/plugins.php
@@ -50,10 +50,10 @@ if (isset($_REQUEST['PLUGINLIST']) || isset($_REQUEST['psubmit'])){
             $obj->pluginDisable();
         }
     }
-    CoreLocal::set('PluginList',$_REQUEST['PLUGINLIST']);
+    CoreLocal::set('PluginList',$_REQUEST['PLUGINLIST'], true);
 }
 $type_check = CoreLocal::get('PluginList');
-if (!is_array($type_check)) CoreLocal::set('PluginList',array());
+if (!is_array($type_check)) CoreLocal::set('PluginList',array(), true);
 
 $mods = AutoLoader::listModules('COREPOS\\pos\\plugins\\Plugin');
 sort($mods);
