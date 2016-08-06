afb41aeb73d1d4a204973c0397934b119faa79b5
Finn <finn@callpipe.com>
2016-08-06 16:05:19 -0700

    Zebra Single label supports multiple labels, one per page

--- fannie/admin/labels/pdf_layouts/Zebra_Single_Label.php
+++ fannie/admin/labels/pdf_layouts/Zebra_Single_Label.php
@@ -56,7 +56,6 @@ if (!class_exists('FpdfWithBarcode')) {
     $pdf=new Zebra_Single_Label('L', 'mm', array(31.75, 57.15));
     $pdf->SetMargins(0, 0);
     $pdf->SetAutoPageBreak('off', 0);
-    $pdf->AddPage('L');
     $pdf->SetFont('Arial', '', 10);
 
     /**
@@ -66,7 +65,6 @@ if (!class_exists('FpdfWithBarcode')) {
     $descTop = $top + $hspace;
     $barTop = $descTop + 16;
     $priceTop = $descTop + 4;
-    $labelCount = 0;
     $brandTop = $descTop + 4;
     $sizeTop = $descTop + 8;
     $genLeft = $left;
@@ -76,60 +74,57 @@ if (!class_exists('FpdfWithBarcode')) {
     $LeftShift = 51.990625;
     $priceLeft = ($w / 2) + ($space);
 
-    /**
-    * instantiate variables for printing on barcode from
-    * $testQ query result set
-    */
-    if ($data[0]['scale'] == 0) {$price = $data[0]['normal_price'];}
-    elseif ($data[0]['scale'] == 1) {$price = $data[0]['normal_price'] . "/lb";}
-    $desc = strtoupper(substr($data[0]['description'],0,27));
-    $brand = ucwords(strtolower(substr($data[0]['brand'],0,13)));
-    $pak = $data[0]['units'];
-    $size = $data[0]['units'] . "-" . $data[0]['size'];
-    $sku = $data[0]['sku'];
-    $upc = substr($data[0]['upc'],1,12);
-    /**
-    * determine check digit using barcode.php function
-    */
-    $check = $pdf->GetCheckDigit($upc);
-    /**
-    * get tag creation date (today)
-    */
-    $tagdate = date('m/d/y');
-    $vendor = substr($data[0]['vendor'],0,7);
+    foreach($data as $item) {
+      $pdf->AddPage('L');
 
-    /**
-    * begin creating tag
-    */
-    $pdf->SetXY($genLeft, $descTop);
-    $pdf->Cell($w,4,substr($desc,0,20),0,0,'L');
-    $pdf->SetXY($genLeft,$brandTop);
-    $pdf->Cell($w/2,4,$brand,0,0,'L');
-    $pdf->SetXY($genLeft,$sizeTop);
-    $pdf->Cell($w/2,4,$size,0,0,'L');
-    $pdf->SetXY($priceLeft+9,$skuTop);
-    $pdf->Cell($w/3,4,$tagdate,0,0,'R');
-    // $pdf->SetFont('Arial','',10);
-    $pdf->SetXY($genLeft,$skuTop);
-    $pdf->Cell($w/3,4,$sku,0,0,'L');
-    $pdf->SetXY($vendLeft,$skuTop);
-    $pdf->Cell($w/3,4,$vendor,0,0,'C');
-    $pdf->SetFont('Arial','B',20);
-    $pdf->SetXY($priceLeft,$priceTop);
-    $pdf->Cell($w/2,8,$price,0,0,'R');
-    /**
-    * add check digit to pid from testQ
-    */
-    $newUPC = $upc . $check;
-    $pdf->UPC_A($barLeft, $barTop, $upc, 10);
-    /**
-    * increment label parameters for next label
-    */
-    $barLeft =$barLeft + $LeftShift;
-    $priceLeft = $priceLeft + $LeftShift;
-    $genLeft = $genLeft + $LeftShift;
-    $vendLeft = $vendLeft + $LeftShift;
-    $labelCount++;
+
+      /**
+      * instantiate variables for printing on barcode from
+      * $testQ query result set
+      */
+      if ($item['scale'] == 0) {$price = $item['normal_price'];}
+      elseif ($item['scale'] == 1) {$price = $item['normal_price'] . "/lb";}
+      $desc = strtoupper(substr($item['description'],0,27));
+      $brand = ucwords(strtolower(substr($item['brand'],0,13)));
+      $pak = $item['units'];
+      $size = $item['units'] . "-" . $item['size'];
+      $sku = $item['sku'];
+      $upc = substr($item['upc'],1,12);
+      /**
+      * determine check digit using barcode.php function
+      */
+      $check = $pdf->GetCheckDigit($upc);
+      /**
+      * get tag creation date (today)
+      */
+      $tagdate = date('m/d/y');
+      $vendor = substr($item['vendor'],0,7);
+
+      /**
+      * begin creating tag
+      */
+      $pdf->SetXY($genLeft, $descTop);
+      $pdf->Cell($w,4,substr($desc,0,20),0,0,'L');
+      $pdf->SetXY($genLeft,$brandTop);
+      $pdf->Cell($w/2,4,$brand,0,0,'L');
+      $pdf->SetXY($genLeft,$sizeTop);
+      $pdf->Cell($w/2,4,$size,0,0,'L');
+      $pdf->SetXY($priceLeft+9,$skuTop);
+      $pdf->Cell($w/3,4,$tagdate,0,0,'R');
+      // $pdf->SetFont('Arial','',10);
+      $pdf->SetXY($genLeft,$skuTop);
+      $pdf->Cell($w/3,4,$sku,0,0,'L');
+      $pdf->SetXY($vendLeft,$skuTop);
+      $pdf->Cell($w/3,4,$vendor,0,0,'C');
+      $pdf->SetFont('Arial','B',20);
+      $pdf->SetXY($priceLeft,$priceTop);
+      $pdf->Cell($w/2,8,$price,0,0,'R');
+      /**
+      * add check digit to pid from testQ
+      */
+      $newUPC = $upc . $check;
+      $pdf->UPC_A($barLeft, $barTop, $upc, 10);
+    }
 
     /**
     * write to PDF
