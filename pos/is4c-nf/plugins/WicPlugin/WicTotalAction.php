f7e70fd41d30af95208597e4f964c5d2ef6e75cd
Andy Theuninck <andy@gohanman.com>
2016-08-16 16:14:15 -0500

    No conditional; just non-discountable WIC

--- pos/is4c-nf/plugins/WicPlugin/WicTotalAction.php
+++ pos/is4c-nf/plugins/WicPlugin/WicTotalAction.php
@@ -36,8 +36,6 @@ class WicTotalAction extends TotalAction
             $dbc->query('
                 UPDATE localtemptrans
                 SET discountable=0
-                WHERE department NOT IN (41, 42, 156, 201, 204, 208)
-                    AND department NOT BETWEEN 509 AND 557
             ');
         }
     }
