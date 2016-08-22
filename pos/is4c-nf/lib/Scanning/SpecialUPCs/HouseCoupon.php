81aab8993c3e7b3e00600563ab846761754ab342
Andy Theuninck <andy@gohanman.com>
2016-08-22 13:24:18 -0500

    New discount type PS for "per set" of coupon items

--- pos/is4c-nf/lib/Scanning/SpecialUPCs/HouseCoupon.php
+++ pos/is4c-nf/lib/Scanning/SpecialUPCs/HouseCoupon.php
@@ -534,6 +534,28 @@ class HouseCoupon extends SpecialUPC
                 $row = $transDB->fetch_row($valR);
                 $value = $row['qty'] * $value;
                 break;
+            case' PS': // per set of items
+                $value = $infoW["discountValue"];
+
+                $qualQ = "
+                    SELECT 
+                       SUM(CASE WHEN ItemQtty IS NULL THEN 0 ELSE ItemQtty END) AS qty
+                    " . $this->baseSQL($transDB, $coupID, 'upc') . "
+                    and h.type in ('BOTH', 'QUALIFIER')";
+                $qualR = $transDB->query($qualQ);
+                $qualW = $transDB->fetch_row($qualR);
+
+                $discQ = "
+                    SELECT 
+                       SUM(CASE WHEN ItemQtty IS NULL THEN 0 ELSE ItemQtty END) AS qty
+                    " . $this->baseSQL($transDB, $coupID, 'upc') . "
+                    and h.type in ('BOTH', 'DISCOUNT')";
+                $discR = $transDB->query($discQ);
+                $discW = $transDB->fetch_row($discR);
+
+                $sets = ($qualW['qty'] > $discW['qty']) ? $discW['qty'] : $qualW['qty'];
+                $value = $sets * $value;
+                break;
             case "F": // completely flat; no scaling for weight
                 $value = $infoW["discountValue"];
                 break;
