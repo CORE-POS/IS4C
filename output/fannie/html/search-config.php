f3b1ed212e541cd2d5e0125981366e26243838f3
Andy Theuninck <andy@gohanman.com>
2016-09-02 11:36:16 -0500

    Doxy was way behind

--- output/fannie/html/search-config.php
+++ output/fannie/html/search-config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "",
-  'logo' => "Generated on Thu Apr 2 2015 12:27:33 for CORE POS - Fannie by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.4 ",
+  'logo' => "Generated on Fri Sep 2 2016 11:31:14 for CORE POS - Fannie by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.6 ",
 );
 
 </script>
