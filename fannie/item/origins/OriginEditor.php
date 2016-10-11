b0539672048b84d962b663a7dd182f4f0fc81ee7
Andy Theuninck <andy@gohanman.com>
2016-10-11 13:15:37 -0500

    BUGFIX: stray apostrophes in origin editor links

--- fannie/item/origins/OriginEditor.php
+++ fannie/item/origins/OriginEditor.php
@@ -355,9 +355,9 @@ class OriginEditor extends FannieRESTfulPage
         <tr>
             <th>Short Name</th>
             <th>Full Name</th>
-            <th><a href="{$self}'?custom=1">Region</a></th>
-            <th><a href="{$self}'?state=1">State/Prov</a></th>
-            <th><a href="{$self}'?country=1">Country</a></th>
+            <th><a href="{$self}?custom=1">Region</a></th>
+            <th><a href="{$self}?state=1">State/Prov</a></th>
+            <th><a href="{$self}?country=1">Country</a></th>
             <th>Local</th>
         </tr>
 HTML;
