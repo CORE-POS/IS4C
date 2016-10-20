80149c4cd23fa2251bf4474bb729c4084251ca86
Andy Theuninck <andy@gohanman.com>
2016-10-20 10:06:38 -0500

    Tabs

--- fannie/classlib2.0/item/signage/Giganto4UpP.php
+++ fannie/classlib2.0/item/signage/Giganto4UpP.php
@@ -84,8 +84,8 @@ class Giganto4UpP extends \COREPOS\Fannie\API\item\FannieSignage
             } elseif (strstr($price, 'OFF')) {
                 $pdf->SetFont($this->font, '', $this->BIG_FONT-27);
             } elseif (strstr($price, 'SAVE')) {
-				$pdf->SetFont($this->font, '', $this->BIG_FONT-40);
-			}
+                $pdf->SetFont($this->font, '', $this->BIG_FONT-40);
+            }
             $pdf->Cell($effective_width, 20, $price, 0, 1, 'C');
 
             if ($item['startDate'] != '' && $item['endDate'] != '') {
