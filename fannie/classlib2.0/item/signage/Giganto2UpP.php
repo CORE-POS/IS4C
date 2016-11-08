7ce43579ee9531c5f76a68501ea7c881234b7537
Corey Sather <csather@key.wfco-op.store>
2016-11-08 12:52:55 -0600

    added OFF/lb sizing

--- fannie/classlib2.0/item/signage/Giganto2UpP.php
+++ fannie/classlib2.0/item/signage/Giganto2UpP.php
@@ -80,7 +80,9 @@ class Giganto2UpP extends \COREPOS\Fannie\API\item\FannieSignage
             if (strstr($price, 'lb')) {
                 $price = str_replace(' /lb.', '/lb.', $price);
                 $pdf->SetFont($this->font, '', $this->BIG_FONT-29);
-            } elseif (strstr($price, 'OFF')) {
+            } elseif (strstr($price, 'OFF/LB')) {
+                $pdf->SetFont($this->font, '', $this->BIG_FONT-80);
+			} elseif (strstr($price, 'OFF')) {
                 $pdf->SetFont($this->font, '', $this->BIG_FONT-27);
             } elseif (strstr($price, 'SAVE')) {
                 $pdf->SetFont($this->font, '', $this->BIG_FONT-60);
