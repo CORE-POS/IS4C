d22fee7ca190777d061904c746183d1c4a31ea22
Andy Theuninck <andy@gohanman.com>
2016-11-25 15:57:15 -0600

    Separate SPO creation functions

--- fannie/classlib2.0/data/models/trans/SpecialOrdersModel.php
+++ fannie/classlib2.0/data/models/trans/SpecialOrdersModel.php
@@ -48,6 +48,7 @@ class SpecialOrdersModel extends BasicModel
     'email' => array('type'=>'VARCHAR(50)'),
     'storeID' => array('type'=>'INT'),
     'sendEmails' => array('type'=>'TINYINT', 'default'=>0),
+    'onlineID' => array('type'=>'INT'),
     );
 
     public function doc()
