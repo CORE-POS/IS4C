e24cd7bb69da75ee657b8b16d85c4d25ac1d3ffd
Andy Theuninck <andy@gohanman.com>
2017-01-05 13:02:42 -0600

    (WFC) Add shrink to Margin Study

--- fannie/reports/Store-Specific/WFC/MarginStudy/MarginStudyReport.php
+++ fannie/reports/Store-Specific/WFC/MarginStudy/MarginStudyReport.php
@@ -6,7 +6,7 @@ if (!class_exists('FannieAPI.php')) {
 
 class MarginStudyReport extends FannieReportPage
 {
-    protected $report_headers = array('Category-Store', 'Raw Sales', '% On Sale', '% Open Ring', '% SPO', 'Est. UNFI Sales', '% UNFI', 'Est. UNFI Purch', 'UNFI Margin');
+    protected $report_headers = array('Category-Store', 'Raw Sales', '% On Sale', '% Open Ring', '% SPO', '% DDD', 'Est. UNFI Sales', '% UNFI', 'Est. UNFI Purch', 'UNFI Margin');
     protected $required_fields = array('date1', 'date2');
     protected $title = 'Margin Study Report';
     protected $header = 'Margin Study Report';
@@ -72,6 +72,17 @@ class MarginStudyReport extends FannieReportPage
                 AND i.salesCode=?");
         
         $dlog = DTransactionsModel::selectDlog($date1, $date2);
+        $dtrans = DTransactionsModel::selectDtrans($date1, $date2);
+        $shrinkP = $this->connection->prepare("
+            SELECT SUM(total)
+            FROM {$dtrans} AS t
+                INNER JOIN departments AS d ON t.department=d.dept_no
+            WHERE t.datetime BETWEEN ? AND ?
+                AND t.store_id=?
+                AND d.salesCode=?
+                AND t.trans_status='Z'
+                AND total < 10000
+        ");
         $query = "
             SELECT SUM(total) AS rawTTL,
                 SUM(CASE WHEN t.discounttype <> 0 THEN total ELSE 0 END) as saleTTL,
@@ -96,12 +107,14 @@ class MarginStudyReport extends FannieReportPage
             $name = isset($this->COA[$row['salesCode']]) ? $this->COA[$row['salesCode']] : $row['salesCode'];
             $poArgs = array($date1 . ' 00:00:00', $date2 . ' 23:59:59', $row['store_id'], $row['salesCode']);
             $poTTL = $this->connection->getValue($poP, $poArgs);
+            $shrinkTTL = $this->connection->getValue($shrinkP, $poArgs);
             $data[] = array(
                 $name . '-' . $row['store_id'],
                 sprintf('%.2f', $row['rawTTL']),
                 sprintf('%.3f%%', ($row['saleTTL']/$row['rawTTL'])*100),
                 sprintf('%.3f%%', ($row['openTTL']/$row['rawTTL'])*100),
                 sprintf('%.3f%%', ($row['spoTTL']/$row['rawTTL'])*100),
+                sprintf('%.3f%%', ($shrinkTTL/$row['rawTTL'])*100),
                 sprintf('%.2f', $row['unfiTTL']),
                 sprintf('%.3f%%', ($row['unfiTTL']/$row['rawTTL'])*100),
                 sprintf('%.2f', $poTTL),
