13bd17d30173d7aa90686443bdfd860f73e33594
Andy Theuninck <andy@gohanman.com>
2017-01-17 13:21:58 -0600

    (WFC) invert narrow tag layout Place barcode higher to be scannable through sale sign holders. Also fix a bunch of single-letter variable names with more meangingful descriptors

--- fannie/admin/labels/pdf_layouts/WFC_Narrow.php
+++ fannie/admin/labels/pdf_layouts/WFC_Narrow.php
@@ -28,174 +28,138 @@ class WFC_Narrow_PDF extends FpdfWithBarcode
 {
 }
 
-function WFC_Narrow($data,$offset=0){
+function WFC_Narrow($data,$offset=0)
+{
+    $pdf=new WFC_Narrow_PDF('P','mm','Letter'); //start new instance of PDF
+    $pdf->Open(); //open new PDF Document
+    $pdf->SetTopMargin(40);  //Set top margin of the page
+    $pdf->SetLeftMargin(4);  //Set left margin of the page
+    $pdf->SetRightMargin(0);  //Set the right margin of the page
+    $pdf->SetAutoPageBreak(False); // manage page breaks yourself
+    $pdf->AddPage();  //Add a page
 
-$pdf=new WFC_Narrow_PDF('P','mm','Letter'); //start new instance of PDF
-$pdf->Open(); //open new PDF Document
-$pdf->SetTopMargin(40);  //Set top margin of the page
-$pdf->SetLeftMargin(4);  //Set left margin of the page
-$pdf->SetRightMargin(0);  //Set the right margin of the page
-$pdf->AddPage();  //Add a page
+    //Set increment counters for rows
+    $upcX = 7;  //x location of barcode
+    $upcY = 15; //y locaton of barcode
+    $priceY = 29; //y location of size and price on label
+    $priceX = 8; //x location of date and price on label
+    $count = 0;  //number of labels created
+    $baseY = 31; // baseline Y location of label
+    $baseX = 6;  // baseline X location of label
+    $down = 31.0;
 
-//Set increment counters for rows 
-$i = 7;  //x location of barcode
-$j = 31; //y locaton of barcode
-$l = 30; //y location of size and price on label
-$k = 8; //x location of date and price on label
-$m = 0;  //number of labels created
-$n = 18; //y location of description for label
-$r = 24; //y location of date for label
-$p = 6;  //x location fields on label
-$t = 28; //y location of SKU and vendor info
-$u = 24; //x locaiton of vendor info for label
-$w = 7; //x location of Brand info for label
-$x = 42; //y location of Brand info for label
-$down = 31.0;
+    if($offset>0) {
+        //increment values in respect to Y
+        $delta = 18.0;
+        $baseY = $baseY+$delta;
+        $upcY = $upcY+$delta;
+        $priceY = $priceY+$delta;
 
-if($offset>0) {
-
-    //increment values in respect to Y
-    $delta = 18.0;
-    $n = $n+$delta; 
-    $j = $j+$delta;
-    $l = $l+$delta;
-    //$n = $n+$delta;
-    $r = $r+$delta;
-    $t = $t+$delta;
-    $x = $x+$delta;
-    
-    $m = $offset;
-    /*
-    //increment counters in respect to X
-    $i = $i + 52.7 * $offset;
-    $k = $k + 52.7 * $offset;
-    $m = $m + 1;
-    $p = $p + 52.7 * $offset;
-    $u = $u + 52.7 * $offset;
-    $w = $w + 52.7 * $offset;
-    */
-    
-}
-
-//cycle through result array of query
-foreach($data as $row){
-   //If $m == 32 add a new page and reset all counters..
-   if($m == 32){
-      $pdf->AddPage();
-    $i = 7;  //x location of barcode
-    $j = 31; //y locaton of barcode
-    $l = 30; //y location of size and price on label
-    $k = 8; //x location of date and price on label
-    $m = 0;  //number of labels created
-    $m = 0;  //number of labels created
-    $n = 18; //y location of description for label
-    $r = 24; //y location of date for label
-    $p = 6;  //x location fields on label
-    $t = 28; //y location of SKU and vendor info
-    $u = 24; //x location of vendor info for label
-    $w = 7; //x location of Brand info for label
-    $x = 42; //y location of Brand info for label
-   }
-
-   //If $i > 175, start a new line of labels
-   if($i > 175){
-      $i = 7;
-      $j = $j + $down;
-      $k = 8;
-      $l = $l + $down;
-      $n = $n + $down;
-      $r = $r + $down;
-      $p = 6;
-      $u = 24;
-      $t = $t + $down;
-      $w = 7;
-      $x = $x + $down;
-   }
-   $price = $row['normal_price'];
-   $desc = strtoupper(substr($row['description'],0,27));
-   $brand = ucwords(strtolower(substr($row['brand'],0,30)));
-   $pak = $row['units'];
-   $size = $row['units'] . "-" . $row['size'];
-   $sku = $row['sku'];
-   $upc = ltrim($row['upc'],0);
-   $check = $pdf->GetCheckDigit($upc);
-   $tagdate = date('m/d/y');
-   $vendor = substr($row['vendor'],0,7);
-   
-   //Start laying out a label 
-   $pdf->SetFont('Arial','',8);  //Set the font 
-
-   $words = preg_split('/[ ,-]+/',$desc);
-   $limit = 13;
-   $lineheight = 0;
-   $curStr = "";
-   $length = 0;
-   $lines = 0;
-   foreach ($words as $word) {
-    if ($length + strlen($word) <= $limit) {
-        $curStr .= $word . ' ';
-        $length += strlen($word) + 1;
-    } else {
-        $lines++;
-        if ($lines >= 2) {
-            break;
-        }
-        $curStr = trim($curStr) . "\n" . $word . ' ';
-        $length = strlen($word)+1;
+        $count = $offset;
     }
-   }
-   $pdf->SetXY($p, $n-3);
-   $pdf->MultiCell(100, 3, $curStr);
-   
-   //$pdf->TEXT($p,$n,$desc);   //Add description to label
 
-   $pdf->TEXT($p,$r,$tagdate);  //Add date to label
-   $pdf->TEXT($p+12,$r,$size);  //Add size to label
-   
-   
-   $words = preg_split('/[ ,-]+/',$brand);
-   $curStr = "";
-   $curCnt = 0;
-   $length = 0;
-   foreach ($words as $word) {
-       if ($curCnt == 0) {
-           $curStr .= $word . " ";
-           $length += strlen($word)+1;
-       } elseif ($curCnt == 1 && ($length + strlen($word + 1)) < 17) {
-           $curStr .= $word . " ";
-           $length += strlen($word)+1;
-       } elseif ($curCnt > 1 && ($length + 1) < 17) {
-           $chars = str_split($word);
-           foreach ($chars as $char) {
-               $curStr .= strtoupper($char);
-               $length += 2;
-               break;
+    //cycle through result array of query
+    foreach($data as $row){
+        //If $count == 32 add a new page and reset all counters..
+        if($count == 32){
+            $pdf->AddPage();
+            $upcX = 7;  //x location of barcode
+            $upcY = 15; //y locaton of barcode
+            $priceY = 29; //y location of size and price on label
+            $priceX = 8; //x location of date and price on label
+            $count = 0;  //number of labels created
+            $baseY = 31; // baseline Y location of label
+            $baseX = 6;  // baseline X location of label
+        }
+
+        //If $upcX > 175, start a new line of labels
+        if($upcX > 175){
+          $upcX = 7;
+          $upcY = $upcY + $down;
+          $priceX = 8;
+          $priceY = $priceY + $down;
+          $baseY = $baseY + $down;
+          $baseX = 6;
+        }
+        $price = $row['normal_price'];
+        $desc = strtoupper(substr($row['description'],0,27));
+        $brand = ucwords(strtolower(substr($row['brand'],0,30)));
+        $pak = $row['units'];
+        $size = $row['units'] . "-" . $row['size'];
+        $sku = $row['sku'];
+        $upc = ltrim($row['upc'],0);
+        $check = $pdf->GetCheckDigit($upc);
+        $tagdate = date('m/d/y');
+        $vendor = substr($row['vendor'],0,7);
+
+        //Start laying out a label
+        $pdf->SetFont('Arial','',8);  //Set the font
+        if (strlen($upc) <= 11)
+            $pdf->UPC_A($upcX,$upcY,$upc,4,.25);  //generate barcode and place on label
+        else
+            $pdf->EAN13($upcX,$upcY,$upc,4,.25);  //generate barcode and place on label
+
+        $pdf->SetFont('Arial','B',18); //change font for price
+        $pdf->TEXT($priceX,$priceY,$price);  //add price
+
+        $words = preg_split('/[ ,-]+/',$desc);
+        $limit = 13;
+        $lineheight = 0;
+        $curStr = "";
+        $length = 0;
+        $lines = 0;
+        foreach ($words as $word) {
+            if ($length + strlen($word) <= $limit) {
+                $curStr .= $word . ' ';
+                $length += strlen($word) + 1;
+            } else {
+                $lines++;
+                if ($lines >= 2) {
+                    break;
+                }
+                $curStr = trim($curStr) . "\n" . $word . ' ';
+                $length = strlen($word)+1;
             }
-       }
-       $curCnt++;
-   }
-   $pdf->TEXT($w,$x,$curStr);  //add brand
-   $pdf->SetFont('Arial','B',18); //change font for price
-   $pdf->TEXT($k,$l,$price);  //add price
+        }
+        $pdf->SetFont('Arial','',8);
+        $pdf->SetXY($baseX, $baseY);
+        $pdf->MultiCell(100, 3, $curStr);
+        $pdf->SetX($baseX);
+        $pdf->Cell(0, 3, $tagdate);
+        $pdf->SetX($baseX+12);
+        $pdf->Cell(0, 3, $size, 0, 1);
 
+        $words = preg_split('/[ ,-]+/',$brand);
+        $curStr = "";
+        $curCnt = 0;
+        $length = 0;
+        foreach ($words as $word) {
+           if ($curCnt == 0) {
+               $curStr .= $word . " ";
+               $length += strlen($word)+1;
+           } elseif ($curCnt == 1 && ($length + strlen($word + 1)) < 17) {
+               $curStr .= $word . " ";
+               $length += strlen($word)+1;
+           } elseif ($curCnt > 1 && ($length + 1) < 17) {
+               $chars = str_split($word);
+               foreach ($chars as $char) {
+                   $curStr .= strtoupper($char);
+                   $length += 2;
+                   break;
+                }
+           }
+           $curCnt++;
+        }
+        $pdf->SetX($baseX);
+        $pdf->Cell(0, 3, $curStr);
 
-   $newUPC = $upc . $check; //add check digit to upc
-   if (strlen($upc) <= 11)
-    $pdf->UPC_A($i,$j,$upc,4,.25);  //generate barcode and place on label
-   else
-    $pdf->EAN13($i,$j,$upc,4,.25);  //generate barcode and place on label
-   
+        //increment counters
+        $upcX = $upcX + 52.7;
+        $priceX = $priceX + 52.7;
+        $count = $count + 1;
+        $baseX = $baseX + 52.7;
+    }
 
-   //increment counters
-   $i = $i + 52.7;
-   $k = $k + 52.7;
-   $m = $m + 1;
-   $p = $p + 52.7;
-   $u = $u + 52.7;
-   $w = $w + 52.7;
-}
-
-$pdf->Output();  //Output PDF file to screen.
-
+    $pdf->Output();  //Output PDF file to screen.
 }
 
