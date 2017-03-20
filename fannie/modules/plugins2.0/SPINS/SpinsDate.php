dc69c9b4e6c04be63c0a1ed611255cf72a9c56c8
Andy Theuninck <andy@gohanman.com>
2017-03-20 09:42:59 -0500

    SPINS date handling
    When the offset from ISO week numbering to SPINS week numbering
    if negative, the default week for the submit task ends up
    multiple weeks in the past. All data still gets sent eventually
    but not by period deadlines. After figuring out the week
    numbering scheme and a pair of start/end dates that match a
    particular week number it now walks forward in seven day
    increments so that the default week is the most recent finished
    week.

--- fannie/modules/plugins2.0/SPINS/SpinsDate.php
+++ fannie/modules/plugins2.0/SPINS/SpinsDate.php
@@ -12,7 +12,9 @@ class SpinsDate
         $this->year = date('Y');
         $this->iso_week = $iso_week;
         // if week is not specified default to previous week
+        $specific_week = true;
         if ($this->iso_week < 0) {
+            $specific_week = false;
             $this->iso_week = date('W');
             $this->iso_week--;
             if ($this->iso_week <= 0) {
@@ -35,6 +37,24 @@ class SpinsDate
         $this->end = $this->start;
         while (date('w', $this->end) != 0) {
             $this->end = mktime(0,0,0,date('n',$this->end),date('j',$this->end)+1,date('Y',$this->end));
+        }
+
+        /**
+          When the offset to SPINS' week numbering is negative, the default week
+          can end up being multiple weeks ago. All data will still get submitted
+          eventually but period deadlines get missed. Unless a specific week number
+          has been requested, this section walks forward in 7 day increments so the
+          default week is also the most recent completed week.
+        */
+        $endDT = new DateTime(date('Y-m-d', $this->end));
+        $today = new DateTime(date('Y-m-d'));
+        $endDT->add(new DateInterval('P7D'));
+        while (!$specific_week && $endDT < $today) {
+            $this->spins_week++;
+            $this->iso_week++;
+            $this->start = mktime(0,0,0, date('n',$this->start), date('j',$this->start)+7, date('Y',$this->start));
+            $this->end = mktime(0,0,0, date('n',$this->end), date('j',$this->end)+7, date('Y',$this->end));
+            $endDT->add(new DateInterval('P7D'));
         }
     }
 
