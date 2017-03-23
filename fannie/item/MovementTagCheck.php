4c847c3ca6d6c6671e568ee68bb50c1c9dc40a4d
Andy Theuninck <andy@gohanman.com>
2017-03-23 13:49:41 -0500

    Add option to filter movement tags by % of case/week

--- fannie/item/MovementTagCheck.php
+++ fannie/item/MovementTagCheck.php
@@ -13,6 +13,8 @@ class MovementTagCheck extends FannieRESTfulPage
     protected function get_id_view()
     {
         $ret = $this->get_view();
+        $standard = FormLib::get('via', 0);
+        $ret .= sprintf('<h3>Cutoff: %.2f%% %s</h3>', $this->id, $standard ? 'Case' : 'Item');
         $ret .= '<table class="table table-bordered table-striped">
             <tr>
                 <th>UPC</th>
@@ -21,30 +23,39 @@ class MovementTagCheck extends FannieRESTfulPage
                 <th>Tag Par</th>
                 <th>Current Par</th>
                 <th>% Change</th>
+                <th>Change as % of Case</th>
             </tr>';
         $query = $this->connection->prepare('
             SELECT p.upc, p.brand, p.description,
-                p.auto_par, m.lastPar
+                p.auto_par, m.lastPar, i.units, i.units, i.units, i.units
             FROM MovementTags AS m
                 INNER JOIN products AS p ON p.upc=m.upc AND p.store_id=m.storeID
+                LEFT JOIN vendorItems AS i ON p.upc=i.upc AND p.default_vendor_id=i.vendorID
             WHERE ABS(m.lastPar - p.auto_par) / m.lastPar > ?
         ');
         $res = $this->connection->execute($query, array($this->id/100));
         while ($row = $this->connection->fetchRow($res)) {
+            $caseSize = $row['units'] ? $row['units'] : 1;
+            $change = abs((7*$row['lastPar']) - (7*$row['auto_par']));
+            if ($standard && ($change/$caseSize) < ($this->id/100)) {
+                continue;
+            }
             $ret .= sprintf('<tr>
-                    <td>%s</td>
+                    <td><a href="ItemEditorPage.php?searchupc=%s">%s</a></td>
                     <td>%s</td>
                     <td>%s</td>
                     <td>%.2f</td>
                     <td>%.2f</td>
-                    <td>%.2f</td>
+                    <td>%.2f%%</td>
+                    <td>%.2f%%</td>
                     </tr>',
-                    $row['upc'], 
+                    $row['upc'], $row['upc'],
                     $row['brand'], 
                     $row['description'], 
                     7*$row['lastPar'], 
                     7*$row['auto_par'], 
-                    (abs($row['lastPar'] - $row['auto_par']) / $row['lastPar'])
+                    (abs($row['lastPar'] - $row['auto_par']) / $row['lastPar']) * 100,
+                    $change / $caseSize * 100
             );
         }
         $ret .= '</table>';
@@ -62,6 +73,10 @@ class MovementTagCheck extends FannieRESTfulPage
             <input type="text" name="id" value="10" class="form-control">
             <span class="input-group-addon">%</span>
         </div>
+        <select name="via" class="form-control">
+            <option value="0">Of single package movement</option>
+            <option value="1">Of case movement</option>
+        </select>
         <button type="submit" class="btn btn-default">Submit</button>
     </div>
 </form>
