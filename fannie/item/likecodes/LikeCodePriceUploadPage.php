00dab8f1138e309abca5b844506c82f747815e1a
Andy Theuninck <andy@gohanman.com>
2017-04-03 14:10:06 -0500

    Overhaul LikeCode Price Upload
    Supports a number of additional columns to normalize products
    by likecode. It checks the uploaded data against existing
    product settings to avoid generating spurious prodUpdate records
    if nothing really changed. It also supports the new
    ProductAttributes table

--- fannie/item/likecodes/LikeCodePriceUploadPage.php
+++ fannie/item/likecodes/LikeCodePriceUploadPage.php
@@ -40,70 +40,40 @@ class LikeCodePriceUploadPage extends \COREPOS\Fannie\API\FannieUploadPage
             'default' => 0,
             'required' => true
         ),
+        'description' => array(
+            'display_name' => 'Description',
+            'default' => 1,
+        ),
         'price' => array(
             'display_name' => 'Price',
-            'default' => 1,
+            'default' => 2,
             'required' => true
         ),
         'cost' => array(
             'display_name' => 'Cost (Unit)',
-            'default' => 2,
+            'default' => 3,
+        ),
+        'scale' => array(
+            'display_name' => 'LB / EA',
+            'default' => 4,
+        ),
+        'department' => array(
+            'display_name' => 'Department',
+            'default' => 5,
+        ),
+        'wicable' => array(
+            'display_name' => 'WIC',
+            'default' => 6,
+        ),
+        'organic' => array(
+            'display_name' => 'Organic',
+            'default' => 7,
+        ),
+        'local' => array(
+            'display_name' => 'Local',
+            'default' => 8,
         ),
     );
-
-    private function prepareStatements($dbc)
-    {
-        $update = $dbc->prepare('
-            UPDATE products AS p
-                INNER JOIN upcLike AS u ON p.upc=u.upc
-            SET p.normal_price = ?,
-                p.modified = ' . $dbc->now() . '
-            WHERE u.likeCode=?');
-        $updateWithCost = $dbc->prepare('
-            UPDATE products AS p
-                INNER JOIN upcLike AS u ON p.upc=u.upc
-            SET p.normal_price=?,
-                p.cost = ?,
-                p.modified = ' . $dbc->now() . '
-            WHERE u.likeCode=?');
-        if ($dbc->dbmsName() == 'mssql') {
-            $update = $dbc->prepare('
-                UPDATE products
-                SET normal_price = ?,
-                    modified = ' . $dbc->now() . '
-                FROM products AS p
-                    INNER JOIN upcLike AS u ON p.upc=u.upc
-                WHERE u.likeCode=?');
-            $updateWithCost = $dbc->prepare('
-                UPDATE products
-                SET normal_price=?,
-                    cost = ?,
-                    modified = ' . $dbc->now() . '
-                FROM products AS p
-                    INNER JOIN upcLike AS u ON p.upc=u.upc
-                WHERE u.likeCode=?');
-        } elseif ($dbc->dbmsName() == 'postgres9') {
-            $update = $dbc->prepare('
-                UPDATE products AS p
-                SET p.normal_price = ?,
-                    p.modified = ' . $dbc->now() . '
-                FROM upcLike AS u
-                WHERE p.upc=u.upc
-                    AND u.likeCode=?');
-            $updateWithCost = $dbc->prepare('
-                UPDATE products AS p
-                SET p.normal_price=?,
-                    p.cost = ?,
-                    p.modified = ' . $dbc->now() . '
-                FROM upcLike AS u
-                WHERE p.upc=u.upc
-                    AND u.likeCode=?');
-        }
-
-        $getUPCs = $dbc->prepare("SELECT upc FROM upcLike WHERE likeCode=?");
-
-        return array($update, $updateWithCost, $getUPCs);
-    }
 
     public function process_file($linedata, $indexes)
     {
@@ -111,42 +81,114 @@ class LikeCodePriceUploadPage extends \COREPOS\Fannie\API\FannieUploadPage
         $dbc = FannieDB::get($FANNIE_OP_DB);
 
         $ret = true;
-        list($update, $updateWithCost, $getUPCs) = $this->prepareStatements($dbc);
+        $getUPCs = $dbc->prepare("SELECT upc FROM upcLike WHERE likeCode=?");
+        $getItem = $dbc->prepare('SELECT description, normal_price, cost, department, numflag, scale, wicable, local FROM products WHERE upc=?');
+        $getAttr = $dbc->prepare('SELECT attributes FROM ProductAttributes WHERE upc=? ORDER BY modified DESC');
+        $setAttr = $dbc->prepare('INSERT INTO ProductAttributes (upc, modified, attributes) VALUES (?, ?, ?)');
+
+        // build update query based on selected columns
+        $update = "UPDATE products SET normal_price=?";
+        $updateCols = array();
+        $numflag = false;
+        if ($indexes['cost'] !== false) {
+            $update .= ', cost=?';
+            $updateCols[] = 'cost';
+        }
+        if ($indexes['department'] !== false) {
+            $update .= ', department=?';
+            $updateCols[] = 'department';
+        }
+        if ($indexes['scale'] !== false) {
+            $update .= ', scale=?';
+            $updateCols[] = 'scale';
+        }
+        if ($indexes['wicable'] !== false) {
+            $update .= ', wicable=?';
+            $updateCols[] = 'wic';
+        }
+        if ($indexes['description'] !== false) {
+            $update .= ', description=?';
+            $updateCols[] = 'description';
+        }
+        if ($indexes['local'] !== false) {
+            $update .= ', local=?';
+            $updateCols[] = 'local';
+        }
+        if ($indexes['numflag'] !== false) {
+            $update .= ', numflag=?';
+            $numflag = true;
+        }
+        $update .= ' WHERE upc=?';
+        
         $this->stats = array('done' => 0, 'error' => array());
         $upcs = array();
         $sets = array();
+        $dbc->startTransaction();
         foreach ($linedata as $line) {
             $likecode = trim($line[$indexes['likecode']]);
             $price =  trim($line[$indexes['price']], ' $');  
-            $cost = 0;
-            if ($indexes['cost'] !== false && isset($line[$indexes['cost']])) {
-                $cost = trim($line[$indexes['cost']], ' $');
-            }
             
             if (!is_numeric($likecode)) continue; // skip header(s) or blank rows
 
-            $try = false;
-            if ($cost == 0) {
-                $try = $dbc->execute($update, array($price, $likecode));
-            } else {
-                $try = $dbc->execute($updateWithCost, array($price, $cost, $likecode));
-            }
-            if ($try === false) {
-                $ret = false;
-                $this->stats['error'][] = ' Problem updating LC# ' . $likecode . ';';
-            } else {
-                $upcR = $dbc->execute($getUPCs, array($likecode));
-                while ($upcW = $dbc->fetchRow($upcR)) {
-                    $upcs[] = $upcW['upc'];
-                    $sets[] = array($price, $cost, $upcW['upc']);
+            $upcR = $dbc->execute($getUPCs, array($likecode));
+            // compare each item in the likecode to
+            // the uploaded values to flag which UPCs
+            // are actually changing
+            while ($upcW = $dbc->fetchRow($upcR)) {
+                $changed = false;
+                $args = array($price);
+                $item = $dbc->getRow($getItem, array($upcW['upc']));
+                foreach ($updateCols as $col) {
+
+                    $normalize = trim(strtoupper($line[$indexes[$col]]));
+                    if ($normalize === 'Y' || $normalize === 'YES' || $normalize === 'X') {
+                        $line[$indexes[$col]] = 1;
+                    } elseif ($normalize === 'N' || $normalize === 'NO' || $normalize === '') {
+                        $line[$indexes[$col]] = 0;
+                    }
+
+                    $args[] = $line[$indexes[$col]];
+                    if ($line[$indexes[$col]] != $item[$col]) {
+                        $changed = true;
+                    }
                 }
-                $this->stats['done']++;
+
+                // numflag needs special handling since we're only
+                // dealing with organic. This sets or unsets the 
+                // appropriate bit in numflag then if necessary
+                // adds a new record to ProductAttributes
+                if ($numflag) {
+                    $current = $item['numflag'];
+                    $flag = (1 << 16);
+                    if ($line[$indexes['numflag']]) {
+                        $new = $current | (1 << 16);
+                    } else {
+                        $new = $current & (~$flag);
+                    }
+                    $args[] = $new;
+                    if ($current != $new) {
+                        $changed = true;
+                        $attr = $dbc->getRow($getAttr, array($upcW['upc']));
+                        $newAttrs = json_decode($attr['attributes'], true);
+                        $newAttrs['Organic'] = ($line[$indexes['numflag']]) ? true : false;
+                        $dbc->execute($setAttr, array($upcW['upc'], date('Y-m-d H:i:s'), json_encode($newAttrs)));
+                    }
+                }
+
+                $args[] = $upcW['upc'];
+                $dbc->execute($update, $args);
+                if ($changed) {
+                    $upcs[] = $upcW['upc'];
+                    $sets[] = array($price, $upcW['upc']);
+                }
             }
+            $this->stats['done']++;
         }
 
         // log the updates
         $model = new ProdUpdateModel($dbc);
         $model->logManyUpdates($upcs, 'EDIT');
+        $dbc->commitTransaction();
 
         // push updates to local lanes
         $FANNIE_LANES = FannieConfig::config('LANES');
@@ -159,7 +201,7 @@ class LikeCodePriceUploadPage extends \COREPOS\Fannie\API\FannieUploadPage
                 // connect failed
                 continue;
             }
-            $upP = $lane_sql->prepare('UPDATE products SET normal_price=?, cost=? WHERE upc=?');
+            $upP = $lane_sql->prepare('UPDATE products SET normal_price=? WHERE upc=?');
             foreach ($sets as $set) {
                 $upR = $lane_sql->execute($upP, $set);
             }
@@ -173,7 +215,7 @@ class LikeCodePriceUploadPage extends \COREPOS\Fannie\API\FannieUploadPage
             $res = $this->connection->execute($prep, array(\FannieConfig::config('STORE_ID')));
             while ($row = $this->connection->fetchRow($res)) {
                 $client = new \Datto\JsonRpc\Http\Client($row['webServiceUrl']);
-                $client->query(time(), 'COREPOS\\Fannie\\API\\webservices\\FannieItemLaneSync', array('upc'=>array_keys($upcs), 'fast'=>true));
+                $client->query(time(), 'COREPOS\\Fannie\\API\\webservices\\FannieItemLaneSync', array('upc'=>$upcs, 'fast'=>true));
                 $client->send();
             }
         }
