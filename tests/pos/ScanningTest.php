b0f5bc1d51e6c8203769c7de7a8ce830b1bfc91b
Andy Theuninck <andy@gohanman.com>
2017-04-24 12:35:33 -0500

    AB gets foodstamp eligibility, too

--- tests/pos/ScanningTest.php
+++ tests/pos/ScanningTest.php
@@ -548,6 +548,7 @@ class ScanningTest extends PHPUnit_Framework_TestCase
         $record['total'] = -0.50;
         $record['regPrice'] = -0.50;
         $record['tax'] = 2;
+        $record['foodstamp'] = 1;
         lttLib::verifyRecord(3, $record, $this);
     }
 
