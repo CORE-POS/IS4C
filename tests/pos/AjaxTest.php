b1459425af4bb265b80ef95841dc0fec0c408e63
Andy Theuninck <andy@gohanman.com>
2017-04-27 09:32:53 -0500

    Expand ajax tests

--- tests/pos/AjaxTest.php
+++ tests/pos/AjaxTest.php
@@ -50,6 +50,14 @@ class AjaxTest extends PHPUnit_Framework_TestCase
         CoreLocal::set('msgrepeat', 1);
         $json = $ajax->ajax();
         $this->assertNotEquals(false, strstr(json_encode($json), 'pos2.php'));
+
+        $vals = new ValueContainer();
+        $vals->repeat = 1;
+        $vals->reginput = 'RF1234567890123';
+        $ajax = new COREPOS\pos\ajax\AjaxParser(new WrappedStorage(), $vals);
+        $json = $ajax->ajax();
+        $this->assertInternalType('array', $json);
+
         CoreLocal::set('CashierNo', '');
     }
 
@@ -73,17 +81,59 @@ class AjaxTest extends PHPUnit_Framework_TestCase
 
     public function testEnd()
     {
+        // default case: full receipt w/ reprint
         $vals = new ValueContainer();
         $vals->receiptType = 'full';
         $vals->ref = '1-1-1';
-        $ajax = new COREPOS\pos\ajax\AjaxEnd(new WrappedStorage(), $vals);
+        $ws = new WrappedStorage();
+        $ws->set('autoReprint', 1);
+        $ajax = new COREPOS\pos\ajax\AjaxEnd($ws, $vals);
+        $this->assertEquals(array(), $ajax->ajax());
+
+        // test receipt number not provided
+        $vals->ref = '';
+        $ajax = new COREPOS\pos\ajax\AjaxEnd($ws, $vals);
+        $this->assertEquals(array(), $ajax->ajax());
+
+        // test receipt type not provided
+        $vals->receiptType = '';
+        $ajax = new COREPOS\pos\ajax\AjaxEnd($ws, $vals);
+        $this->assertEquals(array(), $ajax->ajax());
+
+        // test disabling cancel receipt
+        $vals->ref = '1-1-1';
+        $vals->receiptType = 'cancelled';
+        $ws->set('CancelReceipt', 0);
+        $ajax = new COREPOS\pos\ajax\AjaxEnd($ws, $vals);
+        $this->assertEquals(array(), $ajax->ajax());
+
+        // test disabling suspend receipt
+        $vals->receiptType = 'suspended';
+        $ws->set('SuspendReceipt', 0);
+        $ajax = new COREPOS\pos\ajax\AjaxEnd($ws, $vals);
+        $this->assertEquals(array(), $ajax->ajax());
+
+        // test disabling shrink receipt
+        $vals->receiptType = 'ddd';
+        $ws->set('ShrinkReceipt', 0);
+        $ajax = new COREPOS\pos\ajax\AjaxEnd($ws, $vals);
+        $this->assertEquals(array(), $ajax->ajax());
+
+        // test receipt that doesn't end transaction
+        $vals->receiptType = 'ccSlip';
+        $ajax = new COREPOS\pos\ajax\AjaxEnd($ws, $vals);
         $this->assertEquals(array(), $ajax->ajax());
     }
 
     public function testEndorse()
     {
-        $ajax = new COREPOS\pos\ajax\AjaxEndorse(new WrappedStorage(), new ValueContainer());
-        $this->assertEquals('Done', $ajax->ajax());
+        $vals = new ValueContainer();
+        $vals->amount = 1.00;
+        foreach (array('check', 'giftcert', 'stock', 'classreg', 'unknown') as $type) {
+            $vals->endorseType = $type;
+            $ajax = new COREPOS\pos\ajax\AjaxEndorse(new WrappedStorage(), $vals);
+            $this->assertEquals('Done', $ajax->ajax());
+        }
     }
 
     public function testScale()
