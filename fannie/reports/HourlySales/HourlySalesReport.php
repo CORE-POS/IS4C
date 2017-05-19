2ea8e381686ed1af0fb9e0fe050c39049592e347
Andy Theuninck <andy@gohanman.com>
2017-05-19 15:39:23 -0500

    Start hourly sales/trans report weeks on Monday

--- fannie/reports/HourlySales/HourlySalesReport.php
+++ fannie/reports/HourlySales/HourlySalesReport.php
@@ -203,6 +203,12 @@ class HourlySalesReport extends FannieReportPage
             }
         }
 
+        if (isset($dataset['Sun'])) {
+            $sunday = $dataset['Sun'];
+            unset($dataset['Sun']);
+            $dataset['Sun'] = $sunday;
+        }
+
         /**
           # of columns is dynamic depending on the
           date range selected
