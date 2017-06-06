3d2403d7712b0413f9607c46f78e3c75aa6a3db9
Andy Theuninck <andy@gohanman.com>
2017-06-06 12:20:29 -0500

    Implementation of #870

---
+++ pos/is4c-nf/plugins/RemotePrint/RemotePrintModel.php
@@ -0,0 +1,41 @@
+<?php
+
+/*******************************************************************************
+
+    Copyright 2017 Whole Foods Co-op
+
+    This file is part of CORE-POS.
+
+    IT CORE is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    IT CORE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    in the file license.txt along with IT CORE; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*********************************************************************************/
+        
+namespace COREPOS\pos\plugins\RemotePrint;
+
+/**
+  @class RemotePrintModel
+*/
+class RemotePrintModel extends \COREPOS\pos\lib\models\BasicModel
+{
+    protected $name = "RemotePrint";
+    protected $preferred_db = 'op';
+
+    protected $columns = array(
+    'remotePrintID' => array('type'=>'INT', 'increment'=>true, 'primary_key'=>true),
+    'identifier' => array('type'=>'VARCHAR(13)'),
+    'type' => array('type'=>'VARCHAR(10)', 'default'=>"'UPC'"),
+    );
+}
+
