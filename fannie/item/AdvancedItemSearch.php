a11d1c040407a6231be9817d8c9db9df87348246
Andy Theuninck <andy@gohanman.com>
2017-06-08 16:02:36 -0500

    And this one, too

--- fannie/item/AdvancedItemSearch.php
+++ fannie/item/AdvancedItemSearch.php
@@ -1088,7 +1088,7 @@ class AdvancedItemSearch extends FannieRESTfulPage
 
         $items = $this->runSearchMethods($form);
         $phpunit->assertInternalType('array', $items);
-        $phpunit->assertEquals(1, count($items), $this->connection->error());
+        $phpunit->assertEquals(1, count($items), 'Should have found an item, ' . $this->connection->error());
         $phpunit->assertArrayHasKey('0001707710532', $items);
 
         // easiest filter to trigger is the saved items
