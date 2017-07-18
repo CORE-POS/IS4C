02474331e275a5421ace1aa75075fdbdf47b4121
Andy Theuninck <andy@gohanman.com>
2017-07-18 10:26:41 -0500

    Add letter to account termination check

--- fannie/modules/plugins2.0/PIKiller/PITermCheck.php
+++ fannie/modules/plugins2.0/PIKiller/PITermCheck.php
@@ -23,7 +23,7 @@ class PITermCheck extends FannieRESTfulPage
             return false;
         }
 
-        $equity = $dbc->prepare('SELECT balance FROM ' . $this->config->get('TRANS_DB') . $dbc->sep() . 'equity_live_balance WHERE memnum=?');
+        $equity = $dbc->prepare('SELECT payments FROM ' . $this->config->get('TRANS_DB') . $dbc->sep() . 'equity_live_balance WHERE memnum=?');
         $equity = $dbc->getValue($equity, array($this->id));
 
         $classA = $equity < 20 ? $equity : 20;
@@ -116,7 +116,7 @@ class PITermCheck extends FannieRESTfulPage
         $record['emp_no'] = 1001;
         $record['trans_no'] = $trans;
         $record['upc'] = '0';
-        $record['description'] = '63350';
+        $record['description'] = '$left0';
         $record['trans_type'] = 'C';
         $record['trans_subtype'] = 'CM';
         $record['card_no'] = $this->idk;
@@ -129,8 +129,9 @@ class PITermCheck extends FannieRESTfulPage
          * Generate a check
          *****************/
         $pdf = new FPDF('P', 'mm', 'Letter');
-        $pdf->SetMargins(6.35, 6.35, 6.35); // quarter-inch margins
+        $pdf->SetMargins($left, $left, $left); // quarter-inch margins
         $pdf->SetAutoPageBreak(false);
+        $pdf->AddPage();
 
         $plugin_settings = $this->config->get('PLUGIN_SETTINGS');
         $checkDB = $plugin_settings['GiveUsMoneyDB'];
@@ -153,10 +154,63 @@ class PITermCheck extends FannieRESTfulPage
             $number = GumLib::allocateCheck($custdata, false, 'EQ REFUND', 'eqr' . $card_no);
         }
 
-        $pdf->AddPage();
+        $pdf->SetXY(0, 0);
+        $pdf->Image('../GiveUsMoneyPlugin/img/new_letterhead.png', 10, 10, 35);
+        $pdf->SetFont('Arial', '', 8);
+        $line_height = 3.5;
+        $left = 55;
+        $width = 125;
+        $pdf->SetXY($left, 10);
+        $pdf->Cell($width, $line_height, date('F d, Y'));
+        $pdf->Ln(3*$line_height);
+        $pdf->SetX($left);
+        $pdf->Cell($width, $line_height, $this->id, 0, 1);
+        $pdf->SetX($left);
+        $pdf->Cell($width, $line_height, $custdata->FirstName() . ' ' . $custdata->LastName(), 0, 1);
+        foreach (explode("\n", $meminfo->street()) as $addr) {
+            $pdf->SetX($left);
+            $pdf->Cell($width, $line_height, $addr, 0, 1);
+        }
+        $pdf->SetX($left);
+        $pdf->Cell($width, $line_height, $meminfo->city() . ', ' . $meminfo->state() . ' ' . $meminfo->zip(), 0, 1);
+        $pdf->Ln($line_height);
+        $pdf->SetX($left);
+        $pdf->Cell($width, $line_height, 'To Whom It May Concern:', 0, 1);
+        $pdf->Ln($line_height);
+        $pdf->SetX($left);
+        $pdf->MultiCell($width, $line_height, 'The WFC Board of Directors reviewed and approved your application for termination');
+        $pdf->Ln($line_height);
+        $invest = sprintf('$%.2f Class A Voting Stock', $classA);
+        if ($classB > 0) {
+            $invest .= sprintf(' and $%.2f Class B Equity Stock', $classB);
+        }
+        $pdf->SetX($left);
+        $pdf->MultiCell($width, $line_height, "We are returning your investment of {$invest}. As per our policy your equity was redeemed at face value. Please see the attached check");
+        $pdf->Ln($line_height);
+        $pdf->SetX($left);
+        $pdf->MultiCell($width, $line_height, 'Thank you for your investment and support of the Whole Foods Co-op.  If you have questions or concerns regarding the return of investment, please contact the Finance Department.');
+        $pdf->Ln($line_height);
+        $pdf->SetX($left);
+        $pdf->Cell($width, $line_height, 'Thank you,', 0, 1);
+        $pdf->Ln(2*$line_height);
+        $pdf->SetX($left);
+        $pdf->Cell($width, $line_height, 'Amanda Borgren', 0, 1);
+        $pdf->SetX($left);
+        $pdf->Cell($width, $line_height, 'Finance Coordinator', 0, 1);
+        $pdf->SetX($left);
+        $pdf->Cell($width, $line_height, 'Finance Department', 0, 1);
+        $pdf->Ln(2*$line_height);
+        $pdf->SetX($left);
+        $pdf->Cell($width, $line_height, '218.728.0884 | ext. 453', 0, 1);
+        $pdf->SetX($left);
+        $pdf->Cell($width, $line_height, 'os@wholefoods.coop', 0, 1);
+
         $check = new GumCheckTemplate($custdata, $meminfo, $equity, 'Equity Refund', $number);
         $check->renderAsPDF($pdf);
+
         $pdf->Output('Equity Refund ' . $this->id . '.pdf', 'I');
     }
 }
+
+FannieDispatch::conditionalExec();
 
