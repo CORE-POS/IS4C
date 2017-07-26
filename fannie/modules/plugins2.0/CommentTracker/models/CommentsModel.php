a488b0be5cb77245c9e4890d43b2963521fa5e31
Andy Theuninck <andy@gohanman.com>
2017-07-26 16:12:13 -0500

    CommentTracker plugin w/ mail pipe input feed

---
+++ fannie/modules/plugins2.0/CommentTracker/models/CommentsModel.php
@@ -0,0 +1,43 @@
+<?php
+
+/*******************************************************************************
+
+    Copyright 2017 Whole Foods Co-op
+
+    This file is part of CORE-POS.
+
+    IT CORE is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    IT CORE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    in the file license.txt along with IT CORE; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*********************************************************************************/
+        
+
+/**
+  @class CommentsModel
+*/
+class CommentsModel extends BasicModel
+{
+    protected $name = "Comments";
+    protected $preferred_db = 'plugin::CommentDB';
+
+    protected $columns = array(
+    'commentID' => array('type'=>'INT', 'increment'=>true, 'primary_key'=>true),
+    'categoryID' => array('type'=>'INT'),
+    'publishable' => array('type'=>'TINYINT', 'default'=>0),
+    'appropriate' => array('type'=>'TINYINT', 'default'=>1),
+    'email' => array('type'=>'VARCHAR(255)'),
+    'comment' => array('type'=>'TEXT'),
+    );
+}
+
