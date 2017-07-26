646e274e9a9132d21fbc427c3647f73f83f5b89e
Andy Theuninck <andy@gohanman.com>
2017-07-26 10:42:41 -0500

    Make parameter saving safer across DB connection states

--- pos/is4c-nf/install/conf/ParamConf.php
+++ pos/is4c-nf/install/conf/ParamConf.php
@@ -11,21 +11,22 @@ class ParamConf
 
         $saved = false;
         if ($sql !== false && $sql !== null) {
-            $prep = $sql->prepare('SELECT param_value FROM parameters
-                                        WHERE param_key=? AND lane_id=?');
+            $table = \CoreLocal::get('pDatabase') . $sql->sep() . 'parameters';
+            $prep = $sql->prepare("SELECT param_value FROM {$table}
+                                        WHERE param_key=? AND lane_id=?");
             $exists = $sql->execute($prep, array($key, \CoreLocal::get('laneno')));
             if ($sql->num_rows($exists)) {
-                $prep = $sql->prepare('
-                    UPDATE parameters 
+                $prep = $sql->prepare("
+                    UPDATE {$table} 
                     SET param_value=?,
                         is_array=?,
                         store_id=0
                     WHERE param_key=? 
-                        AND lane_id=?');
+                        AND lane_id=?");
                 $saved = $sql->execute($prep, array($value, $save_as_array, $key, \CoreLocal::get('laneno')));
             } else {
-                $prep = $sql->prepare('INSERT INTO parameters (store_id, lane_id, param_key,
-                                        param_value, is_array) VALUES (0, ?, ?, ?, ?)');
+                $prep = $sql->prepare("INSERT INTO {$table} (store_id, lane_id, param_key,
+                                        param_value, is_array) VALUES (0, ?, ?, ?, ?)");
                 $saved = $sql->execute($prep, array(\CoreLocal::get('laneno'), $key, $value, $save_as_array));
             }
         }
