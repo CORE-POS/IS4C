33ad7b5aff5b13f209383a8db0aaf89c46e170a3
Andy Theuninck <andy@gohanman.com>
2017-09-27 15:36:38 -0500

    TagDataSource::getMany()
    Alternate method to fetch lots of UPCs at once

--- fannie/classlib2.0/item/TagDataSource.php
+++ fannie/classlib2.0/item/TagDataSource.php
@@ -64,6 +64,63 @@ class TagDataSource
             $ret['normal_price'] = $price;
         }
 
+        $ret = $this->fixupRow($ret);
+
+        return $ret;
+    }
+
+    /** 
+      Get shelf tag fields for several items
+      @param $dbc [SQLManager] database connection object
+      @param $upcs [array] Item UPC
+      @return [array keyed by UPC] of tag data. Each record
+        has the following keys:
+        - upc
+        - description
+        - brand
+        - normal_price
+        - sku
+        - size
+        - units
+        - vendor
+        - pricePerUnit
+    */
+    public function getMany($dbc, $upcs)
+    {
+        list($inStr, $args) = $dbc->safeInClause($upcs);
+        $query = '
+            SELECT p.upc,
+                p.description,
+                p.normal_price,
+                p.brand,
+                v.vendorName AS vendor,
+                p.size AS p_size,
+                p.unitofmeasure,
+                i.sku,
+                i.units,  
+                i.size AS vi_size
+            FROM products AS p
+                LEFT JOIN vendors AS v ON p.default_vendor_id=v.vendorID
+                LEFT JOIN vendorItems AS i ON p.upc=i.upc AND v.vendorID=i.vendorID
+            WHERE p.upc IN (' . $inStr . ')
+            ORDER BY p.upc';
+        $prep = $dbc->prepare($query);
+        $res = $dbc->execute($prep, $args);
+        $ret = array();
+        $prevUPC = false;
+        while ($row = $dbc->fetchRow($res)) {
+            if ($prevUPC != $row['upc']) {
+                $row = $this->fixupRow($row);
+                $ret[$row['upc']] = $row;
+            }
+            $prevUPC = $row['upc'];
+        }
+
+        return $upc;
+    }
+
+    private function fixupRow($ret)
+    {
         $ret['size'] = '';
         if (is_numeric($ret['p_size']) && !empty($ret['p_size']) && !empty($ret['unitofmeasure'])) {
             $ret['size'] = $ret['p_size'] . ' ' . $ret['unitofmeasure'];
