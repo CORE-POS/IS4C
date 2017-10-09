dd085c07ed73f0282837abb0837f1c4f71dd72bf
Corey Sather <csather@key.wfco-op.store>
2017-10-09 13:36:33 -0500

    revert to previous commit, broken img loading logic

--- fannie/modules/plugins2.0/IncidentTracker/AlertIncident.php
+++ fannie/modules/plugins2.0/IncidentTracker/AlertIncident.php
@@ -227,16 +227,8 @@ class AlertIncident extends FannieRESTfulPage
     {
         $row = $this->getIncident($this->id);
         $row['details'] = nl2br($row['details']);
-
-        $imgs = array(1=>'image1',2=>'image2');
-        foreach ($imgs as $num => $name) {
-            if ($ext = substr($row[$name],-3) == "pdf") {
-                    ${'img'.$num} = $row[$name] ? "<a href=\"image/{$row[$name]}\">{$row[$name]}</a>" : '';
-            } else {
-                    ${'img'.$num} = $row[$name] ? "<img style=\"max-width: 95%;\" src=\"image/{$row[$name]}\" />" : '';
-            }
-        }
-
+        $img1 = $row['image1'] ? "<img style=\"max-width: 95%;\" src=\"image/{$row['image1']}\" />" : '';
+        $img2 = $row['image2'] ? "<img style=\"max-width: 95%;\" src=\"image/{$row['image2']}\" />" : '';
         $row['details'] = preg_replace('/#(\d+)/', '<a href="?id=$1">#$1</a>', $row['details']);
         $row['details'] = preg_replace('`(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?`',
             '<a href="$1://$2$3">$1://$2$3</a>', $row['details']);
@@ -247,15 +239,8 @@ class AlertIncident extends FannieRESTfulPage
             $c['comment'] = preg_replace('/#(\d+)/', '<a href="?id=$1">#$1</a>', $c['comment']);
             $c['comment'] = preg_replace('`(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?`',
                 '<a href="$1://$2$3">$1://$2$3</a>', $c['comment']);
-
-            foreach ($imgs as $num => $name) {
-                if ($ext = substr($row[$name],-3) == "pdf") {
-                    ${'cmg'.$num} = $row[$name] ? "<a href=\"image/{$row[$name]}\">{$row[$name]}</a>" : '';
-                } else {
-                    ${'cmg'.$num} = $row[$name] ? "<img style=\"max-width: 95%;\" src=\"image/{$row[$name]}\" />" : '';
-                }
-            }
-
+            $cmg1 = $c['image1'] ? "<img style=\"max-width: 95%;\" src=\"image/{$c['image1']}\" />" : '';
+            $cmg2 = $c['image2'] ? "<img style=\"max-width: 95%;\" src=\"image/{$c['image2']}\" />" : '';
             $cHtml .= sprintf('<div class="panel panel-default">
                 <div class="panel panel-heading">%s - %s</div>
                 <div class="panel panel-body">%s
