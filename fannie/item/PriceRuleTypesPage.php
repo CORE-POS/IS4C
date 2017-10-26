416fa88ce2de263a4a26223ab46c12d83f4bc964
Andy Theuninck <andy@gohanman.com>
2017-10-26 09:52:24 -0500

    Correct class_exists check

--- fannie/item/PriceRuleTypesPage.php
+++ fannie/item/PriceRuleTypesPage.php
@@ -22,7 +22,7 @@
 *********************************************************************************/
 
 include(dirname(__FILE__) . '/../config.php');
-if (!class_exists('FannieAPI.php')) {
+if (!class_exists('FannieAPI')) {
     include(__DIR__ . '/../classlib2.0/FannieAPI.php');
 }
 
