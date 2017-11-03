668f56b0ac899bf2ee2c9d907570bb19f583de79
Andy Theuninck <andy@gohanman.com>
2017-11-03 11:12:42 -0500

    SQLManager changes for #941
    Creating a connection will now throw an exception if the connection
    fails with more information about *why* it failed. I simplified
    the process a little so that connectAndCreate just calls
    addConnection again w/o a database name so there isn't so much
    code duplicated between the two.

--- common/SQLManager.php
+++ common/SQLManager.php
@@ -78,8 +78,8 @@ class SQLManager
         @param $database Database name
         @param $username Database username
         @param $password Database password
-        @param $persistent Make persistent connection.
-        @param $new Force new connection
+        @param [obsolete] $persistent Make persistent connection.
+        @param [obsolote] $new Force new connection
     */
     public function __construct($server,$type,$database,$username,$password='',$persistent=false, $new=false)
     {
@@ -128,8 +128,8 @@ class SQLManager
         @param $database Database name
         @param $username Database username
         @param $password Database password
-        @param $persistent Make persistent connection.
-        @param $new Force new connection
+        @param [obsolete] $persistent Make persistent connection.
+        @param [obsolete] $new Force new connection
 
         When dealing with multiple connections, user the
         database name to distinguish which is to be used
@@ -137,37 +137,40 @@ class SQLManager
     public function addConnection($server,$type,$database,$username,$password='',$persistent=false,$new=false)
     {
         if (empty($type)) {
-            return false;
+            throw new Exception("Database type is required");
         } elseif (strtolower($type) == 'postgres9') {
             // value here is really schema. Database name must match user name.
             $savedDB = $database;
             $database = $username;
         } elseif (strtolower($type) == 'mysql' && version_compare(PHP_VERSION, '7.0.0') >= 0) {
-            $type = function_exists('mysqli_connect') ? 'mysqli' : 'pdo_mysql';
+            if (function_exists('mysqli_connect')) {
+                $type = 'mysqli';
+            } elseif (class_exists('PDO', false)) {
+                $type = 'pdo_mysql';
+            } else {
+                throw new Exception("mysql driver is not supported on PHP 7+. Use msyqli or PDO");
+            }
         }
 
         $conn = ADONewConnection($this->isPDO($type) ? 'pdo' : $type);
         $conn->SetFetchMode(ADODB_FETCH_BOTH);
         $conn = $this->setConnectTimeout($conn, $type);
-        $connected = false;
-        if (isset($this->connections[$database]) || $new) {
-            $connected = $conn->NConnect($this->getDSN($server,$type,$database),$username,$password, $database);
-        } elseif ($persistent) {
-            $connected = $conn->PConnect($this->getDSN($server,$type,$database),$username,$password,$database);
-        } else {
+        if ($database) {
             $connected = $conn->Connect($this->getDSN($server,$type,$database),$username,$password,$database);
+        } else {
+            $connected = $conn->Connect($this->getDSN($server,$type),$username,$password);
         }
         $conn = $this->clearConnectTimeout($conn, $type);
 
         if (strtolower($type) == 'postgres9') {
             $database = $savedDB;
         }
-        $this->connections[$database] = $conn;
+        $this->connections[$database ? $database : '_unselected'] = $conn;
 
         $this->last_connect_error = false;
-        if (!$connected) {
+        if (!$connected && $database) {
             $this->last_connect_error = $conn->ErrorMsg();
-            return $this->connectAndCreate($server, $type, $username, $password, $database);
+            $this->connectAndCreate($server, $type, $username, $password, $database);
         }
         $this->saveConnection($server, $type, $username, $password, $database);
 
@@ -233,30 +236,26 @@ class SQLManager
     */
     private function connectAndCreate($server, $type, $username, $password, $database)
     {
-        $conn = ADONewConnection($this->isPDO($type) ? 'pdo' : $type);
-        $conn->SetFetchMode(ADODB_FETCH_BOTH);
-        $conn = $this->setConnectTimeout($conn, $type);
-        $connected = $conn->Connect($this->getDSN($server,$type,false),$username,$password);
-        $conn = $this->clearConnectTimeout($conn, $type);
-        if ($connected) {
-            $this->last_connect_error = false;
+        $connected = $this->addConnection($server, $type, null, $username, $password);
+        if ($connected && is_object($this->connections['_unselected'])) {
+            $conn = $this->connections['_unselected'];
+            unset($this->connections['_unselected']);
             $adapter = $this->getAdapter(strtolower($type));
             $stillok = $conn->Execute($adapter->createNamedDB($database));
             if (!$stillok) {
-                $this->last_connect_error = $conn->ErrorMsg();
+                $last_connect_error = $conn->ErrorMsg();
                 $this->connections[$database] = false;
-                return false;
+                throw new Exception("Could not create database {$database} ({$last_connect_error})");
             }
             $conn->Execute($adapter->useNamedDB($database));
             $conn->SelectDB($database);
             $this->connections[$database] = $conn;
-            $this->saveConnection($server, $type, $username, $password, $database);
             return true;
-        } else {
-            $this->last_connect_error = $conn->ErrorMsg();
-            $this->connections[$database] = false;
-            return false;
         }
+
+        unset($this->connections['_unselected']);
+        $last_connect_error = $conn->ErrorMsg();
+        throw new Exception("Could not connect to database. Check username and password. ({$last_connect_error})");
     }
 
     /**
