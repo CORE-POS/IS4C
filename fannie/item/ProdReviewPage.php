4b121e596f3c38cf117e3d032e7c6f5ed8676102
Andy Theuninck <andy@gohanman.com>
2017-12-06 13:25:40 -0600

    Update ProdReview links

--- fannie/item/ProdReviewPage.php
+++ fannie/item/ProdReviewPage.php
@@ -818,9 +818,10 @@ HTML;
                     <div class="divider"></div>
                     <label>Other Pages</label>
                     <ul>
+                        <li><a href="ProdLocationEditor.php">Product Location Editor</a></li>
+                        <li><a href="ProdReviewPage.php?batchLog=1">Review Batch Log</a></li>
                         <li><a href="ProdReviewPage.php?schedule=1">Vendor Review Schedule</a></li>
                         <li><a href="ProdReviewPage.php?schedule=1&setup=1">Vendor Schedule Setup</a></li>
-                        <li><a href="ProdReviewPage.php?batchLog=1">Review Batch Log</a></li>
                     </ul>
                 </form>
                 <div class="divider hidden-md hidden-lg"></div>
