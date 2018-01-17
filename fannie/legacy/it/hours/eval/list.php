c614f22882a24d040fcee139acc8f690d3577c31
Andy Theuninck <andy@gohanman.com>
2018-01-17 10:47:24 -0600

    Re-enable old eval page

--- fannie/legacy/it/hours/eval/list.php
+++ fannie/legacy/it/hours/eval/list.php
@@ -1,5 +1,9 @@
 <?php
 include('../../../../config.php');
+/*
+header('Location: '.$FANNIE_URL.'modules/plugins2.0/WfcHoursTracking/eval/WfcHtEvalList.php');
+exit;
+ */
 
 require(__DIR__ . '/../../../../auth/login.php');
 include('../db.php');
