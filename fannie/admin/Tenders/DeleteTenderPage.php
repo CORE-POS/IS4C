6b28ef4c5bfbd5f95f119dd9927f5de51a7260b5
Andy Theuninck <andy@gohanman.com>
2018-02-20 10:49:44 -0600

    Delete tender should be a POST handler

--- fannie/admin/Tenders/DeleteTenderPage.php
+++ fannie/admin/Tenders/DeleteTenderPage.php
@@ -36,7 +36,7 @@ class DeleteTenderPage extends FannieRESTfulPage
     public $description = '[Delete Tender] gets rid of a tender type.';
     public $themed = true;
 
-    protected function get_id_view()
+    protected function post_id_view()
     {
         global $FANNIE_OP_DB;
         $dbc = FannieDB::get($FANNIE_OP_DB);
