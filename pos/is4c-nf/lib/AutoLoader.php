766c2b0bd78d32926fd8b4573940b7f60adb89c9
Andy Theuninck <andy@gohanman.com>
2018-03-01 13:55:48 -0600

    Whoops. #976
    I broke the environment manually to verify the appropriate
    error was logged, but I need to un-break it again to make
    everything actually work

--- pos/is4c-nf/lib/AutoLoader.php
+++ pos/is4c-nf/lib/AutoLoader.php
@@ -340,7 +340,6 @@ class AutoLoader
             $form = new FormValueContainer();
             $page = basename($_SERVER['PHP_SELF']);
             $class = substr($page,0,strlen($page)-4);
-            $class = '';
             if (CoreLocal::get('CashierNo') !== '' && $class != 'index' && class_exists($class)) {
                 $page = new $class($session, $form);
             } elseif ($class === '') {
