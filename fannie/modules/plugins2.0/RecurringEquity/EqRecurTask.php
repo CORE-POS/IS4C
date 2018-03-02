925974c076f96c518d1a19c337fcb684edbb3a23
Andy Theuninck <andy@gohanman.com>
2018-03-02 11:34:03 -0600

    Ignore recurring payments from 9999/99

--- fannie/modules/plugins2.0/RecurringEquity/EqRecurTask.php
+++ fannie/modules/plugins2.0/RecurringEquity/EqRecurTask.php
@@ -266,6 +266,8 @@ class EqRecurTask extends FannieTask
             SELECT *
             FROM PaycardTransactions
             WHERE dateID=?
+                AND empNo <> 9999
+                AND registerNo <> 99
                 AND transType LIKE 'R.%'
                 AND xToken IS NOT NULL
                 AND xToken <> ''
