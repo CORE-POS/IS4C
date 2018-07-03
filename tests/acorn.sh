27750ad7f885bfae3efff44e155d2df98e91a694
Andy Theuninck <andy@gohanman.com>
2018-07-03 13:33:20 -0500

    Exclude JSX from automatic .js syntax checking
    Skips node_modules too just because it's a waste of time

--- tests/acorn.sh
+++ tests/acorn.sh
@@ -2,7 +2,7 @@
 
 find ./pos/is4c-nf -type f -name '*.js' | while read file
 do
-    emsg=`node_modules/.bin/acorn --silent "$file"`;
+    emsg=`~/node_modules/.bin/acorn --silent "$file"`;
     exit=$?
     if [ "$exit" != "0" ]; then
         echo "Error in $file: $emsg"
@@ -12,7 +12,16 @@ done
 
 find ./fannie -type f -name '*.js' | while read file
 do
-    emsg=`node_modules/.bin/acorn --silent "$file"`;
+    # don't rescan vendor'd files
+    if [ "${file#*node_modules}" != "$file" ]; then
+        continue;
+    fi
+    # EndCapper has jsx that will fail
+    if [ "${file#*EndCapper}" != "$file" ]; then
+        continue;
+    fi
+    echo $file;
+    emsg=`~/node_modules/.bin/acorn --silent "$file"`;
     exit=$?
     if [ "$exit" != "0" ]; then
         echo "Error in $file: $emsg"
