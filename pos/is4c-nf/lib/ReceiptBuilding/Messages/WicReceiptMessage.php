26ae4cee1f3efd0563f058a8f20dae9651a49383
Andy Theuninck <andy@gohanman.com>
2018-08-03 13:37:45 -0500

    Add digital ephemra to eWic receipt

--- pos/is4c-nf/lib/ReceiptBuilding/Messages/WicReceiptMessage.php
+++ pos/is4c-nf/lib/ReceiptBuilding/Messages/WicReceiptMessage.php
@@ -16,6 +16,66 @@ class WicReceiptMessage extends ReceiptMessage
     {
         $date = date('Ymd');
         list($emp, $reg, $trans) = ReceiptLib::parseRef($ref);
+        $slip = '';
+
+        $dbc = Database::tDataConnect();
+        if ($reprint) {
+            $dbc = Database::mDataConnect();
+            if ($dbc === false) {
+                return '';
+            }
+        }
+
+        $transType = $dbc->concat('p.cardType', "' '", 'p.transType', '');
+
+        $query = "SELECT p.amount,
+                    p.name,
+                    p.PAN,
+                    p.refNum,
+                    $transType AS ebtMode,
+                    p.xResultMessage,
+                    p.xTransactionID,
+                    p.xBalance,
+                    p.requestDatetime AS datetime
+                  FROM PaycardTransactions AS p
+                  WHERE dateID=" . date('Ymd') . "
+                    AND empNo=" . $emp . "
+                    AND registerNo=" . $reg . "
+                    AND transNo=" . $trans . "
+                    AND p.validResponse=1
+                    AND p.xResultMessage LIKE '%APPROVE%'
+                    AND p.cardType = 'EWIC'
+                  ORDER BY p.requestDatetime";
+        $result = $dbc->query($query);
+        $prevRefNum = false;
+        while ($row = $dbc->fetchRow($result)) {
+
+            // failover to mercury's backup server can
+            // result in duplicate refnums. this is
+            // by design (theirs, not CORE's)
+            if ($row['refNum'] == $prevRefNum) {
+                continue;
+            }
+            $slip .= ReceiptLib::centerString("................................................")."\n";
+            // store header
+            for ($i=1; $i<= CoreLocal::get('chargeSlipCount'); $i++) {
+                $slip .= ReceiptLib::centerString(CoreLocal::get("chargeSlip" . $i))."\n";
+            }
+            $slip .= "\n";
+            $col1 = array();
+            $col2 = array();
+            $col1[] = $row['ebtMode'];
+            $col2[] = "Entry Method: swiped\n";
+            $col1[] = "Sequence: " . $row['xTransactionID'];
+            $col2[] = "Card: ".$row['PAN'];
+            $col1[] = "Authorization: " . $row['xResultMessage'];
+            $col2[] = ReceiptLib::boldFont() . "Amount: " . $row['amount'] . ReceiptLib::normalFont();
+            $slip .= ReceiptLib::twoColumns($col1, $col2);
+
+            $slip .= ReceiptLib::centerString("................................................")."\n";
+
+            $prevRefNum = $row['refNum'];
+        }
 
         $dbc = Database::tDataConnect();
         $emvP = $dbc->prepare('
@@ -27,9 +87,10 @@ class WicReceiptMessage extends ReceiptMessage
                 AND transNo=?
                 AND transID=?
         ');
-        $ret = $dbc->getValue($emvP, array($date, $emp, $reg, $trans, $val));
+        $balance = $dbc->getValue($emvP, array($date, $emp, $reg, $trans, $val));
+        $slip .= $balance ? $balance : '';
 
-        return $ret ? $ret : '';
+        return $slip;
     }
 }
 
