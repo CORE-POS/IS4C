0f65f4f6b6aedd6553571fd5bfc62373aa338d9d
Andy Theuninck <andy@gohanman.com>
2018-10-31 13:24:43 -0500

    Tack on copy/paste for batchgeo

--- fannie/modules/plugins2.0/CoreWarehouse/reports/CwTopReport.php
+++ fannie/modules/plugins2.0/CoreWarehouse/reports/CwTopReport.php
@@ -15,6 +15,17 @@ class CwTopReport extends FannieReportPage
     protected $sort_column = 3;
     protected $sort_direction = 1;
 
+    public function report_content() 
+    {
+        $default = parent::report_content();
+
+        if ($this->report_format == 'html') {
+            $default .= '<div class="col-sm-10 col-sm-offset-1" id="addrDiv"></div>';
+        }
+
+        return $default;
+    }
+
     public function fetch_report_data()
     {
         $settings = $this->config->get('PLUGIN_SETTINGS');
@@ -45,6 +56,7 @@ class CwTopReport extends FannieReportPage
         $prep = $this->connection->prepare($query);
         $res = $this->connection->execute($prep, $args);
         $data = array();
+        $mems = array();
         while ($row = $this->connection->fetchRow($res)) {
             $record = array(
                 sprintf('<a href="CWMemberSummaryReport.php?id=%d">%s</a>', $row['card_no'], $row['card_no']),
@@ -56,7 +68,25 @@ class CwTopReport extends FannieReportPage
             $record[] = $parts['hillside'];
             $record[] = $parts['denfeld'];
             $data[] = $record;
+            $mems[] = $row['card_no'];
         }
+
+        $table = '(BatchGeo)<br /><table class="table table-bordered small">
+            <tr><th>ID</th><th>Address</th><th>City</th><th>State</th><th>Zip</th></tr>';
+        list($inStr, $args) = $this->connection->safeInClause($mems);
+        $prep = $this->connection->prepare("
+            SELECT card_no, street, city, state, zip
+            FROM meminfo
+            WHERE card_no IN ({$inStr})");  
+        $res = $this->connection->execute($prep, $args);
+        while ($row = $this->connection->fetchRow($res)) {
+            $table .= sprintf('<tr><td>%d</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>',
+                $row['card_no'], $row['street'], $row['city'], $row['state'], $row['zip']);
+        }
+        $table = str_replace("'", '', $table);
+        $table = str_replace("\n", '', $table);
+        $table .= '</table>';
+        $this->addOnloadCommand("\$('#addrDiv').html('{$table}');");
 
         return $data;
     }
