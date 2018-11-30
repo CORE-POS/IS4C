c9c36f5757abf569885546ba4d5d671d63d2871c
Corey Sather <corey@doodleclouddevelopers.com>
2018-11-30 13:50:12 -0600

    Added 'Adv' option

--- fannie/item/RoundingFixerTool.php
+++ fannie/item/RoundingFixerTool.php
@@ -31,6 +31,7 @@ class RoundingFixerTool extends FannieRESTfulPage
 
     protected $header = 'Rounding Fixer Tool';
     protected $title = 'Rounding Fixer Tool';
+    protected $upcs = array();
 
     public $description = '[Rounding Fixer Tool] finds as set of 32
         products ending in $x.x5 and creates a batch changing price
@@ -39,6 +40,7 @@ class RoundingFixerTool extends FannieRESTfulPage
     function preprocess()
     {
         $this->__routes[] = 'get<createBatch>';
+        $this->__routes[] = 'get<createBatchAdv>';
 
         return parent::preprocess();
     }
@@ -49,9 +51,112 @@ class RoundingFixerTool extends FannieRESTfulPage
 <p>Use this page to create a price-change batch for a set of 
     products ending in \$x.x5 and updating them to \$x.x9 to follow 
     WFC price-rounding-rules.</p>
-<p><a class="btn btn-default" 
-   href="{$_SERVER['PHP_SELF']}?createBatch=true&i=1">Create</a> 
-   a new batch.</p>
+<ul>
+    <li><div class="form-group"><a class="btn btn-default" 
+        href="{$_SERVER['PHP_SELF']}?createBatch=true&i=1">Create A</a> Update x.x5 to x.x9.</div></li>
+    <li><div class="form-group"><a class="btn btn-default"
+        href="{$_SERVER['PHP_SELF']}?createBatchAdv=true&i=1">Create B</a> Correct invalid price endings.</div></li>
+</ul>
+</table>
+HTML;
+    }
+
+    function get_createBatchAdv_view()
+    {
+        $i = FormLib::get('i');
+        $rounder = new \COREPOS\Fannie\API\item\PriceRounder();
+        $date1 = new DateTime();
+        $date2 = new DateTime();
+        $date2 = $date2->sub(new DateInterval('P2W'));
+        $dlog = DTransactionsModel::selectDlog(
+            $date2->format('Y-m-d 00:00:00'), $date1->format('Y-m-d 00:00:00'));
+        $dbc = FannieDB::get($this->config->get('OP_DB'));
+        $ret = "";
+
+        $args = array(
+            $date2->format('Y-m-d H:i:s'),
+            $date1->format('Y-m-d H:i:s'),
+        );
+        $prep = $dbc->prepare("
+            SELECT p.upc, p.brand, p.description, p.normal_price, p.department,
+                m.super_name
+            FROM ". $dlog ." AS d
+                LEFT JOIN products AS p ON d.upc=p.upc
+                LEFT JOIN MasterSuperDepts AS m ON p.department=m.dept_ID
+            WHERE d.tdate BETWEEN ? AND ?
+                AND (RIGHT(normal_price, 2) NOT IN (
+                    29, 39, 49, 69, 79, 89, 99) 
+                AND normal_price BETWEEN 0 AND 1)
+                OR (RIGHT(normal_price, 2) NOT IN (
+                    19, 39, 49, 69, 89, 99) 
+                AND normal_price BETWEEN 1 AND 3)
+                OR (RIGHT(normal_price, 2) NOT IN (
+                   39, 69, 99) 
+                AND normal_price BETWEEN 3 AND 6)
+                OR (RIGHT(normal_price, 2) NOT IN (
+                   99) 
+                AND normal_price BETWEEN 6 AND 1
+                )
+                AND p.default_vendor_id = 1
+                AND p.price_rule_id = 0
+                AND p.upc NOT IN (
+                    SELECT upc 
+                    FROM batchList AS bl
+                    LEFT JOIN batches AS b ON bl.batchID=b.batchID
+                    WHERE batchName like 'UNFI ROUNDING FIXER%'
+                )
+            GROUP BY d.upc
+            ORDER BY p.department
+            LIMIT 32;
+        ");
+        $res = $dbc->execute($prep, $args);
+        $upcs = array();
+        $data = array();
+        while ($row = $dbc->fetchRow($res)) {
+            $upcs[$row['upc']] = $row['normal_price'];
+            $data[$row['upc']]['brand'] = $row['brand'];
+            $data[$row['upc']]['description'] = $row['description'];
+            $data[$row['upc']]['dept'] = $row['super_name'];
+            $this->upcs[$row['upc']] = $rounder->round($row['normal_price']);
+        }
+        
+        $table = "<div class='table-responsive'><table class='table table-condensed small'>
+            <thead><th>upc</th><th>Brand</th><th>Description</th><th>Department</th>
+            <th>Current Price</th><th>New Price</th></thead>";
+        foreach ($upcs as $upc => $price) {
+            $table .= "<tr>"; 
+            $newprice = $rounder->round($price);
+            $table .= "<td>$upc</td><td>{$data[$upc]['brand']}</td>
+                <td>{$data[$upc]['description']}</td><td>{$data[$upc]['dept']}</td>
+                <td>$price</td><td>$newprice</td>";
+            $table .= "</tr>";
+        }
+        $table .= "</table></div>";
+
+        list($saved, $batchID) = $this->createBatch($dbc, $i);
+        $name = "UNFI ROUNDING FIXER ".$date1->format('Y-m-d')." [$i]";
+
+        if ($saved == true) {
+            $alertType = 'success';
+            $alertVerb = ' creation successful';
+            $i++;
+        } else {
+            $alertType = 'danger';
+            $alertVerb = ' creation failed';
+        }
+
+        return <<<HTML
+<div class="container">
+<p><a class="btn btn-default" href="{$_SERVER['PHP_SELF']}?createBatchAdv=true&i=$i">
+    Create</a> another batch</p>
+<div class="alert alert-$alertType">
+<div>Batch: "$name" $alertVerb.</div>
+<p><a href="../batches/newbatch/EditBatchPage.php?id=$batchID" target="_blank">
+    view</a> in Batch Editor.</p>
+<div>
+<h4>$name</h4>
+$table
+</div></div>
 HTML;
     }
 
@@ -98,6 +203,7 @@ HTML;
             $data[$row['upc']]['brand'] = $row['brand'];
             $data[$row['upc']]['description'] = $row['description'];
             $data[$row['upc']]['dept'] = $row['super_name'];
+            $this->upcs[$row['upc']] = $row['normal_price'] + .04;;
         }
         
         $table = "<div class='table-responsive'><table class='table table-condensed small'>
@@ -112,8 +218,39 @@ HTML;
             $table .= "</tr>";
         }
         $table .= "</table></div>";
-        //create a new batch
+
+        list($saved, $batchID) = $this->createBatch($dbc, $i);
         $name = "UNFI ROUNDING FIXER ".$date1->format('Y-m-d')." [$i]";
+
+        if ($saved == true) {
+            $alertType = 'success';
+            $alertVerb = ' creation successful';
+            $i++;
+        } else {
+            $alertType = 'danger';
+            $alertVerb = ' creation failed';
+        }
+
+        return <<<HTML
+<div class="container">
+<p><a class="btn btn-default" href="{$_SERVER['PHP_SELF']}?createBatch=true&i=$i">
+    Create</a> another batch</p>
+<div class="alert alert-$alertType">
+<div>Batch: "$name" $alertVerb.</div>
+<p><a href="../batches/newbatch/EditBatchPage.php?id=$batchID" target="_blank">
+    view</a> in Batch Editor.</p>
+<div>
+<h4>$name</h4>
+$table
+</div></div>
+HTML;
+    }
+
+    private function createBatch($dbc, $i)
+    {
+        $upcs = $this->upcs;
+        $date = new DateTime();
+        $name = "UNFI ROUNDING FIXER ".$date->format('Y-m-d')." [$i]";
         $batch = new BatchesModel($dbc);
         $batch->startDate('');
         $batch->endDate('');
@@ -155,29 +292,8 @@ HTML;
             $bu->logUpdate($bu::UPDATE_ADDED);
         }
         $dbc->commitTransaction();
-        $i++;
 
-        if ($saved == true) {
-            $alertType = 'success';
-            $alertVerb = ' creation successful';
-        } else {
-            $alertType = 'danger';
-            $alertVerb = ' creation failed';
-        }
-
-        return <<<HTML
-<div class="container">
-<p><a class="btn btn-default" href="{$_SERVER['PHP_SELF']}?createBatch=true&i=$i">
-    Create</a> another batch</p>
-<div class="alert alert-$alertType">
-<div>Batch: "$name" $alertVerb.</div>
-<p><a href="../batches/newbatch/EditBatchPage.php?id=$batchID" target="_blank">
-    view</a> in Batch Editor.</p>
-<div>
-<h4>$name</h4>
-$table
-</div></div>
-HTML;
+        return array($saved, $batchID);
     }
 
     public function helpContent()
