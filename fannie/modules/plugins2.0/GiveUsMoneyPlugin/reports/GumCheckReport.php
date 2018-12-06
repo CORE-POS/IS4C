b324196ddfe53fbd778b8cb550175d317e21f440
Andy Theuninck <andy@gohanman.com>
2018-12-06 14:37:03 -0600

    Checks report for GiveUsMoneyPlugin

--- fannie/modules/plugins2.0/GiveUsMoneyPlugin/reports/GumCheckReport.php
+++ fannie/modules/plugins2.0/GiveUsMoneyPlugin/reports/GumCheckReport.php
@@ -1,63 +1,84 @@
 <?php
+/*******************************************************************************
 
-include(__DIR__ . '/../../../../config.php');
+    Copyright 2013 Whole Foods Co-op
+
+    This file is part of IT CORE.
+
+    IT CORE is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    IT CORE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    in the file license.txt along with IT CORE; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*********************************************************************************/
+
+include(dirname(__FILE__).'/../../../../config.php');
 if (!class_exists('FannieAPI')) {
     include(__DIR__ . '/../../../../classlib2.0/FannieAPI.php');
 }
 
-class GumCheckReport extends FannieReportPage
+/**
+*/
+class GumCheckReport extends FannieReportPage 
 {
-    protected $report_headers = array('Issue Date', 'Amount', 'Check#', 'Memo', 'Program', 'Account#', 'Name');
+    public $discoverable = false; // access is very restricted; no need to list
+                                  // as an available report
 
+    protected $must_authenticate = true;
+    protected $auth_classes = array('GiveUsMoney');
+
+    protected $required_fields = array('date1', 'date2');
+    protected $report_headers = array('Check#', 'Issue Date', 'Amount', 'Additional Info');
+
+    public function preprocess()
+    {
+        $this->header = 'Checks Report';
+        $this->title = 'Checks Report';
+
+        return parent::preprocess();
+    }
+    
     public function fetch_report_data()
     {
-        $settings = $this->config->get('PLUGIN_SETTINGS');
-        $prefix = $settings['GiveUsMoneyDB'] . $this->connection->sep();
+        global $FANNIE_PLUGIN_SETTINGS, $FANNIE_OP_DB;
+        $dbc = FannieDB::get($FANNIE_PLUGIN_SETTINGS['GiveUsMoneyDB']);
 
-        $query = "
-            SELECT p.issueDate,
-                p.amount,
-                p.checkNumber,
-                p.reason as memo,
-                CASE
-                    WHEN d.gumPayoffID IS NOT NULL THEN 'DIVIDEND'
-                    WHEN l.gumPayoffID IS NOT NULL THEN 'LOAN'
-                    ELSE '?'
-                END AS program,
-                d.gumDividendID
-            FROM {$prefix}GumPayoffs AS p
-                LEFT JOIN {$prefix}GumDividendPayoffMap AS d ON p.gumPayoffID=d.gumPayoffID
-                LEFT JOIN {$prefix}GumLoanPayoffMap AS l ON p.gumPayoffID=l.gumPayoffID
-            WHERE reason LIKE '%DIVIDEND%'
-            ORDER BY p.issueDate,
-                p.checkNumber";
-        $dividendP = $this->connection->prepare("
-            SELECT c.FirstName,
-                c.LastName,
-                d.card_no
-            FROM {$prefix}GumDividends AS d
-                LEFT JOIN custdata AS c ON d.card_no=c.CardNo AND c.personNum=1
-            WHERE d.gumDividendID=?");
-        $res = $this->connection->query($query);
+        $prep = $dbc->prepare("
+            SELECT * FROM GumPayoffs
+            WHERE issueDate BETWEEN ? AND ?");
+        $res = $dbc->execute($prep, array($this->form->date1, $this->form->date2 . ' 23:59:59'));
         $data = array();
-        while ($row = $this->connection->fetchRow($res)) {
-            $record = array(
+        while ($row = $dbc->fetchRow($res)) {
+            $data[] = array(
+                $row['checkNumber'],
                 $row['issueDate'],
                 $row['amount'],
-                $row['checkNumber'],
-                $row['memo'],
-                $row['program'],
+                $row['reason'] . ' ' . $row['alternateKey'],
             );
-            if ($row['program'] === 'DIVIDEND') {
-                $div = $this->connection->getRow($dividendP, array($row['gumDividendID']));
-                $record[] = $div['card_no'];
-                $record[] = $div['FirstName'] . ' ' . $div['LastName'];
-            }
-            $data[] = $record;
         }
-
         return $data;
     }
+
+    public function form_content()
+    {
+        $dates = FormLib::standardDateFields();
+        return <<<HTML
+<form method="get">
+    {$dates}
+    <p><button type="submit" class="btn btn-default">List Checks</button></p>
+</form>
+HTML;
+    }
+
 }
 
 FannieDispatch::conditionalExec();
