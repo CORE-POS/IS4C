0fe8e51511c628a02109ddd2174f0e079aaf71bb
Andy Theuninck <andy@gohanman.com>
2018-12-06 11:18:38 -0600

    Efficency tweaks
    Instead of picking the lowest numbered department, pick
    the highest sales department.
    
    When fixing 5s, pull in vendorItems.srp and use that if
    it isn't also a x.x5 so changed items don't end up in
    the incorrectly rounded 9s list next.

--- fannie/item/RoundingFixerTool.php
+++ fannie/item/RoundingFixerTool.php
@@ -47,6 +47,9 @@ class RoundingFixerTool extends FannieRESTfulPage
 
     function get_view() {
 
+        $this->addScript('../src/javascript/chosen/chosen.jquery.min.js');
+        $this->addCssFile('../src/javascript/chosen/bootstrap-chosen.css');
+        $this->addOnloadCommand("\$('select.chosen').chosen();\n");
         $vendors = new VendorsModel($this->connection);
         $vOpts = $vendors->toOptions();
         $supers = new MasterSuperDeptsModel($this->connection);
@@ -58,7 +61,7 @@ class RoundingFixerTool extends FannieRESTfulPage
 <form method="get" action="RoundingFixerTool.php">
 <div class="form-group">
     <label>Vendor</label>
-    <select name="vendor" class="form-control">
+    <select name="vendor" class="form-control chosen">
         <option value="">Optional</option>
         {$vOpts}
     </select>
@@ -108,6 +111,42 @@ HTML;
             $args[] = $super;
         }
         $prep = $dbc->prepare("
+            SELECT p.department
+            FROM ". $dlog ." AS d
+                LEFT JOIN products AS p ON d.upc=p.upc
+                LEFT JOIN MasterSuperDepts AS m ON p.department=m.dept_ID
+            WHERE d.tdate BETWEEN ? AND ?
+                AND (RIGHT(normal_price, 2) NOT IN (
+                    29, 39, 49, 69, 79, 89, 99)
+                AND normal_price BETWEEN 0 AND 1)
+                OR (RIGHT(normal_price, 2) NOT IN (
+                    19, 39, 49, 69, 89, 99)
+                AND normal_price BETWEEN 1 AND 3)
+                OR (RIGHT(normal_price, 2) NOT IN (
+                   39, 69, 99)
+                AND normal_price BETWEEN 3 AND 6)
+                OR (RIGHT(normal_price, 2) NOT IN (
+                   99)
+                AND normal_price BETWEEN 6 AND 1
+                )
+                AND p.price_rule_id = 0
+                " . $extraWhere . "
+                AND p.upc NOT IN (
+                    SELECT upc
+                    FROM batchList AS bl
+                    LEFT JOIN batches AS b ON bl.batchID=b.batchID
+                    WHERE batchName like 'UNFI ROUNDING FIXER%'
+                )
+            GROUP BY p.department
+            ORDER BY SUM(total) DESC
+            LIMIT 1;
+        ");
+        $department = $dbc->getValue($prep, $args);
+        $orderBy = 'p.department';
+        if ($department) {
+            $orderBy = sprintf('CASE WHEN p.department=%d THEN 1 ELSE p.department+99999 END', $department);
+        }
+        $prep = $dbc->prepare("
             SELECT p.upc, p.brand, p.description, p.normal_price, p.department,
                 m.super_name
             FROM ". $dlog ." AS d
@@ -136,7 +175,7 @@ HTML;
                     WHERE batchName like 'UNFI ROUNDING FIXER%'
                 )
             GROUP BY d.upc
-            ORDER BY p.department
+            ORDER BY " . $orderBy . "
             LIMIT 32;
         ");
         $res = $dbc->execute($prep, $args);
@@ -216,9 +255,9 @@ HTML;
             $extraWhere .= ' AND m.superID=? ';
             $args[] = $super;
         }
+
         $prep = $dbc->prepare("
-            SELECT p.upc, p.brand, p.description, p.normal_price, p.department,
-                m.super_name
+            SELECT p.department
             FROM ". $dlog ." AS d
                 LEFT JOIN products AS p ON d.upc=p.upc
                 LEFT JOIN MasterSuperDepts AS m ON p.department=m.dept_ID
@@ -231,8 +270,33 @@ HTML;
                     LEFT JOIN batches AS b ON bl.batchID=b.batchID
                     WHERE batchName like 'UNFI ROUNDING FIXER%'
                 )
+            GROUP BY p.department
+            ORDER BY SUM(d.total) DESC
+            LIMIT 1;
+        ");
+        $department = $dbc->getValue($prep, $args);
+        $orderBy = 'p.department';
+        if ($department) {
+            $orderBy = sprintf('CASE WHEN p.department=%d THEN 1 ELSE p.department+99999 END', $department);
+        }
+        $prep = $dbc->prepare("
+            SELECT p.upc, p.brand, p.description, p.normal_price, p.department,
+                m.super_name, v.srp
+            FROM ". $dlog ." AS d
+                LEFT JOIN products AS p ON d.upc=p.upc
+                LEFT JOIN MasterSuperDepts AS m ON p.department=m.dept_ID
+                LEFT JOIN vendorItems AS v ON p.upc=v.upc AND p.default_vendor_id=v.vendorID
+            WHERE d.tdate BETWEEN ? AND ?
+                AND RIGHT(p.normal_price, 1) = 5
+                " . $extraWhere . "
+                AND p.upc NOT IN (
+                    SELECT upc
+                    FROM batchList AS bl
+                    LEFT JOIN batches AS b ON bl.batchID=b.batchID
+                    WHERE batchName like 'UNFI ROUNDING FIXER%'
+                )
             GROUP BY d.upc
-            ORDER BY p.department
+            ORDER BY " . $orderBy . "
             LIMIT 32;
         ");
         $res = $dbc->execute($prep, $args);
@@ -244,6 +308,9 @@ HTML;
             $data[$row['upc']]['description'] = $row['description'];
             $data[$row['upc']]['dept'] = $row['super_name'];
             $this->upcs[$row['upc']] = $row['normal_price'] + .04;;
+            if (substr($row['srp'], -1) == '9') {
+                $this->upcs[$row['upc']] = $row['srp'];
+            }
         }
 
         $table = "<div class='table-responsive'><table class='table table-condensed small'>
