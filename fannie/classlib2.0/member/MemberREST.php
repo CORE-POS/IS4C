96a994101c96793818c7b2cf32f2c5a25251702a
Andy Theuninck <andy@gohanman.com>
2018-12-17 10:15:37 -0600

    Also for #1009
    There's no reason to allow extra whitespace into the columns
    in the first place

--- fannie/classlib2.0/member/MemberREST.php
+++ fannie/classlib2.0/member/MemberREST.php
@@ -579,6 +579,9 @@ class MemberREST
                 foreach ($columns as $col_name => $info) {
                     if ($col_name == 'cardNo') continue;
                     if ($col_name == 'modified') continue;
+                    if ($col_name == 'lastName' || $col_name == 'firstName') {
+                        $c_json[$col_name] = trim($c_json[$col_name]);
+                    }
 
                     if (isset($c_json[$col_name])) {
                         $customers->$col_name($c_json[$col_name]);
@@ -718,11 +721,11 @@ class MemberREST
                     $personNum++;
                 }
                 if (isset($c_json['firstName'])) {
-                    $loopCD->FirstName($c_json['firstName']);
+                    $loopCD->FirstName(trim($c_json['firstName']));
                     $loopCD_changed = true;
                 }
                 if (isset($c_json['lastName'])) {
-                    $loopCD->LastName($c_json['lastName']);
+                    $loopCD->LastName(trim($c_json['lastName']));
                     $loopCD_changed = true;
                 }
                 if (isset($c_json['chargeAllowed'])) {
