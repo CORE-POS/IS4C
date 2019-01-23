c49fefa8fc882bd9f6e698e1743308d707b6ad28
Andy Theuninck <andy@gohanman.com>
2019-01-23 13:26:25 -0600

    Use MT weight protocol instead

--- pos/is4c-nf/scale-drivers/drivers/NewMagellan/SPH_SMA_Scale.cs
+++ pos/is4c-nf/scale-drivers/drivers/NewMagellan/SPH_SMA_Scale.cs
@@ -10,7 +10,7 @@ namespace SPH {
 public class SPH_SPA_Scale : SerialPortHandler 
 {
 
-    private const char STX = '\n';
+    private const char STX = ((char)2);
     private const char ETX = '\r';
 
     public SPH_SPA_Scale(string p) : base(p)
@@ -18,9 +18,9 @@ public class SPH_SPA_Scale : SerialPortHandler
         sp = new SerialPort();
         sp.PortName = this.port;
         sp.BaudRate = 9600;
-        sp.DataBits = 8;
+        sp.DataBits = 7;
         sp.StopBits = StopBits.One;
-        sp.Parity = Parity.None;
+        sp.Parity = Parity.Even;
         sp.RtsEnable = true;
         sp.Handshake = Handshake.None;
         sp.ReadTimeout = 500;
@@ -62,39 +62,31 @@ public class SPH_SPA_Scale : SerialPortHandler
     private void GetWeight()
     {
         try {
-            this.sp.Write(STX + "P" + ETX);
+            this.sp.Write("W");
         } catch (Exception) {
         }
     }
 
     private string TranslateResponse(string resp)
     {
-        if (resp.Length < 15) {
-            return "";
+        if (resp.Length == 5) {
+            return "S11" + resp.Replace(".", "");
+        } else if (resp.Length == 2) {
+            int status = (int)resp[1];
+            if ((status & 1) != 0) {
+                return "S141";
+            } else if ((status & 2) != 0) {
+                return "S142";
+            } else if ((status & 4) != 0) {
+                return "S145";
+            }
         }
 
-        var status = resp.Substring(0, 1);
-        var motion = resp.Substring(3, 1);
-        var weight = resp.Substring(5, 10).Trim();
-
-        if (motion == "M") {
-            return "S141";
-        } else if (status == "O") {
-            return "S142";
-        } else if (status == "U") {
-            return "S145";
-        } else if (status == "E" || status == "I" || status == "T") {
-            return "S140";
-        } else if (status == "Z") {
-            return "S11000";
-        } else if (weight.Length > 0) {
-            // format is xxxx.xxx with three decimal places
-            // take out the decimal point and convert to just two
-            // decimal places since that's what the POS side understands
-            var fraction = weight.Substring(weight.Length - 3).Substring(0, 2);
-            var whole = weight.Substring(0, weight.Length - 4);
-            return "S11" + whole + fraction;
-        }
+        Console.WriteLine("Unknown command: " + resp);
+        var encoder = new System.Text.ASCIIEncoding();
+        var bytes = encoder.GetBytes(resp);
+        var hex = BitConverter.ToString(bytes);
+        Console.WriteLine("As hex: " + hex);
 
         return "";
     }
