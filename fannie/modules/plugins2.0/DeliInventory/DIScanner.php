3bedb344abcab44d31aa31221601ecd3a9665457
Andy Theuninck <andy@gohanman.com>
2019-02-01 09:39:39 -0600

    Separate DI scan tool

---
+++ fannie/modules/plugins2.0/DeliInventory/DIScanner.php
@@ -0,0 +1,167 @@
+<?php
+
+include(dirname(__FILE__).'/../../../config.php');
+if (!class_exists('FannieAPI')) {
+    include(__DIR__ . '/../../../classlib2.0/FannieAPI.php');
+}
+
+class DIScanner extends FannieRESTfulPage 
+{
+    public $page_set = 'Plugin :: Deli Inventory';
+    public $description = '[Deli Inventory Scanner] is a tool for entering deli inventory';
+    protected $title = 'Deli Inventory';
+    protected $header = '';
+
+    public function preprocess()
+    {
+        $this->addRoute('post<id><qty>', 'get<search>');
+
+        return parent::preprocess();
+    }
+
+    protected function get_search_handler()
+    {
+        $dbc = $this->connection;
+        $store = COREPOS\Fannie\API\lib\Store::getIdByIp();
+        $prep = $dbc->prepare("SELECT upc, item
+            FROM deliInventoryCat
+            WHERE upc is not null
+                AND upc <> ''
+                AND storeID=?
+                AND item LIKE ?
+            ORDER BY item");
+        $res = $dbc->execute($prep, array($store, '%' . $this->search . '%'));
+        $ret = array();
+        while ($row = $dbc->fetchRow($res)) {
+            $ret[] = array(
+                'label' => $row['item'],
+                'value' => $row['upc'],
+            );
+        }
+
+        echo json_encode($ret);
+
+        return false;
+    }
+
+    protected function post_id_qty_handler()
+    {
+        $realQty = FormLib::get('real');
+        $dbc = $this->connection;
+        $store = COREPOS\Fannie\API\lib\Store::getIdByIp();
+
+        $prep = $dbc->prepare("
+            UPDATE deliInventoryCat
+            SET fraction=?
+            WHERE upc=?
+                AND storeID=?");
+        $res = $dbc->execute($prep, array($realQty, BarcodeLib::padUPC($this->id), $store));
+        echo $this->getRecent($dbc);
+
+        return false;
+    }
+
+    private function getRecent($dbc)
+    {
+        $store = COREPOS\Fannie\API\lib\Store::getIdByIp();
+        $query = "SELECT d.upc, d.item,
+                d.fraction
+            FROM dleiInventoryCat AS d
+            WHERE d.storeID=?";
+        $args = array($store);
+        $query = $dbc->addSelectLimit($query, 15);
+        $prep = $dbc->prepare($query);
+        $res = $dbc->execute($prep, $args);
+        $ret = '<table class="table table-bordered tabled-striped">';
+        while ($row = $dbc->fetchRow($res)) {
+            $item = $row['upc'] . ' ' . $row['item'];
+            $ret .= sprintf('<tr><td>%.2f</td><td>%s</td></tr>', $row['fraction'], $item);
+        }
+        $ret .= '</table>';
+
+        return $ret;
+    }
+
+    protected function post_id_handler()
+    {
+        $store = COREPOS\Fannie\API\lib\Store::getIdByIp();
+        $dbc = $this->connection;
+        $prep = $dbc->prepare("
+            SELECT d.upc, d.item,
+                d.fraction
+            FROM deliInventoryCat AS d
+            WHERE d.storeID=?
+                AND d.upc=?");
+        $this->id = BarcodeLib::padUPC($this->id);
+        if (substr($this->id, 0, 3) == "002") {
+            $this->id = substr($this->id, 0, 7) . '000000';
+        }
+        $row = $dbc->getRow($prep, array($store, $this->id));
+        if ($row === false) {
+            echo '<div class="alert alert-danger">Item not found' . $this->id . '</div>' . 'err:' . $dbc->error();
+            return false;
+        }
+        $item = $row['upc'] . ' ' . $row['item'];
+        if (!$row['fraction']) {
+            $row['fraction'] = 0;
+        }
+
+        echo <<<HTML
+<h3>{$item}</h3>
+<div class="row lead">
+    <div class="col-sm-3">
+        Current quantity: <span id="curQty">{$row['fraction']}</span>
+        <input type="hidden" id="lastQty" value="{$row['fraction']}" />
+    </div>
+    <div class="col-sm-3">
+        <div class="input-group">
+            <span class="input-group-addon">Quantity</span>
+            <input type="number" name="qty" id="newQty" class="form-control" 
+                onkeyup="scanner.keybind(event);" onkeydown="scanner.tab(event);"
+                min="-999" max="999" step="1" />
+        </div> 
+    </div>
+    <input type="hidden" name="id" value="{$this->id}" />
+</div>
+HTML;
+
+        return false;
+    }
+
+    function css_content()
+    {
+        return <<<CSS
+input.focused {
+    background: #ffeebb;
+}
+CSS;
+    }
+
+    protected function get_view()
+    {
+        $this->addScript('scanner.js');
+        $this->addOnloadCommand("scanner.autocomplete('#upc');");
+        $this->addOnloadCommand("\$('#upc').on('autocompleteselect', function(event, ui) { scanner.autosubmit(event, ui); });");
+        $this->addOnloadCommand("\$('#upc').focus();");
+
+        return <<<HTML
+<form method="post" id="searchform" onsubmit="scanner.search(); return false;">
+    <hr />
+    <p>
+        <div class="input-group">
+            <span class="input-group-addon">Search</span>
+            <input type="text" name="id" id="upc" class="form-control focused" />
+            <span class="input-group-btn">
+                <button type="submit" class="btn btn-default" tabindex="-1">Go</button>
+            </span>
+        </div>
+    </p>
+</form>
+<div id="results"></div>
+<div id="recent"></div>
+HTML;
+    }
+}
+
+FannieDispatch::conditionalExec();
+
---
+++ fannie/modules/plugins2.0/DeliInventory/scanner.js
@@ -0,0 +1,98 @@
+var scanner = (function ($) {
+    var mod = {};
+
+    var lastSearch = 0;
+
+    var repaint = function(a, b) {
+        $(a).addClass('focused');
+        $(b).removeClass('focused');
+    };
+
+    mod.setFilter = function(f) {
+        if (f == "") {
+            EXTRA_AUTO_COMPLETE_PARAMS = { lc: true };
+        } else {
+            EXTRA_AUTO_COMPLETE_PARAMS = { lc: true, superID: f };
+        }
+    };
+
+    mod.search = function() {
+        if ($('#upc').val().length > 0) {
+            transferring = true;
+            $.ajax({
+                type: 'post',
+                data: $('#searchform').serialize()
+            }).fail(function (xhr, st, err) {
+                $('#results').html('<div class="alert alert-danger">Error (' + st + ',' + err + ')</div>');
+                transferring = false;
+            }).done(function (resp) {
+                lastSearch = new Date().getTime();
+                $('#results').html(resp); 
+                $('#upc').val('');
+                if ($('#newQty').length > 0) {
+                    $('#newQty').focus();
+                    repaint('#newQty', '#upc');
+                }
+            });
+        } else {
+            $('#results').html('');
+        }
+    };
+
+    mod.tab = function(ev) {
+        if (ev.which == 9) {
+            ev.preventDefault();
+            $('#upc').focus();
+            repaint('#upc', '#newQty');
+        }
+    };
+
+
+    mod.keybind = function(ev) {
+        repaint('#newQty', '#upc');
+        if (new Date().getTime() > (100 + lastSearch)) {
+            console.log(ev.which);
+            if (ev.which == 13) {
+                $('#upc').focus();
+                repaint('#upc', '#newQty');
+            }
+            var cur = $('#lastQty').val();
+            var qty = $('#newQty').val();
+            var next = (cur*1) + (qty*1);
+            $('#curQty').html(next);
+            $.ajax({
+                type: 'post', 
+                data: $('#results input').serialize()+"&real="+next
+            }).done(function (resp) {
+                $('#recent').html(resp);
+            });
+        }
+    };
+
+    mod.autosubmit = function(ev, ui) {
+        $('#upc').val(ui.item.value);
+        mod.search();
+        console.log("Current: " + $('#upc').val());
+    };
+
+    mod.autocomplete = function(selector) {
+        $(selector).autocomplete({
+            source: function(request, callback) {
+                $.ajax({
+                    url: 'DIScanner.php',
+                    type: 'get',
+                    data: 'search=' + request.term,
+                    dataType: 'json'
+                }).done(function (data) {
+                    callback(data);
+                }).fail(function () {
+                    callback([]);
+                });
+            },
+            minLength: 2
+        });
+    };
+
+    return mod;
+
+})(jQuery);
