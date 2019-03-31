00e40f758efa7ce851498f9d6ecbb4fe9011964e
Narendra Varma <narendra@ourtable.us>
2019-03-30 19:07:46 -0700

    Our Table specific HH menu

--- pos/is4c-nf/plugins/QuickMenus/quickmenus/2.php
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/2.php
@@ -1,10 +1,11 @@
 <?php
 
 $my_menu = array(
-	"Certified Local Soup - $8.99" => "2020",
-	"Certified Local Jam - $4.99" => "2022",
-	"Kale Salad Sm. - $4.99" => "2037",
-	"Kale Salad Lg. - $6.99" => "2038",
-	"Parsley Pesto - $6.99" => "2019"
+	"Hummus To Dip - $6" => "7406",
+	"Salad - $7.50" => "7427",
+	"Pizza Red - $14.00" => "7356",
+	"Pizza Veggie - $14.00" => "7437",
+	"Pizza Cheese - $14.00" => "7511",
+	"Add Mama Lil's - $1.00" => "7281"
 );
 
