74a958c005e4d6132f372390026e191bbed890be
Narendra Varma <narendra@ourtable.us>
2019-04-03 08:18:44 -0700

    Added notes on bypassing Powershell ExecutionPolicy

--- pos/is4c-nf/SetLaneOffline.ps1
+++ pos/is4c-nf/SetLaneOffline.ps1
@@ -1,4 +1,7 @@
+# Run this script on Windows using the command:
+#      powershell.exe -ExecutionPolicy Bypass -File ./SetLaneOffline.ps1
+#
 ###### CHANGE lanenumber below to the actual lane number for this lane #######
-$postParams = @{id='lanenumber';up='0'}
+$postParams = @{id='4';up='0'}
 
 Invoke-WebRequest -Uri http://POS-Server/Our-Table/fannie/admin/LaneStatus.php -Method POST -Body $postParams -UseBasicParsing
--- pos/is4c-nf/SetLaneOnline.ps1
+++ pos/is4c-nf/SetLaneOnline.ps1
@@ -1,4 +1,7 @@
+# Run this script on Windows using the command:
+#      powershell.exe -ExecutionPolicy Bypass -File ./SetLaneOnline.ps1
+#
 ###### CHANGE lanenumber below to the actual lane number for this lane #######
-$postParams = @{id='lanenumber';up='1'}
+$postParams = @{id='4';up='1'}
 
 Invoke-WebRequest -Uri http://POS-Server/Our-Table/fannie/admin/LaneStatus.php -Method POST -Body $postParams -UseBasicParsing
