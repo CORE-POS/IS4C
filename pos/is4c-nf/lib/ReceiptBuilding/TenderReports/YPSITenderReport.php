114308328985f2748bc9d8c721c9af02b349df9d
Narendra Varma <narendra@ourtable.us>
2019-04-06 07:38:33 -0700

    Undo errors in commit fa2452ea2

--- pos/is4c-nf/lib/ReceiptBuilding/TenderReports/YPSITenderReport.php
+++ pos/is4c-nf/lib/ReceiptBuilding/TenderReports/YPSITenderReport.php
@@ -52,10 +52,10 @@ static public function get($session)
     $cashier_names = "";
     $cashierQ = "SELECT CONCAT(SUBSTR(e.FirstName,1,1),e.Lastname) as cashier
         FROM dlog d, opdata.employees e
-        WHERE d.emp_no = e.emp_no AND d.register_no = ". $session->get('laneno')." AND d.emp_no <> 99999 AND d.trans_type <> 'L' 
+        WHERE d.emp_no = e.emp_no AND d.register_no = ". $session->get('laneno')." AND d.emp_no <> 9999 AND d.trans_type <> 'L'
         AND d.tdate >= '".$shiftCutoff."'
         GROUP BY d.emp_no ORDER BY d.tdate";
-        
+
     $cashierR = $dba->query($cashierQ);
 
     for ($i = 0; $i < $row = $dba->fetchRow($cashierR); $i++) {
@@ -186,15 +186,15 @@ function trTotal($session,$k, $label,$i=False)
         $q = 'department';
     }
     // $q = (!is_numeric($k)) ? 'trans_subtype' : 'department';
-    
+
     if($i===False) {
-        $tenderQ = "SELECT -SUM(total) AS net, COUNT(total) FROM dlog 
+        $tenderQ = "SELECT -SUM(total) AS net, COUNT(total) FROM dlog
             WHERE register_no=".$session->get('laneno').
-            " AND $q IN($k) AND tdate >= '$shiftCutoff' AND emp_no <> 99999";
+            " AND $q IN($k) AND tdate >= '$shiftCutoff' AND emp_no <> 9999";
     } else {
-        $tenderQ = "SELECT tdate,register_no,emp_no,trans_no,card_no,total FROM dlog 
+        $tenderQ = "SELECT tdate,register_no,emp_no,trans_no,card_no,total FROM dlog
             WHERE register_no=".$session->get('laneno').
-            " and $q IN($k) AND tdate >= '$shiftCutoff' AND emp_no <> 99999 order by tdate";
+            " and $q IN($k) AND tdate >= '$shiftCutoff' AND emp_no <> 9999 order by tdate";
     }
     $tenderR = $dba->query($tenderQ);
     $tender = $dba->fetchRow($tenderR);
