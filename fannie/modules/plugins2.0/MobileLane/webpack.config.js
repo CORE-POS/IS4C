e1afa4bb58eb53c8cebd179d83ebec388243a863
Andy Theuninck <andy@gohanman.com>
2019-05-01 10:30:37 -0500

    Tweaks to update jquery; unsure if js build tools are spot on still. Not critical plugin

--- fannie/modules/plugins2.0/MobileLane/webpack.config.js
+++ fannie/modules/plugins2.0/MobileLane/webpack.config.js
@@ -17,27 +17,36 @@ var config = {
         filename: 'bundle.js'
     },
     module : {
-        loaders : [
+        rules : [
             {
                 test : /\.jsx?/,
                 include : APP_DIR,
-                loader : 'babel',
+                loader : 'babel-loader',
                 query: {
-                    presets: ['es2015', 'react']
+                    presets: ['env', 'react']
                 }
             },
             { test : /\.css?/, loader : 'style-loader!css-loader' },
             { test: /\.png$/, loader: "url-loader?limit=100000" },
             { test: /\.jpg$/, loader: "file-loader" },
-            { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
+            { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' }
         ]
     },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js")
-    ]
+    optimization: {
+        runtimeChunk: "single", // enable "runtime" chunk
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendor",
+                    chunks: "all"
+                }
+            }
+        }
+    }
 };
 
 module.exports = config;
