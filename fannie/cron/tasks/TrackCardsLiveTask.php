d7c7b29b1d19f9ed648de1c3ebe850145db1dfc7
Andy Theuninck <andy@gohanman.com>
2019-05-27 10:54:30 -0500

    Card tracking task for updating current-day data

---
+++ fannie/cron/tasks/TrackCardsLiveTask.php
@@ -0,0 +1,70 @@
+<?php
+
+class TrackCardsLiveTask extends FannieTask
+{
+    public $name = 'Track Cards (live)';
+
+    public $description = 'Monitors current activity for known payment cards and re-numbers transactions with the appropriate customer';
+
+    public $default_schedule = array(
+        'min' => 10,
+        'hour' => '*',
+        'day' => '*',
+        'month' => '*',
+        'weekday' => '*',
+    );
+
+    /**
+     * Look up non-owner card transactions for the current day
+     * Check whether these cards have been assigned a tracking customer number
+     * If so, update the transaction data w/ the tracking number
+     */
+    public function run()
+    {
+        $dbc = FannieDB::get($this->config->get('OP_DB'));
+        $ptP = $dbc->prepare("SELECT * FROM " . FannieDB::fqn('PaycardTransactions', 'trans') . "
+            WHERE dateID=? AND empNo=? AND registerNo=? AND transNo=? AND amount=? AND xResultMessage LIKE '%AP%'");
+        $tcP = $dbc->prepare("SELECT cardNo FROM " . FannieDB::fqn('TrackedCards', 'op') . " WHERE hash=?");
+        $upP = $dbc->prepare("UPDATE " . FannieDB::fqn('dtransactions', 'trans') . "
+            SET card_no=?
+            WHERE datetime BETWEEN ? AND ?
+                AND emp_no=?
+                AND register_no=?
+                AND trans_no=?
+                AND store_id=?");
+        $transR = $dbc->query("SELECT * FROM " . FannieDB::fqn('dlog', 'trans') . " WHERE trans_type='T' AND charflag='PT' and card_no IN (9, 11)");
+        while ($transW = $dbc->fetchRow($transR)) {
+            $stamp = strtotime($transW['tdate']);
+            $ptArgs = array(
+                date('Ymd', $stamp),
+                $transW['emp_no'],
+                $transW['register_no'],
+                $transW['trans_no'],
+                abs($transW['total']),
+            );
+            $ptRow = $dbc->getRow($ptP, $ptArgs);
+            if ($ptRow) {
+                $name = trim($ptRow['name']);
+                $pan = str_replace('X', '*', $ptRow['PAN']);
+                if ($name == 'Cardholder' || $name == '/' || $name == 'Customer' || $name == '') {
+                    continue;
+                }
+                $hash = md5($pan . $name);
+                $tracked = $dbc->getValue($tcP, array($hash));
+                if ($tracked) {
+                    echo "$hash is $tracked\n";
+                    $upArgs = array(
+                        $tracked,
+                        date('Y-m-d 00:00:00', $stamp),
+                        date('Y-m-d 23:59:59', $stamp),
+                        $transW['emp_no'],
+                        $transW['register_no'],
+                        $transW['trans_no'],
+                        $transW['store_id'],
+                    );
+                    $dbc->execute($upP, $upArgs);
+                }
+            }
+        }
+    }
+}
