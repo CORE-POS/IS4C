b2f890306d48d58fd1cb02d0f93a9b184ff16af6
Andy Theuninck <andy@gohanman.com>
2019-06-25 15:47:04 -0500

    Set expanded text on COOL upload

--- fannie/item/hobartcsv/CoolItemUploadPage.php
+++ fannie/item/hobartcsv/CoolItemUploadPage.php
@@ -105,7 +105,7 @@ class CoolItemUploadPage extends \COREPOS\Fannie\API\FannieUploadPage
                 'Price' => $price,
                 'Type' => $item['weight'] == 0 ? 'Random Weight' : 'Fixed Weight',
                 'ReportingClass' => 1,
-                'ExpandedText' => $text,
+                'ExpandedText' => $item['text'],
                 'MOSA' => $item['mosaStatement'],
                 'OriginText' => $cool,
             );
