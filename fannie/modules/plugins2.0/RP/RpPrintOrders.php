2b82e1e14f30a5f675eaf1976671d22164d3f602
Andy Theuninck <andy@gohanman.com>
2019-07-02 11:20:50 -0500

    RP order archiving, faster date setting

--- fannie/modules/plugins2.0/RP/RpPrintOrders.php
+++ fannie/modules/plugins2.0/RP/RpPrintOrders.php
@@ -10,6 +10,13 @@ class RpPrintOrders extends FannieRESTfulPage
     protected $header = '';
     protected $title = '';
 
+    public function preprocess()
+    {
+        $this->addRoute('get<archive>');
+
+        return parent::preprocess();
+    }
+
     protected function post_id_handler()
     {
         $orderID = FormLib::get('orderID');
@@ -19,6 +26,34 @@ class RpPrintOrders extends FannieRESTfulPage
         echo 'OK';
 
         return false;
+    }
+
+    protected function get_archive_view()
+    {
+        $ids = explode(',', $this->archive);
+        list($inStr, $args) = $this->connection->safeInClause($ids);
+
+        $archiveP = $this->connection->prepare("
+            UPDATE PurchaseOrder
+            SET placed=1, placedDate=" . $this->connection->now() . "
+            WHERE orderID IN ({$inStr})");
+        $this->connection->execute($archiveP, $args);
+
+        $ret = '<div class="alert alert-success">Order(s) archived</div>';
+        $prep = $this->connection->prepare("
+            SELECT o.orderID, v.vendorName
+            FROM PurchaseOrder AS o
+                LEFT JOIN vendors AS v ON o.vendorID=v.vendorID
+            WHERE o.orderID IN ({$inStr})");
+        $res = $this->connection->execute($prep, $args);
+        $ret .= '<ul>';
+        while ($row = $this->connection->fetchRow($res)) {
+            $ret .= sprintf('<li><a href="../../../purchasing/ViewPurchaseOrders.php?id=%d">#%d %s</a></li>',
+                $row['orderID'], $row['orderID'], $row['vendorName']);
+        }
+        $ret .= '</ul>';
+
+        return $ret;
     }
 
     protected function get_id_view()
@@ -61,11 +96,13 @@ class RpPrintOrders extends FannieRESTfulPage
                     list($row['receivedDate'],) = explode(' ', $row['receivedDate']);
                 }
                 $recv = sprintf('<input type="text" size="2" class="form-control input-sm date-field"
-                            value="%s" style="font-size: 85%%;" onchange="setRecvDate(this.value, \'%s\', %d);" />',
+                            value="%s" style="font-size: 85%%;" data-upc="%s" data-order-id="%d" />',
                     $row['receivedDate'], $row['upc'], $row['orderID']);
             }
             $organic = $this->connection->getValue($orgP, array(str_replace('LC', '', $row['upc'])));
-            $ret .= sprintf('<tr class="%s"><td>%s</td><td>%s</td><td>$%.2f (%s) %s %s</td><td>%s%s</td><td>%s</td></tr>',
+            $ret .= sprintf('<tr class="%s">
+                <td class="vendor">%s</td><td>%s</td><td>$%.2f (%s) %s %s</td><td>%s%s</td>
+                <td class="incoming">%s</td></tr>',
                 $this->vendorColor($row['vendorName']),
                 $row['vendorName'],
                 ($organic ? 'org' : 'non-o'),
@@ -80,6 +117,11 @@ class RpPrintOrders extends FannieRESTfulPage
             $costTotal += ($row['quantity'] * $row['caseSize'] * $row['unitCost']);
         }
         $ret .= '</table>';
+        $ret .= '<p>
+            <a href="RpPrintOrders.php?archive=' . $this->id . '" class="btn btn-default">Archive Order(s)</a>
+            </p>';
+
+        $this->addOnloadCommand("\$('td.incoming input').change(function () { syncIncoming(this); });");
 
         return '<div class="pull-right h4">Est. Total: $' . $costTotal . '</div>' . $ret;
     }
@@ -112,6 +154,17 @@ function setRecvDate(d, upc, oID) {
         url: 'RpPrintOrders.php',
         type: 'post',
         data: dstr
+    });
+}
+function syncIncoming(elem) {
+    var myDate = elem.value;
+    var farm = $(elem).closest('tr').find('td.vendor').text();
+    $('td.vendor').each(function () {
+        if ($(this).text() == farm) {
+            var inp = $(this).closest('tr').find('td.incoming input');
+            $(inp).val(myDate);
+            setRecvDate(myDate, $(inp).attr("data-upc"), $(inp).attr("data-order-id"));
+        }
     });
 }
 JAVASCRIPT;
