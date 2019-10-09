f00aeb76d4a9e48b95b43ea4d71ac326539355ca
Andy Theuninck <andy@gohanman.com>
2019-10-09 12:36:33 -0500

    Bad query in Overshort notes lookup

--- fannie/modules/plugins2.0/OverShortTools/OverShortDayPage.php
+++ fannie/modules/plugins2.0/OverShortTools/OverShortDayPage.php
@@ -318,7 +318,7 @@ class OverShortDayPage extends FanniePage
             $output .= "<td id=overallCountTotal>$overallCountTotal</td>";
             $output .= "<td id=overallOSTotal>$overallOSTotal</td></tr>";
 
-            $noteR = $dbc->execute($noteP, array(-1, $date));
+            $noteR = $dbc->execute($noteP, array(-1, $date, $store));
             $noteW = $dbc->fetchRow($noteR);
             $note = $noteW[0];
             $output .= "<tr><td>&nbsp;</td><td>Notes</td><td colspan=3</td>";
