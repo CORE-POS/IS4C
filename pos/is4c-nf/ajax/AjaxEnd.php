3dbcf96e8f829f2e188169ab85762575d263a699
Andy Theuninck <andy@gohanman.com>
2019-11-22 12:29:41 -0600

    Push termReset later after receipt

--- pos/is4c-nf/ajax/AjaxEnd.php
+++ pos/is4c-nf/ajax/AjaxEnd.php
@@ -67,7 +67,6 @@ class AjaxEnd extends AjaxCallback
         }
 
         if ($transFinished) {
-            UdpComm::udpSend("termReset");
             $sdObj = ScaleDriverWrapper::factory($this->session->get('scaleDriver'));
             if (is_object($sdObj)) {
                 $sdObj->readReset();
@@ -91,6 +90,10 @@ class AjaxEnd extends AjaxCallback
         }
 
         $this->outputReceipt($receiptContent, $customerEmail);
+
+        if ($transFinished) {
+            UdpComm::udpSend("termReset");
+        }
 
         return array();
     }
