86e36708653190d7a4441052ef61645abb02f6ae
Andy Theuninck <andy@gohanman.com>
2019-12-17 14:43:35 -0600

    Expanded potential bagfee totalaction behavior

--- pos/is4c-nf/plugins/AccessProgram/BagFeeTotal.php
+++ pos/is4c-nf/plugins/AccessProgram/BagFeeTotal.php
@@ -10,12 +10,29 @@ class BagFeeTotal extends TotalAction
     {
         $dbc = Database::tDataConnect();
 
-        $prep = $dbc->prepare("SELECT SUM(total) FROM localtemptrans WHERE trans_subtype IN ('EF','EC','WI')");
-        $tenderAmt = $dbc->getValue($prep);
+        $prep = $dbc->prepare("SELECT
+                SUM(CASE WHEN trans_subtype IN ('EF','EC','WI') THEN total ELSE 0 END) as tender,
+                SUM(CASE WHEN department=701 THEN total ELSE 0 END) as donate,
+                SUM(CASE WHEN upc='0000000010730' THEN total ELSE 0 END) as fees
+            FROM localtemptrans");
+        $info = $dbc->getRow($prep);
+        $tenderAmt = $info['tender'];
         if (CoreLocal::get('memType') == 5 || abs($tenderAmt) > 0.005) {
-            $dbc->query("UPDATE localtemptrans SET total=0 WHERE upc='0000000001354'");
+            $dbc->query("UPDATE localtemptrans SET total=0 WHERE upc='0000000010730'");
             return MiscLib::baseURL() . 'gui-modules/pos2.php?reginput=TO&repeat=1';
         }
+
+        /*
+        $due = sprintf('%.2f', CoreLocal::get('amtdue'));
+        if (substr($due, -2) != '00' && $info['fees'] > 0 && $info['donate'] > 0) {
+            $reduce = '0.' . substr($due, -2);
+            if ($reduce <= $info['donate']) {
+                $upP = $dbc->prepare("UPDATE localtemptrans SET total=?
+                    WHERE department=701");
+                $dbc->execute($upP, array($info['donate'] - $reduce));
+            }
+        }
+         */
 
         return true;
     }
