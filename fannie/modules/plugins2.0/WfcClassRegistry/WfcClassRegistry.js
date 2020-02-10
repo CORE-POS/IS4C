e155f80a213e8092cf33b7b73c16f78b7329abdd
Andy Theuninck <andy@gohanman.com>
2020-02-10 15:08:22 -0600

    (WFC) lose unnecessary near-sold-out alerting

--- fannie/modules/plugins2.0/WfcClassRegistry/WfcClassRegistry.js
+++ fannie/modules/plugins2.0/WfcClassRegistry/WfcClassRegistry.js
@@ -117,17 +117,3 @@ function withdraw()
         });
     });
 }
-function checkSoldOut()
-{
-    $('.first_name').change(function(){
-        // testing 10/22 csather
-        console.log('checkSoldOut() is being called');
-        $.ajax({
-            type: 'post',
-            url: 'noauto/alertRegFull.php',
-            dataType: 'json',
-            data: 'upc='+$('#upc').val(),
-            success: function(resp) {}
-        });
-    });
-}
