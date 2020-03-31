c5a428ff0a9fa60d6ced76f465cfe5cac329651e
Corey Sather <corey042@gmail.com>
2020-03-31 13:30:59 -0500

    New shelftag classes

---
+++ fannie/admin/labels/pdf_layouts/WFC_Dark_Extended.php
@@ -0,0 +1,168 @@
+<?php
+use COREPOS\Fannie\API\FanniePlugin;
+if (!class_exists('FpdfWithBarcode')) {
+    include(dirname(__FILE__) . '/../FpdfWithBarcode.php');
+}
+if (!class_exists('FannieAPI')) {
+    include(__DIR__ . '/../../classlib2.0/FannieAPI.php');
+}
+
+class WFC_Dark_Extended extends FpdfWithBarcode
+{
+    private $tagdate;
+    function setTagDate($str){
+        $this->tagdate = $str;
+    }
+
+    function barcodeText($x, $y, $h, $barcode, $len)
+    {
+        $this->SetFont('Arial','',8);
+        $this->Text($x,$y-$h+(17/$this->k),substr($barcode,-$len).' '.$this->tagdate);
+    }
+}
+
+function WFC_Dark_Extended($data,$offset=0)
+{
+    $dbc = FannieDB::get(FannieConfig::config('OP_DB'));
+    $pdf = new FPDF('L','mm','Letter');
+    $pdf->AddPage();
+    $pdf->SetFillColor(0, 0, 0);
+    $pdf->SetTextColor(255, 255, 255);
+
+    define('FPDF_FONTPATH', dirname(__FILE__) . '/../../../modules/plugins2.0/CoopDealsSigns/noauto/fonts/');
+    $pdf->AddFont('Gill', 'B', 'GillSansMTPro-Heavy.php');
+    $pdf->AddFont('Gill', '', 'GillSansMTPro-Heavy.php');
+    $pdf->SetFont('Gill','B', 16);  //Set the font 
+
+    $width = 68;
+    $height = 34;
+    $left = 3;  
+    $top = 3;
+    $guide = 0.3;
+
+    $x = $left+$guide; $y = $top+$guide;
+
+    $pdf->SetTopMargin($top); 
+    $pdf->SetLeftMargin($left);
+    $pdf->SetRightMargin($left);
+    $pdf->SetAutoPageBreak(False);
+
+    $i = 0;
+    foreach($data as $k => $row){
+        if ($i % 24 == 0 && $i != 0) {
+            $pdf->AddPage('L');
+            $x = $left;
+            $y = $top;
+            $i = 0;
+        }
+        if ($i == 0) {
+            $pdf = generateTag($x, $y, $guide, $width, $height, $pdf, $row, $dbc);
+        } else if ($i % 4 == 0 && $i != 0) {
+            $x = $left+$guide;
+            $y += $height+$guide;
+        } else {
+            $x += $width+$guide;
+        }
+        $pdf = generateTag($x, $y, $guide, $width, $height, $pdf, $row, $dbc);
+        $i++;
+    }
+
+    $pdf = $pdf->Output();
+}
+
+function generateTag($x, $y, $guide, $width, $height, $pdf, $row, $dbc)
+{
+    $desc = $row['description'];
+    $brand = $row['brand'];
+    $price = $row['normal_price'];
+
+    $args = array($row['upc']);
+    $prep = $dbc->prepare("
+        SELECT pu.description, p.scale
+        FROM productUser AS pu
+            INNER JOIN products AS p ON pu.upc=p.upc
+        WHERE pu.upc = ?");
+    $res = $dbc->execute($prep, $args);
+    $row = $dbc->fetchRow($res);
+    $desc = $row['description'];
+
+    // prep tag canvas
+    $pdf->SetXY($x,$y);
+    $pdf->Cell($width, $height, '', 0, 1, 'C', true); 
+
+    // use line break to split str if exists; else wordwrap if 2 lines req.
+    $lines = array();
+    if (strstr($desc, "\r\n")) {
+        $lines = explode ("\r\n", $desc);
+    } elseif (strlen($desc) > 20) {
+        $wrp = wordwrap($desc, strlen($desc)/1.5, "*", false);
+        $lines = explode('*', $wrp);
+    } else {
+        $lines[0] = $desc;
+    }
+
+    /*
+        Add Brand Text
+    */
+    $strlen = (strlen($brand));
+    if ($strlen >= 30) {
+        $pdf->SetFont('Gill','B', 9);
+    } elseif ($strlen > 20 && $strlen < 30) {
+        $pdf->SetFont('Gill','B', 12);
+    } else {
+        $pdf->SetFont('Gill','B', 16);
+    }
+    $pdf->SetXY($x,$y+2);
+    $pdf->Cell($width, 8, $brand, 0, 1, 'C', true); 
+
+    /*
+        Add Description Text
+    */
+    $strlen = (strlen($lines[0]) > strlen($lines[1])) ? strlen($lines[0]) : strlen($lines[1]);
+    if ($strlen >= 30) {
+        $pdf->SetFont('Gill','', 9);
+    } elseif ($strlen > 20 && $strlen < 30) {
+        $pdf->SetFont('Gill','', 12);
+    } else {
+        $pdf->SetFont('Gill','', 16);
+    }
+    if (count($lines) > 1) {
+        $pdf->SetXY($x,$y+12);
+        $pdf->Cell($width, 5, $lines[0], 0, 1, 'C', true); 
+        $pdf->SetXY($x, $y+19);
+        $pdf->Cell($width, 5, $lines[1], 0, 1, 'C', true); 
+    } else {
+        $pdf->SetXY($x,$y+14);
+        $pdf->Cell($width, 5, $lines[0], 0, 1, 'C', true); 
+    }
+
+
+    /*
+        Add Price Text
+    */
+    $pdf->SetXY($x,$y+27);
+    $pdf->Cell($width, 5, "$".$price, 0, 1, 'C', true); 
+
+    /*
+        Create Guide-Lines
+    */ 
+    $pdf->SetFillColor(155, 155, 155);
+    // vertical 
+    $pdf->SetXY($width+$x, $y);
+    $pdf->Cell($guide, $height+$guide, '', 0, 1, 'C', true);
+
+    $pdf->SetXY($x-$guide, $y-$guide); 
+    $pdf->Cell($guide, $height+$guide, '', 0, 1, 'C', true);
+
+    // horizontal
+    $pdf->SetXY($x, $y-$guide); 
+    $pdf->Cell($width+$guide, $guide, '', 0, 1, 'C', true);
+
+    $pdf->SetXY($x, $y+$height); 
+    $pdf->Cell($width+$guide, $guide, '', 0, 1, 'C', true);
+
+    $pdf->SetFillColor(0, 0, 0);
+
+    return $pdf;
+}
+
