726af1151585018bc3e65e9df92c7c9da8be0aad
Andy Theuninck <andy@gohanman.com>
2020-05-07 12:40:50 -0500

    RP: Archive page as report for date ranges, sorting

--- fannie/modules/plugins2.0/RP/RpArchivePage.php
+++ fannie/modules/plugins2.0/RP/RpArchivePage.php
@@ -1,13 +1,22 @@
 <?php include(__DIR__ . '/../../../config.php'); if (!class_exists('FannieAPI')) { include(__DIR__ . '/../../../classlib2.0/FannieAPI.php');
 }
 
-class RpArchivePage extends FannieRESTfulPage
+class RpArchivePage extends FannieReportPage
 {
     protected $header = 'Archived Orders';
     protected $title = 'Archived Orders';
+    protected $new_tablesorter = true;
+    protected $required_fields = array('date1', 'date2');
+    protected $report_headers = array('Farm', 'Item', 'Quantity', 'Ordered', 'Delivery');
 
-    protected function get_view()
+    public function fetch_report_data()
     {
+        try {
+            $date1 = $this->form->date1;
+            $date2 = $this->form->date2;
+        } catch (Exception $ex) {
+            return array();
+        }
         $store = FormLib::get('store');
         if (!$store) {
             $store = COREPOS\Fannie\API\lib\Store::getIdByIp();
@@ -20,16 +29,12 @@ class RpArchivePage extends FannieRESTfulPage
             WHERE o.userID=-99
                 AND o.vendorID=-2
                 AND o.storeID=?
-                AND
-                    (o.placedDate >= CURDATE()
-                    OR
-                    i.receivedDate >= CURDATE()
-                )
+                AND i.receivedDate BETWEEN ? AND ?
             ORDER BY i.receivedDate");
-        $res = $this->connection->execute($prep, array($store));
-        $table = '';
+        $res = $this->connection->execute($prep, array($store, $date1, $date2));
+        $data = array();
         while ($row = $this->connection->fetchRow($res)) {
-            $table .= sprintf('<tr><td>%s</td><td>%s</td><td>%.2f</td><td>%s</td><td>%s</td></tr>',
+            $data[] = array(
                 $row['brand'],
                 $row['description'],
                 $row['quantity'],
@@ -38,11 +43,38 @@ class RpArchivePage extends FannieRESTfulPage
             );
         }
 
+        return $data;
+    }
+
+    public function form_content()
+    {
+        $dates = FormLib::standardDateFields();
+        $stores = FormLib::storePicker();
+
+        $start = strtotime('last monday');
+        if (date('N') == 1) {
+            $start = time();
+        }
+        $end = mktime(0, 0, 0, date('n', $start), date('j', $start) + 7, date('Y', $start));
+        $sDate = date('Y-m-d', $start);
+        $eDate = date('Y-m-d', $end);
+        // only default populate if there aren't form values provided
+        if (FormLib::get('json', false) === false) {
+            $this->addOnloadCommand("\$('#date1').val('{$sDate}');");
+            $this->addOnloadCommand("\$('#date2').val('{$eDate}');");
+        }
+
         return <<<HTML
-<table class="table table-bordered">
-    <tr><th>Vendor</th><th>Item</th><th>Quantity</th><th>Ordered</th><th>Est. Delivery</th></tr>
-    {$table}
-</table>
+<form method="get" action="RpArchivePage.php">
+    {$dates}
+    <div class="row"></div>
+    <div class="form-group">
+        <label>Store</label>
+        {$stores['html']}
+    </div>
+    <div class="form-group">
+        <button class="btn btn-default">Get Archived Orders</button>
+    </div>
 HTML;
     }
 }
