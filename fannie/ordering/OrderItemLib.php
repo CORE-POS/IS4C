fc3c7553b7e2411bbc91d01668423676310a9824
Andy Theuninck <andy@gohanman.com>
2020-09-10 12:37:25 -0500

    Use current info for SPO items when active OR on sale

--- fannie/ordering/OrderItemLib.php
+++ fannie/ordering/OrderItemLib.php
@@ -86,7 +86,7 @@ class OrderItemLib
                 LEFT JOIN vendors AS n ON p.default_vendor_id=n.vendorID
                 LEFT JOIN PriceRules AS r ON p.price_rule_id=r.priceRuleID
             WHERE p.upc=?
-                AND p.inUse=1
+                AND (p.inUse=1 OR p.discounttype=1)
         ');
         $prodR = $dbc->execute($prodP, array($upc));
         if ($prodR && $dbc->numRows($prodR) > 0) {
