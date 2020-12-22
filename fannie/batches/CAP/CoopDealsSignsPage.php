394583de3f6bca2ae51c7bf4fe30a31ac7c2f6b4
Corey Sather <corey042@gmail.com>
2020-12-22 13:56:54 -0600

    Updated year

--- fannie/batches/CAP/CoopDealsSignsPage.php
+++ fannie/batches/CAP/CoopDealsSignsPage.php
@@ -245,7 +245,7 @@ HTML;
         }
 
         $years = '';
-        $curYear = 2021;
+        $curYear = 2022;
         $curMonth = date('m');
         for ($i=2017; $i<$curYear; $i++) {
             $sel = ($i == FormLib::get('year')) ? " selected " : "";
