9500945e91dee5bd2733834f14f1173140034575
Andy Theuninck <andy@gohanman.com>
2021-07-21 14:23:38 -0500

    TaxView fix for ONLY_FULL_GROUP_BY

--- pos/is4c-nf/lib/models/trans/TaxViewModel.php
+++ pos/is4c-nf/lib/models/trans/TaxViewModel.php
@@ -53,7 +53,7 @@ class TaxViewModel extends ViewModel
                 WHEN l.trans_type IN ('I','D') AND discountable=0 THEN total 
                 WHEN l.trans_type IN ('I','D') AND discountable<>0 THEN total * ((100-s.percentDiscount)/100)
                 ELSE 0 END
-            ) * r.rate AS DECIMAL(10,2)) as taxTotal,
+            ) * MAX(r.rate) AS DECIMAL(10,2)) as taxTotal,
             CAST(SUM(CASE 
                 WHEN l.trans_type IN ('I','D') AND discountable=0 AND foodstamp=1 THEN total 
                 WHEN l.trans_type IN ('I','D') AND discountable<>0 AND foodstamp=1 THEN total * ((100-s.percentDiscount)/100)
@@ -63,7 +63,7 @@ class TaxViewModel extends ViewModel
                 WHEN l.trans_type IN ('I','D') AND discountable=0 AND foodstamp=1 THEN total 
                 WHEN l.trans_type IN ('I','D') AND discountable<>0 AND foodstamp=1 THEN total * ((100-s.percentDiscount)/100)
                 ELSE 0 END
-            ) * r.rate AS DECIMAL(10,2)) as fsTaxTotal,
+            ) * MAX(r.rate) AS DECIMAL(10,2)) as fsTaxTotal,
             -1*MAX(fsTendered) as foodstampTender,
             MAX(r.rate) as taxrate
             FROM
