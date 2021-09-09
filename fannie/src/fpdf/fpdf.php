a931880720d5caf86679c2c70c672ea29f77ff95
Andy Theuninck <andy@gohanman.com>
2021-09-09 12:31:13 -0500

    PHP Compat: stop use curly brace array access in FPDF

--- fannie/src/fpdf/fpdf.php
+++ fannie/src/fpdf/fpdf.php
@@ -434,7 +434,7 @@ function GetStringWidth($s)
     $w=0;
     $l=strlen($s);
     for($i=0;$i<$l;$i++)
-        $w+=$cw[$s{$i}];
+        $w+=$cw[$s[$i]];
     return $w*$this->FontSize/1000;
 }
 
@@ -742,7 +742,7 @@ function MultiCell($w,$h,$txt,$border=0,$align='J',$fill=0)
     while($i<$nb)
     {
         //Get next character
-        $c=$s{$i};
+        $c=$s[$i];
         if($c=="\n")
         {
             //Explicit line break
@@ -832,7 +832,7 @@ function Write($h,$txt,$link='')
     while($i<$nb)
     {
         //Get next character
-        $c=$s{$i};
+        $c=$s[$i];
         if($c=="\n")
         {
             //Explicit line break
@@ -1180,13 +1180,13 @@ function _putfonts()
         $compressed=(substr($file,-2)=='.z');
         if(!$compressed && isset($info['length2']))
         {
-            $header=(ord($font{0})==128);
+            $header=(ord($font[0])==128);
             if($header)
             {
                 //Strip first binary header
                 $font=substr($font,6);
             }
-            if($header && ord($font{$info['length1']})==128)
+            if($header && ord($font[$info['length1']])==128)
             {
                 //Strip second binary header
                 $font=substr($font,0,$info['length1']).substr($font,$info['length1']+6);
@@ -1446,7 +1446,7 @@ function _beginpage($orientation)
         $orientation=$this->DefOrientation;
     else
     {
-        $orientation=strtoupper($orientation{0});
+        $orientation=strtoupper($orientation[0]);
         if($orientation!=$this->DefOrientation)
             $this->OrientationChanges[$this->page]=true;
     }
