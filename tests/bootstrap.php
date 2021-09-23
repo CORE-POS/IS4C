66a850ac2f746167de3af8a15368872024adce3a
Andy Theuninck <andy@gohanman.com>
2021-09-23 10:55:45 -0500

    Need newer PHPUnit compat

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,3 +2,10 @@
 include(dirname(__FILE__) . '/pos/bootstrap.php');
 include(dirname(__FILE__) . '/fannie/bootstrap.php');
 
+// patch in PHPUnit compatibility
+if (class_exists('PHPUnit\Framework\TestCase')) {
+    class PHPUnit_Framework_TestCase extends PHPUnit\Framework\TestCase
+    {
+    }
+}
+
