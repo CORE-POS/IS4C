54d5ca91feec07c961a6fbf907714484e6a9580f
Andy Theuninck <andy@gohanman.com>
2021-09-23 10:58:57 -0500

    Fixed 7.2, now need this for 5.6

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,8 @@
 include(dirname(__FILE__) . '/pos/bootstrap.php');
 include(dirname(__FILE__) . '/fannie/bootstrap.php');
 
-// patch in PHPUnit compatibility
-if (class_exists('PHPUnit\Framework\TestCase')) {
+// patch in PHPUnit compatibility if needed
+if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
     class PHPUnit_Framework_TestCase extends PHPUnit\Framework\TestCase
     {
     }
