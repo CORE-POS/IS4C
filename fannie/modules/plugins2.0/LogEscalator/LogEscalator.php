631b25466547bbbe56294b1433c5b24f578925af
Lance Edgar <lance@edbob.org>
2021-09-29 18:45:11 -0400

    Make configurable the "offset" file path for LogEscalator

--- fannie/modules/plugins2.0/LogEscalator/LogEscalator.php
+++ fannie/modules/plugins2.0/LogEscalator/LogEscalator.php
@@ -33,6 +33,9 @@ class LogEscalator extends FanniePlugin
     'description'=>'Email address for notifications'),
     'LogEscalateFrom' => array('default'=>'','label'=>'From Address',
     'description'=>'From address on the notifications'),
+    'LogEscalateOffsetFile' => array('label'=>'Offset File',
+                                     'default'=>'/tmp/flmoffset',
+                                     'description'=>'Path to file for tracking current offset'),
     );
 
     public $plugin_description = 'Plugin to trigger email notifications on logged errors & warnings.';
