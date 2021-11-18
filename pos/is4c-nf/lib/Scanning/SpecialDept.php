a1f36cfd957db59da961d00b797937c3643b4813
Andy Theuninck <andy@gohanman.com>
2021-11-18 13:37:20 -0600

    Remove type arg since non-array is handled anyway

--- pos/is4c-nf/lib/Scanning/SpecialDept.php
+++ pos/is4c-nf/lib/Scanning/SpecialDept.php
@@ -61,7 +61,7 @@ class SpecialDept
       @param $arr a handler map (array)
       @return handler map (array)
     */
-    public function register($deptID, array $arr)
+    public function register($deptID, $arr)
     {
         if (!is_array($arr)) {
             $arr = array();
