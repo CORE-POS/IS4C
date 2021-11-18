5be3d964aef71357f4e68bbacb086e2b49849014
Andy Theuninck <andy@gohanman.com>
2021-11-18 15:16:21 -0600

    Numeric equality has gotten stricter

--- fannie/batches/UNFI/load-classes/RdwUploadPage.php
+++ fannie/batches/UNFI/load-classes/RdwUploadPage.php
@@ -177,7 +177,7 @@ class RdwUploadPage extends \COREPOS\Fannie\API\FannieUploadPage {
     {
         $phpunit->assertEquals(false, $this->getVendorID());
         $phpunit->assertEquals(false, $this->process_file(array(), array()));
-        $phpunit->assertEquals('1234', $this->cleanUPC('91234'));
+        $phpunit->assertEquals('0000000001234', $this->cleanUPC('91234'));
         $phpunit->assertEquals('0001234512345', $this->cleanUPC('12345 12345-0'));
         $phpunit->assertInternalType('string', $this->results_content());
     }
