f1ddfe84e1b7c9a678c8f2d0e279fdcb75f0ac41
Andy Theuninck <andy@gohanman.com>
2021-11-18 16:14:12 -0600

    Make type signature match

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ include(dirname(__FILE__) . '/fannie/bootstrap.php');
 if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
     class PHPUnit_Framework_TestCase extends PHPUnit\Framework\TestCase
     {
-        public static function assertInternalType($type, $var, $message='')
+        public static function assertInternalType(string $type, $var, string $message='')
         {
             switch (strtolower($type)) {
                 case 'object':
