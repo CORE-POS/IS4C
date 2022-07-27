07522b87777c63aff791553a88fa9c23752d9888
Andy Theuninck <andy@gohanman.com>
2022-07-27 10:13:43 -0500

    Comments tweak

--- fannie/batches/UNFI/VPBPIV.php
+++ fannie/batches/UNFI/VPBPIV.php
@@ -865,10 +865,14 @@ JAVASCRIPT;
             $date = new DateTime();
             $date = $date->format('Y-m-d');
             $changeClassA = ($date == substr($row['date'], -10)) ? 'highlight' : '';
-            //$changeClassA = ('2021-09-03' == substr($row['date'], -10)) ? 'highlight' : '';
+            /* 
+            $changeClassA = ('2022-07-06' == substr($row['date'], -10)) ? 'highlight' : '';
+            */
 
             $changeClassB = ($date == $row['reviewed']) ? 'highlight' : '';
-            //$changeClassB = ('2022-04-04' == $row['reviewed']) ? 'highlight' : '';
+            /*
+            $changeClassB = ('2022-07-06' == $row['reviewed']) ? 'highlight' : '';
+            */
 
             $srpClassA = ($row['srp'] > $row['normal_price']) ? 'red' : 'yellow';
             $direction = ($row['srp'] > $row['normal_price']) ? '&#x2191;' : '&#x2193;';
