8018c777cb6c0a05fe43e87f3a98ecab383f8aa1
Corey Sather <corey042@gmail.com>
2022-11-18 10:14:06 -0600

    Make multiple selection easier

--- fannie/modules/plugins2.0/Pickup/ViewPickups.php
+++ fannie/modules/plugins2.0/Pickup/ViewPickups.php
@@ -356,7 +356,7 @@ HTML;
             $table .= sprintf('<tr>
                 <td><a href="ViewPickups.php?id=%d" class="btn btn-default">View</td>
                 <td>%s</td><td>%s</td><td>%s</td><td>%s</td>
-                <td><input type="checkbox" name="print" value="%d" /></tr>',
+                <td><input type="checkbox" name="print" class="print-checkbox value="%d" /></tr>',
                 $row['pickupOrderID'],
                 $row['orderNumber'], $row['name'], $dateTime, $row['status'],
                 $row['pickupOrderID']);
@@ -395,6 +395,44 @@ function printAll() {
     $('body').append(form);
     $('#printAllForm').submit();
 }
+
+var lastChecked = null;
+var i = 0;
+var indexCheckboxes = function() {
+    var upcCheckBoxes = document.getElementsByClassName("print-checkbox"); 
+    for (var i = 0; i < upcCheckBoxes.length; i++) {
+        upcCheckBoxes.item(i).setAttribute("data-index", i);
+    }
+};
+document.addEventListener("click", function (event) {
+    indexCheckboxes();
+});
+document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("print-checkbox")) {
+        if (lastChecked && event.shiftKey) {
+            var i = parseInt(lastChecked.getAttribute("data-index"));
+            var j = parseInt(event.target.getAttribute("data-index"));
+            var checked = event.target.checked;
+
+            var low = i;
+            var high = j;
+            if (i > j){
+                var low = j;
+                var high = i;
+            }
+
+            for(var c = low; c < high; c++) {
+                if (c != low && c!= high) {
+                    var check = checked ? true : false;
+                    curbox = document.querySelectorAll('[data-index="'+c+'"]');
+                    box = curbox.item(0);
+                    box.checked = check;
+                }
+            }
+        }
+        lastChecked = event.target; 
+    }
+});
 </script>
 HTML;
     }
