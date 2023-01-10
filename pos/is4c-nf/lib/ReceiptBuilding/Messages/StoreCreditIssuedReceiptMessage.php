e45fed3585257b5c6e02409a7d082065a04c6a13
Andy Theuninck <andy@gohanman.com>
2023-01-10 13:02:52 -0600

    (WFC) 2022 voucher tweak

--- pos/is4c-nf/lib/ReceiptBuilding/Messages/StoreCreditIssuedReceiptMessage.php
+++ pos/is4c-nf/lib/ReceiptBuilding/Messages/StoreCreditIssuedReceiptMessage.php
@@ -74,7 +74,8 @@ class StoreCreditIssuedReceiptMessage extends ReceiptMessage{
 
         $slip .= " * no cash back on Co-op Cash purchases\n";
         $slip .= " * change amount is not transferable to\n   another ticket\n";
-        $slip .= " * valid Dec 12 through Dec 31, 2020\n";
+        $slip .= " * valid through Dec 31, 2022\n";
+        $slip .= " * in store purchases only\n";
         $slip .= " * " . $ref . "\n";
         $slip .= ReceiptLib::centerString("................................................")."\n";
 
