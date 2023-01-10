83066aa62c52de78c459455b24a37b7321bbd1dd
Andy Theuninck <andy@gohanman.com>
2023-01-10 13:09:29 -0600

    Base class for Likecode callbacks

---
+++ fannie/classlib2.0/item/LikecodeCallback.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace COREPOS\Fannie\API\item;
+
+class LikecodeCallback
+{
+    public function run($likeCode)
+    {
+        $log = new \FannieLogger();
+        $log->debug('In the parent for some reason');
+    }
+}
+
+
