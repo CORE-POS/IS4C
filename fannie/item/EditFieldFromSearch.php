493fadf6703e9e8beb5e093345bc3fd1dd0170dd
Corey Sather <corey042@gmail.com>
2023-02-06 10:29:01 -0600

    Allow SignProperties edit if table exists

--- fannie/item/EditFieldFromSearch.php
+++ fannie/item/EditFieldFromSearch.php
@@ -42,7 +42,29 @@ class EditFieldFromSearch extends FannieRESTfulPage
     {
        $this->__routes[] = 'post<u>'; 
        $this->__routes[] = 'post<upc><field><value>';
+       $this->__routes[] = 'post<upc><signField><signValue>';
        return parent::preprocess();
+    }
+
+    protected function post_upc_signField_signValue_handler()
+    {
+        $model = new SignPropertiesModel($this->connection);
+        $columns = $model->getColumns();
+        if (!isset($columns[$this->signField])) {
+            echo 'Invalid field';
+            return false;
+        }
+
+        $args = array($this->signValue);
+        list($inStr, $args) = $this->connection->safeInClause($this->upc, $args);
+        $args[] = COREPOS\Fannie\API\lib\Store::getIdByIp();
+        $query = "UPDATE SignProperties SET " 
+            . $this->connection->identifierEscape($this->signField) . "=? 
+            WHERE upc IN ({$inStr}) AND storeID = ?";
+        $prep = $this->connection->prepare($query);
+        $res = $this->connection->execute($prep, $args);
+
+        return true;
     }
 
     protected function post_upc_field_value_handler()
@@ -77,6 +99,13 @@ class EditFieldFromSearch extends FannieRESTfulPage
             . $this->post_u_view();
     }
 
+    protected function post_upc_signField_signValue_view()
+    {
+        $this->u = $this->upc;
+        return '<div class="alert alert-success">Set ' . $this->signField . ' to ' . $this->signValue . '</div>'
+            . $this->post_u_view();
+    }
+
     protected function post_u_view()
     {
         $model = new ProductsModel($this->connection);
@@ -89,32 +118,71 @@ class EditFieldFromSearch extends FannieRESTfulPage
             }
         }
 
+        $s_def = $this->connection->tableExists('SignProperties');
+        if ($s_def !== false) {
+            $signModel = new SignPropertiesModel($this->connection);
+            $signColumns = $signModel->getColumns();
+            $signOpts = '<option value="">Select field...</option>';
+            $signIgnoreColumn = array('upc', 'storeID');
+            foreach ($signColumns as $name => $info) {
+                if (!in_array($name, $signIgnoreColumn)) {
+                    $signOpts .= '<option>' . $name . '</option>';
+                }
+            }
+        }
+
         $items = '<table class="table table-bordered small">';
+        $items2 = '';
         list($inStr, $args) = $this->connection->safeInClause($this->u);
         $prep = $this->connection->prepare("SELECT upc, brand, description FROM products WHERE upc IN ({$inStr}) GROUP BY upc, brand, description");
         $res = $this->connection->execute($prep, $args);
         while ($row = $this->connection->fetchRow($res)) {
             $items .= sprintf('<tr><td>%s<input type="hidden" name="upc[]" value="%s" /></td><td>%s</td><td>%s</td></tr>',
                 $row['upc'], $row['upc'], $row['brand'], $row['description']);
+            $items2 .= "<input type=\"hidden\" name=\"upc[]\" value=\"{$row['upc']}\" />";
         }
         $items .= '</table>';
 
         return <<<HTML
-<form method="post" action="EditFieldFromSearch.php">
-    <div class="form-group">
-        <label>Field</label>
-        <select name="field" required class="form-control">{$opts}</select>
+<div class="row">
+    <div class="col-lg-4">
+        <h3>Product Data</h3>
+        <form method="post" action="EditFieldFromSearch.php">
+            <div class="form-group">
+                <label>Field</label>
+                <select name="field" required class="form-control">{$opts}</select>
+            </div>
+            <div class="form-group">
+                <label>New Value</label>
+                <input type="text" class="form-control" name="value" />
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-default btn-core">Update Items</button>
+            </div>
+            {$items2}
+        </form>
     </div>
-    <div class="form-group">
-        <label>New Value</label>
-        <input type="text" class="form-control" name="value" />
+    <div class="col-lg-4">
+        <h3>Sign Data</h3>
+        <form method="post" action="EditFieldFromSearch.php">
+            <div class="form-group">
+                <label>Field</label>
+                <select name="signField" required class="form-control">{$signOpts}</select>
+            </div>
+            <div class="form-group">
+                <label>New Value</label>
+                <input type="text" class="form-control" name="signValue" />
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-default btn-core">Update Items</button>
+            </div>
+            {$items2}
+        </form>
     </div>
-    <div class="form-group">
-        <button type="submit" class="btn btn-default btn-core">Update Items</button>
-    </div>
-    <hr />
-    {$items}
-</form>
+    <div class="col-lg-4"></div>
+</div>
+<hr />
+{$items}
 HTML;
     }
 }
