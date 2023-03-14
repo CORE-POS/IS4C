6e13d9cb49b09ca587bf27a69f65eb6efb98dc4c
Andy Theuninck <andy@gohanman.com>
2023-03-14 10:49:39 -0500

    Minor name tidiness

--- fannie/classlib2.0/item/BatchCallback.php
+++ fannie/classlib2.0/item/BatchCallback.php
@@ -4,7 +4,7 @@ namespace COREPOS\Fannie\API\item;
 
 class BatchCallback
 {
-    public function run($upc)
+    public function run($batchID)
     {
         $log = new \FannieLogger();
         $log->debug('In the parent for some reason');
