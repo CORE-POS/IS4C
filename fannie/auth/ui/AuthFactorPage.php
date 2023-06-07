75e5f7ee00989eb976ffaae841493bb7cafd2fd9
Andy Theuninck <andy@gohanman.com>
2023-06-07 12:16:47 -0500

    Fix 2FA QR code

--- fannie/auth/ui/AuthFactorPage.php
+++ fannie/auth/ui/AuthFactorPage.php
@@ -52,7 +52,7 @@ class AuthFactorPage extends FannieRESTfulPage
         $qrCode
             ->setText($this->qr)
             ->setSize(400)
-            ->setPadding(10)
+            ->setMargin(10)
             ->setErrorCorrection('high')
             ->setForegroundColor(array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0))
             ->setBackgroundColor(array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0))
